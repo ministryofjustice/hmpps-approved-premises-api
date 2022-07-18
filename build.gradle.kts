e3f4b9c0a5a0ae4bfacf0c88ed810f26a97c54fc
Ed Davey <ed.davey@digital.justice.gov.uk>
2022-07-18 18:18:08 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.3"
+  kotlin("plugin.spring") version "1.6.21"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "17"
+    }
+  }
+}
