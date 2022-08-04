aa3d4cb1a4cced7abae10d83211085b9b8f426a4
brownbearsoftware <brownbearsoftware@gmail.com>
2022-08-04 13:04:53 +0100

    Implement GET Premises by Id Endpoint
    Implementation + integration tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.repository.PremisesRepository
+import java.util.UUID
 
 @Service
 class PremisesService(private val premisesRepository: PremisesRepository) {
   fun getAllPremises(): List<PremisesEntity> = premisesRepository.findAll()
+  fun getPremises(premisesId: UUID): PremisesEntity? = premisesRepository.findByIdOrNull(premisesId)
 }
