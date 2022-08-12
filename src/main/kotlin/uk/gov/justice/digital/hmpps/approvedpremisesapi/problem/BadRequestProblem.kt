31811a1f84066279a2a20b8138052f598fc4c649
brownbearsoftware <brownbearsoftware@gmail.com>
2022-08-12 12:19:35 +0100

    Add support for returning validation errors as per ADR-0001

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/problem/BadRequestProblem.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.problem
+
+import org.zalando.problem.AbstractThrowableProblem
+import org.zalando.problem.Exceptional
+import org.zalando.problem.Status
+
+class BadRequestProblem(private val invalidParams: Map<String, String>?) : AbstractThrowableProblem(null, "Bad Request", Status.BAD_REQUEST, "There is a problem with your request") {
+  override fun getCause(): Exceptional? {
+    return null
+  }
+
+  override fun getParameters(): MutableMap<String, Any> {
+    return mutableMapOf(
+      "invalid-params" to (invalidParams ?: emptyMap<String, Any>())
+    )
+  }
+}
