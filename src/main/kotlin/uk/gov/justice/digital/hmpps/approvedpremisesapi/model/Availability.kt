95e7120f0fe88a1f9ebf8125594b12f836600540
brownbearsoftware <brownbearsoftware@gmail.com>
2022-08-16 12:00:37 +0100

    Add method to PremisesService to get availability summary over a date range for a Premises

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/Availability.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.model
+
+import java.time.LocalDate
+
+data class Availability(
+  val date: LocalDate,
+  val pendingBookings: Int,
+  val arrivedBookings: Int,
+  val nonArrivedBookings: Int,
+  val cancelledBookings: Int,
+  val lostBeds: Int
+)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
@@ -2,18 +2,44 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.LostBedsEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.LostBedsRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.Availability
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.getDaysUntilExclusiveEnd
+import java.time.LocalDate
 import java.util.UUID
 
 @Service
 class PremisesService(
   private val premisesRepository: PremisesRepository,
-  private val lostBedsRepository: LostBedsRepository
+  private val lostBedsRepository: LostBedsRepository,
+  private val bookingRepository: BookingRepository
 ) {
   fun getAllPremises(): List<PremisesEntity> = premisesRepository.findAll()
   fun getPremises(premisesId: UUID): PremisesEntity? = premisesRepository.findByIdOrNull(premisesId)
   fun createLostBeds(lostBed: LostBedsEntity): LostBedsEntity = lostBedsRepository.save(lostBed)
+
+  fun getAvailabilityForRange(premises: PremisesEntity, startDate: LocalDate, endDate: LocalDate): Map<LocalDate, Availability> {
+    if (endDate.isBefore(startDate)) throw RuntimeException("startDate must be before endDate when calculating availability for range")
+
+    val bookings = bookingRepository.findAllByPremisesIdAndOverlappingDate(premises.id, startDate, endDate)
+    val lostBeds = lostBedsRepository.findAllByPremisesIdAndOverlappingDate(premises.id, startDate, endDate)
+
+    return startDate.getDaysUntilExclusiveEnd(endDate).map { date ->
+      val bookingsOnDay = bookings.filter { booking -> booking.arrivalDate <= date && booking.departureDate >= date }
+      val lostBedsOnDay = lostBeds.filter { lostBed -> lostBed.startDate <= date && lostBed.endDate >= date }
+
+      Availability(
+        date = date,
+        pendingBookings = bookingsOnDay.count { it.arrival == null && it.nonArrival == null && it.cancellation == null },
+        arrivedBookings = bookingsOnDay.count { it.arrival != null },
+        nonArrivedBookings = bookingsOnDay.count { it.nonArrival != null },
+        cancelledBookings = bookingsOnDay.count { it.cancellation != null },
+        lostBeds = lostBedsOnDay.sumOf { it.numberOfBeds }
+      )
+    }.associateBy { it.date }
+  }
 }
