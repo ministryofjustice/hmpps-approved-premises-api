e84ccf0bec6e8a2faefefc010661e4e029623b3d
brownbearsoftware <brownbearsoftware@gmail.com>
2022-08-16 14:21:16 +0100

    Change PersistedFactory so that passing a repository is optional.
    Calling produceAndPersist() when no repository provided throws an exception with advice in it.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PersistedFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PersistedFactory.kt
@@ -3,7 +3,9 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 import io.github.bluegroundltd.kfactory.Factory
 import org.springframework.data.jpa.repository.JpaRepository
 
-abstract class PersistedFactory<EntityType : Any, PrimaryKeyType : Any>(private val repository: JpaRepository<EntityType, PrimaryKeyType>) : Factory<EntityType> {
-  fun produceAndPersist(): EntityType = repository.saveAndFlush(this.produce())
+abstract class PersistedFactory<EntityType : Any, PrimaryKeyType : Any>(private val repository: JpaRepository<EntityType, PrimaryKeyType>?) : Factory<EntityType> {
+  constructor() : this(null)
+
+  fun produceAndPersist(): EntityType = repository?.saveAndFlush(this.produce()) ?: throw RuntimeException("Cannot persist because no repository was provided.  If this is a unit test did you mean to call .produce() instead?")
   fun produceAndPersistMultiple(amount: Int) = (1..amount).map { produceAndPersist() }
 }
