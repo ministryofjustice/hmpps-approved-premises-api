fd4cab459ce4c264e4a53cd84665999e3208ddc1
pezholio <pezholio@gmail.com>
2022-08-16 11:26:48 +0100

    Add a Cancellation Reasons reference endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ReferenceDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ReferenceDataController.kt
@@ -3,12 +3,15 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.ReferenceDataApiDelegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.health.api.model.CancellationReason
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.health.api.model.DepartureReason
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.health.api.model.DestinationProvider
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.health.api.model.MoveOnCategory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.CancellationReasonRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DepartureReasonRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DestinationProviderRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.MoveOnCategoryRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.CancellationReasonTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.DepartureReasonTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.DestinationProviderTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.MoveOnCategoryTransformer
@@ -18,9 +21,11 @@ class ReferenceDataController(
   private val departureReasonRepository: DepartureReasonRepository,
   private val moveOnCategoryRepository: MoveOnCategoryRepository,
   private val destinationProviderRepository: DestinationProviderRepository,
+  private val cancellationReasonRepository: CancellationReasonRepository,
   private val departureReasonTransformer: DepartureReasonTransformer,
   private val moveOnCategoryTransformer: MoveOnCategoryTransformer,
-  private val destinationProviderTransformer: DestinationProviderTransformer
+  private val destinationProviderTransformer: DestinationProviderTransformer,
+  private val cancellationReasonTransformer: CancellationReasonTransformer
 ) : ReferenceDataApiDelegate {
   override fun referenceDataDepartureReasonsGet(): ResponseEntity<List<DepartureReason>> {
     val reasons = departureReasonRepository.findAll()
@@ -38,5 +43,11 @@ class ReferenceDataController(
     val destinationProviders = destinationProviderRepository.findAll()
 
     return ResponseEntity.ok(destinationProviders.map(destinationProviderTransformer::transformJpaToApi))
+  }
+
+  override fun referenceDataCancellationReasonsGet(): ResponseEntity<List<CancellationReason>> {
+    val cancellationReasons = cancellationReasonRepository.findAll()
+
+    return ResponseEntity.ok(cancellationReasons.map(cancellationReasonTransformer::transformJpaToApi))
   }
 }
