2fec49bab236a142f6b7a3706acb72f6276ddd18
brownbearsoftware <brownbearsoftware@gmail.com>
2022-09-09 12:05:52 +0100

    Add prisons-api webclient config + client

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/PrisonsApiClient.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.client
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component
+class PrisonsApiClient(
+  @Qualifier("prisonsApiWebClient") webClient: WebClient,
+  objectMapper: ObjectMapper
+) : BaseHMPPSClient(webClient, objectMapper)
