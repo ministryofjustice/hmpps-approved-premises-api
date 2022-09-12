c1297fb75d33aab5326acb108889c4d3427908a7
brownbearsoftware <brownbearsoftware@gmail.com>
2022-09-12 11:56:06 +0100

    Add method to get inmate details from Prisons API
    Plus model to deserialize it into

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/PrisonsApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/PrisonsApiClient.kt
@@ -4,9 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateDetail
 
 @Component
 class PrisonsApiClient(
   @Qualifier("prisonsApiWebClient") webClient: WebClient,
   objectMapper: ObjectMapper
-) : BaseHMPPSClient(webClient, objectMapper)
+) : BaseHMPPSClient(webClient, objectMapper) {
+  fun getInmateDetails(nomsNumber: String) = getRequest<InmateDetail> {
+    path = "/api/offenders/$nomsNumber"
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/prisonsapi/InmateDetail.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi
+
+data class InmateDetail(
+  val offenderNo: String,
+  val inOutStatus: InOutStatus,
+  val assignedLivingUnit: AssignedLivingUnit?
+)
+
+data class AssignedLivingUnit(
+  val agencyId: String,
+  val locationId: Long,
+  val description: String?,
+  val agencyName: String
+)
+
+enum class InOutStatus {
+  IN,
+  OUT,
+  TRN
+}
