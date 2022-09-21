d0b08014603a0e7d6ac3b9c7e9bb30a0a4d385ee
brownbearsoftware <brownbearsoftware@gmail.com>
2022-09-21 16:29:49 +0100

    Upgrade schema version on applications where possible.
    Validate any applications created with older schema versions against the newest schema, promoting them to the newest version if validation passes.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/ApplicationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/ApplicationTest.kt
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.reactive.server.returnResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Application
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.ApplicationsTransformer
+import java.time.OffsetDateTime
 
 class ApplicationTest : IntegrationTestBase() {
   @Autowired
@@ -24,20 +25,65 @@ class ApplicationTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `Get all applications returns 200 with correct body`() {
+  fun `Get all applications returns 200 with correct body, outdated applications upgraded where possible`() {
     applicationSchemaRepository.deleteAll()
 
-    val schemaEntity = applicationSchemaEntityFactory.produceAndPersist()
-    val probationOfficerEntity = probationOfficerEntityFactory.produceAndPersist { withDistinguishedName("PROBATIONPERSON") }
-
-    val applicationEntities = applicationEntityFactory.produceAndPersistMultiple(5) {
-      withApplicationSchema(schemaEntity)
-      withCreatedByProbationOfficer(probationOfficerEntity)
+    val newestJsonSchema = applicationSchemaEntityFactory.produceAndPersist {
+      withAddedAt(OffsetDateTime.parse("2022-09-21T12:45:00+01:00"))
+      withSchema(
+        """
+        {
+          "${"\$schema"}": "https://json-schema.org/draft/2020-12/schema",
+          "${"\$id"}": "https://example.com/product.schema.json",
+          "title": "Thing",
+          "description": "A thing",
+          "type": "object",
+          "properties": {
+            "thingId": {
+              "description": "The unique identifier for a thing",
+              "type": "integer"
+            }
+          },
+          "required": [ "thingId" ]
+        }
+        """
+      )
     }
 
-    applicationEntities.forEach {
-      // Temporary until schema validation is in place
-      it.schemaUpToDate = true
+    val olderJsonSchema = applicationSchemaEntityFactory.produceAndPersist {
+      withAddedAt(OffsetDateTime.parse("2022-09-21T09:45:00+01:00"))
+      withSchema(
+        """
+        {
+          "${"\$schema"}": "https://json-schema.org/draft/2020-12/schema",
+          "${"\$id"}": "https://example.com/product.schema.json",
+          "title": "Thing",
+          "description": "A thing",
+          "type": "object",
+          "properties": { }
+        }
+        """
+      )
+    }
+
+    val probationOfficerEntity = probationOfficerEntityFactory.produceAndPersist { withDistinguishedName("PROBATIONPERSON") }
+
+    val upgradableApplicationEntity = applicationEntityFactory.produceAndPersist {
+      withApplicationSchema(olderJsonSchema)
+      withCreatedByProbationOfficer(probationOfficerEntity)
+      withData(
+        """
+          {
+             "thingId": 123
+          }
+          """
+      )
+    }
+
+    val nonUpgradableApplicationEntity = applicationEntityFactory.produceAndPersist {
+      withApplicationSchema(olderJsonSchema)
+      withCreatedByProbationOfficer(probationOfficerEntity)
+      withData("{}")
     }
 
     val jwt = jwtAuthHelper.createValidAuthorizationCodeJwt("PROBATIONPERSON")
@@ -54,19 +100,33 @@ class ApplicationTest : IntegrationTestBase() {
 
     val responseBody = objectMapper.readValue(rawResponseBody, object : TypeReference<List<Application>>() {})
 
-    applicationEntities.map(applicationsTransformer::transformJpaToApi).forEach { expectedApplication ->
-      assertThat(responseBody).anyMatch {
-        expectedApplication.id == it.id &&
-          expectedApplication.crn == it.crn &&
-          expectedApplication.createdAt.toInstant() == it.createdAt.toInstant() &&
-          expectedApplication.createdByProbationOfficerId == it.createdByProbationOfficerId &&
-          expectedApplication.submittedAt?.toInstant() == it.submittedAt?.toInstant() &&
-          serializableToJsonNode(expectedApplication.data) == serializableToJsonNode(it.data) &&
-          expectedApplication.schemaVersion == it.schemaVersion &&
-          expectedApplication.outdatedSchema == it.outdatedSchema
-      }
+    assertThat(responseBody).anyMatch {
+      nonUpgradableApplicationEntity.id == it.id &&
+        nonUpgradableApplicationEntity.crn == it.crn &&
+        nonUpgradableApplicationEntity.createdAt.toInstant() == it.createdAt.toInstant() &&
+        nonUpgradableApplicationEntity.createdByProbationOfficer.id == it.createdByProbationOfficerId &&
+        nonUpgradableApplicationEntity.submittedAt?.toInstant() == it.submittedAt?.toInstant() &&
+        serializableToJsonNode(nonUpgradableApplicationEntity.data) == serializableToJsonNode(it.data) &&
+        olderJsonSchema.id == it.schemaVersion &&
+        it.outdatedSchema == true
+    }
+
+    assertThat(responseBody).anyMatch {
+      upgradableApplicationEntity.id == it.id &&
+        upgradableApplicationEntity.crn == it.crn &&
+        upgradableApplicationEntity.createdAt.toInstant() == it.createdAt.toInstant() &&
+        upgradableApplicationEntity.createdByProbationOfficer.id == it.createdByProbationOfficerId &&
+        upgradableApplicationEntity.submittedAt?.toInstant() == it.submittedAt?.toInstant() &&
+        serializableToJsonNode(upgradableApplicationEntity.data) == serializableToJsonNode(it.data) &&
+        newestJsonSchema.id == it.schemaVersion &&
+        it.outdatedSchema == false
     }
   }
 
-  private fun serializableToJsonNode(serializable: Any?): JsonNode = if (serializable == null) NullNode.instance else objectMapper.readTree(objectMapper.writeValueAsString(serializable))
+  private fun serializableToJsonNode(serializable: Any?): JsonNode {
+    if (serializable == null) return NullNode.instance
+    if (serializable is String) return objectMapper.readTree(serializable)
+
+    return objectMapper.readTree(objectMapper.writeValueAsString(serializable))
+  }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/JsonSchemaServiceTest.kt
@@ -0,0 +1,108 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.service
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ApplicationEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ApplicationSchemaEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ProbationOfficerEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationSchemaRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.JsonSchemaService
+import java.util.UUID
+
+class JsonSchemaServiceTest {
+  private val mockApplicationSchemaRepository = mockk<ApplicationSchemaRepository>()
+  private val mockApplicationRepository = mockk<ApplicationRepository>()
+
+  private val jsonSchemaService = JsonSchemaService(
+    objectMapper = jacksonObjectMapper(),
+    applicationSchemaRepository = mockApplicationSchemaRepository,
+    applicationRepository = mockApplicationRepository
+  )
+
+  @Test
+  fun `attemptSchemaUpgrade upgrades schema version where possible, marks outdated where not`() {
+    val newestJsonSchema = ApplicationSchemaEntityFactory()
+      .withSchema(
+        """
+        {
+          "${"\$schema"}": "https://json-schema.org/draft/2020-12/schema",
+          "${"\$id"}": "https://example.com/product.schema.json",
+          "title": "Thing",
+          "description": "A thing",
+          "type": "object",
+          "properties": {
+            "thingId": {
+              "description": "The unique identifier for a thing",
+              "type": "integer"
+            }
+          },
+          "required": [ "thingId" ]
+        }
+        """
+      )
+      .produce()
+
+    val olderJsonSchema = ApplicationSchemaEntityFactory()
+      .withSchema(
+        """
+        {
+          "${"\$schema"}": "https://json-schema.org/draft/2020-12/schema",
+          "${"\$id"}": "https://example.com/product.schema.json",
+          "title": "Thing",
+          "description": "A thing",
+          "type": "object",
+          "properties": { }
+        }
+        """
+      )
+      .produce()
+
+    val probationOfficerId = UUID.fromString("8a0624b8-8e92-47ce-b645-b65ea5a197d0")
+    val distinguishedName = "SOMEPERSON"
+    val probationOfficerEntity = ProbationOfficerEntityFactory()
+      .withId(probationOfficerId)
+      .withDistinguishedName(distinguishedName)
+      .produce()
+
+    val upgradableApplication = ApplicationEntityFactory()
+      .withCreatedByProbationOfficer(probationOfficerEntity)
+      .withApplicationSchema(olderJsonSchema)
+      .withData(
+        """
+          {
+             "thingId": 123
+          }
+          """
+      )
+      .produce()
+
+    val nonUpgradableApplication = ApplicationEntityFactory()
+      .withCreatedByProbationOfficer(probationOfficerEntity)
+      .withApplicationSchema(olderJsonSchema)
+      .withData("{}")
+      .produce()
+
+    val applicationEntities = listOf(upgradableApplication, nonUpgradableApplication)
+
+    every { mockApplicationSchemaRepository.findFirstByOrderByAddedAtDesc() } returns newestJsonSchema
+    every { mockApplicationRepository.findAllByCreatedByProbationOfficer_Id(probationOfficerId) } returns applicationEntities
+    every { mockApplicationRepository.save(any()) } answers { it.invocation.args[0] as ApplicationEntity }
+
+    assertThat(jsonSchemaService.attemptSchemaUpgrade(upgradableApplication)).matches {
+      it.id == upgradableApplication.id &&
+        it.schemaVersion == newestJsonSchema &&
+        it.schemaUpToDate
+    }
+
+    assertThat(jsonSchemaService.attemptSchemaUpgrade(nonUpgradableApplication)).matches {
+      it.id == nonUpgradableApplication.id &&
+        it.schemaVersion == olderJsonSchema &&
+        !it.schemaUpToDate
+    }
+  }
+}
