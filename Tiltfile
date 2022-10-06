fe25c250b2f8a6a56702cc3be070ed8f2596cf47
brownbearsoftware <brownbearsoftware@gmail.com>
2022-10-06 10:13:01 +0100

    Add Tilt for running UI and API together

---
+++ Tiltfile
@@ -0,0 +1,3 @@
+docker_compose('./docker-compose.yml')
+config.clear_enabled_resources()
+config.set_enabled_resources(['database', 'hmpps-auth', 'redis', 'community-api'])
---
+++ seed/community-api/V99_1__offenders.sql
@@ -0,0 +1,2 @@
+-- Set the NOMS Number for Aadland Danger Bertrand to one that exists in the Prison API seed data
+UPDATE OFFENDER SET NOMS_NUMBER = 'A1234AI' WHERE CRN = 'X320741';
