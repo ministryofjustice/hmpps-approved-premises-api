957322528b855a470eaed42baf1c59bdadfbb0c5
brownbearsoftware <brownbearsoftware@gmail.com>
2022-10-10 11:08:35 +0100

    Fix application placeholder schema

---
+++ src/main/resources/db/migration/all/20221010105404__fix_placeholder_application_schema.sql
@@ -0,0 +1,9 @@
+UPDATE application_schemas SET "schema" = '{
+ "$schema": "https://json-schema.org/draft/2020-12/schema",
+ "title": "Application Placeholder Schema",
+ "description": "An application schema that requires no properties",
+ "type": "object",
+ "properties": {
+ },
+ "required": []
+}' WHERE id = 'f96725f6-27ac-46f2-83e0-00cf4af48370';
