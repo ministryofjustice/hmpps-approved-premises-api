761bfb8404235ec30055aae44e302b65e2ba2945
brownbearsoftware <brownbearsoftware@gmail.com>
2022-10-14 10:47:23 +0100

    Remove ShouldNotBeReached.kt
    No longer needed as mockk have released a fix for being unable to mock sealed interfaces.  This means we can rely on the compiler to ensure all branches are covered in where statements again.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/ShouldNotBeReached.kt
+++
@@ -1,4 +0,0 @@
-package uk.gov.justice.digital.hmpps.approvedpremisesapi.model
-
-// Mocking sealed interfaces is currently broken in mockk, so an else branch is need until this is resolved: https://github.com/mockk/mockk/issues/832
-fun shouldNotBeReached(): Nothing = throw RuntimeException("This branch should not be reached")
