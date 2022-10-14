87205ea7b3b087171e54a30ed9be491ae41e26ec
brownbearsoftware <brownbearsoftware@gmail.com>
2022-10-14 12:14:20 +0100

    Add JPA entities for assessments

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/AssessmentEntity.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.hibernate.annotations.Type
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.time.OffsetDateTime
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToOne
+import javax.persistence.Table
+
+@Repository
+interface AssessmentRepository : JpaRepository<AssessmentEntity, UUID>
+
+@Entity
+@Table(name = "assessments")
+data class AssessmentEntity(
+  @Id
+  val id: UUID,
+
+  @OneToOne
+  @JoinColumn(name = "application_id")
+  val application: ApplicationEntity,
+
+  @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+  var data: String?,
+
+  @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+  var document: String?,
+
+  @ManyToOne
+  @JoinColumn(name = "schema_version")
+  var schemaVersion: JsonSchemaEntity,
+
+  @ManyToOne
+  @JoinColumn(name = "allocated_to_probation_officer_id")
+  val allocatedToProbationOfficerId: ProbationOfficerEntity,
+
+  val allocatedAt: OffsetDateTime,
+
+  val createdAt: OffsetDateTime,
+  var submittedAt: OffsetDateTime?,
+
+  @Transient
+  var schemaUpToDate: Boolean
+)
