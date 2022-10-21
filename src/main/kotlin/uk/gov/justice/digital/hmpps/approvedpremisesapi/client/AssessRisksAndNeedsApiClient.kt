cac070aeee68ef50f0ad4aa9e3803da97c99511e
brownbearsoftware <brownbearsoftware@gmail.com>
2022-10-21 12:23:36 +0100

    Add get ARNS needs client method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/AssessRisksAndNeedsApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/AssessRisksAndNeedsApiClient.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.assessrisksandneeds.Needs
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.assessrisksandneeds.RoshRisks
 
 @Component
@@ -14,5 +15,10 @@ class AssessRisksAndNeedsApiClient(
   fun getRoshRisks(crn: String, jwt: String) = getRequest<RoshRisks> {
     withHeader("Authorization", "Bearer $jwt")
     path = "/risks/crn/$crn"
+  }
+
+  fun getNeeds(crn: String, jwt: String) = getRequest<Needs> {
+    withHeader("Authorization", "Bearer $jwt")
+    path = "/needs/crn/$crn"
   }
 }
