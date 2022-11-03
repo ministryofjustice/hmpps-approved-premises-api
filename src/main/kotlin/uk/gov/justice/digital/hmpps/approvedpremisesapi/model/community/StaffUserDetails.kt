89dce61ba67b94cbd956145d2d97577468ef63a8
brownbearsoftware <brownbearsoftware@gmail.com>
2022-11-03 12:03:32 +0000

    Fix for StaffUserDetails deserialization issue
    telephoneNumber is actually nullable

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/community/StaffUserDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/community/StaffUserDetails.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community
 data class StaffUserDetails(
   val username: String,
   val email: String,
-  val telephoneNumber: String,
+  val telephoneNumber: String?,
   val staffCode: String,
   val staffIdentifier: Long,
   val staff: StaffNames
