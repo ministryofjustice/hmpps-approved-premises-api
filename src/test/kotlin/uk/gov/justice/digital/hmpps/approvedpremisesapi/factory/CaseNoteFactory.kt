4a3507aac56c0d7296cd8103483af97df67cdcef
brownbearsoftware <brownbearsoftware@gmail.com>
2022-11-08 14:14:40 +0000

    Update CaseNotes model to format returned by Case Notes Service

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/CaseNoteFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/CaseNoteFactory.kt
@@ -9,27 +9,37 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringUpperCa
 import java.time.LocalDateTime
 
 class CaseNoteFactory : Factory<CaseNote> {
-  private var caseNoteId: Yielded<Long> = { randomInt(0, 1000).toLong() }
-  private var bookingId: Yielded<Long> = { randomInt(0, 1000).toLong() }
-  private var type: Yielded<String> = { "ACP" }
-  private var typeDescription: Yielded<String?> = { "Accredited Programme" }
-  private var subType: Yielded<String> = { "ASSESSMENT" }
-  private var subTypeDescription: Yielded<String?> = { "Assessment" }
+  private var caseNoteId: Yielded<String> = { randomStringUpperCase(6) }
+  private var offenderIdentifier: Yielded<String> = { randomStringUpperCase(8) }
+  private var sensitive: Yielded<Boolean> = { false }
+  private var type: Yielded<String> = { if (this.sensitive()) "OMIC" else "ACP" }
+  private var typeDescription: Yielded<String?> = { if (this.sensitive()) "OMiC" else "Accredited Programme" }
+  private var subType: Yielded<String> = { if (this.sensitive()) "COMM" else "ASSESSMENT" }
+  private var subTypeDescription: Yielded<String?> = { if (this.sensitive()) "Communication" else "Assessment" }
   private var source: Yielded<String?> = { null }
   private var creationDateTime: Yielded<LocalDateTime> = { LocalDateTime.now().randomDateTimeBefore(50) }
   private var occurrenceDateTime: Yielded<LocalDateTime> = { LocalDateTime.now().randomDateTimeBefore(50) }
-  private var staffId: Yielded<Long> = { randomInt(0, 1000).toLong() }
+  private var authorUserId: Yielded<String> = { randomInt(0, 1000).toLong().toString() }
   private var authorName: Yielded<String> = { randomStringUpperCase(10) }
   private var text: Yielded<String> = { randomStringUpperCase(20) }
   private var originalTextNote: Yielded<String> = { randomStringUpperCase(20) }
-  private var agencyId: Yielded<String?> = { randomStringUpperCase(5) }
+  private var eventId: Yielded<Int> = { randomInt(0, 1000) }
+  private var locationId: Yielded<String?> = { null }
 
-  fun withCaseNoteId(caseNoteId: Long) = apply {
+  fun withCaseNoteId(caseNoteId: String) = apply {
     this.caseNoteId = { caseNoteId }
   }
 
-  fun withBookingId(bookingId: Long) = apply {
-    this.bookingId = { bookingId }
+  fun withOffenderIdentifier(offenderIdentifier: String) = apply {
+    this.offenderIdentifier = { offenderIdentifier }
+  }
+
+  fun withSensitive(sensitive: Boolean) = apply {
+    this.sensitive = { sensitive }
+  }
+
+  fun withEventId(eventId: Int) = apply {
+    this.eventId = { eventId }
   }
 
   fun withType(type: String) = apply {
@@ -60,8 +70,8 @@ class CaseNoteFactory : Factory<CaseNote> {
     this.occurrenceDateTime = { occurrenceDateTime }
   }
 
-  fun withStaffId(staffId: Long) = apply {
-    this.staffId = { staffId }
+  fun withAuthorUserId(authorUserId: String) = apply {
+    this.authorUserId = { authorUserId }
   }
 
   fun withAuthorName(authorName: String) = apply {
@@ -76,13 +86,12 @@ class CaseNoteFactory : Factory<CaseNote> {
     this.originalTextNote = { originalNoteText }
   }
 
-  fun withAgencyId(agencyId: String?) = apply {
-    this.agencyId = { agencyId }
+  fun withLocationId(locationId: String?) = apply {
+    this.locationId = { locationId }
   }
 
   override fun produce(): CaseNote = CaseNote(
     caseNoteId = this.caseNoteId(),
-    bookingId = this.bookingId(),
     type = this.type(),
     typeDescription = this.typeDescription(),
     subType = this.subType(),
@@ -90,11 +99,14 @@ class CaseNoteFactory : Factory<CaseNote> {
     source = this.source(),
     creationDateTime = this.creationDateTime(),
     occurrenceDateTime = this.occurrenceDateTime(),
-    staffId = this.staffId(),
+    authorUserId = this.authorUserId(),
     authorName = this.authorName(),
     text = this.text(),
     originalNoteText = this.originalTextNote(),
-    agencyId = this.agencyId(),
-    amendments = listOf()
+    locationId = this.locationId(),
+    amendments = listOf(),
+    offenderIdentifier = this.offenderIdentifier(),
+    eventId = this.eventId(),
+    sensitive = this.sensitive()
   )
 }
