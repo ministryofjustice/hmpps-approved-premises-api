dc37ff8321dbeb04d8eb68eca9acb0c92e4c0557
brownbearsoftware <brownbearsoftware@gmail.com>
2022-11-21 14:40:16 +0000

    Add base class for OASys transformers
    To hold common helpers methods such as `oASysQuestionWithSingleAnswer`

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/OASysTransformer.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
+
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.OASysQuestion
+
+abstract class OASysTransformer {
+  protected fun oASysQuestionWithSingleAnswer(label: String, questionNumber: String, answer: String) = OASysQuestion(
+    label = label,
+    questionNumber = questionNumber,
+    answers = listOf(answer)
+  )
+}
