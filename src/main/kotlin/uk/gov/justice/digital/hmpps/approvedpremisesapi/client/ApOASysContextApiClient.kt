d844887ec318b616784e6a23cdb71855e701221a
brownbearsoftware <brownbearsoftware@gmail.com>
2022-11-22 12:45:56 +0000

    Add client method to get risk to individual from OASys Context

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/ApOASysContextApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/client/ApOASysContextApiClient.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.OffenceDetails
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RiskManagementPlan
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RisksToTheIndividual
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RoshSummary
 
 @Component
@@ -23,5 +24,9 @@ class ApOASysContextApiClient(
 
   fun getRoshSummary(crn: String) = getRequest<RoshSummary> {
     path = "/rosh-summary/$crn"
+  }
+
+  fun getRiskToTheIndividual(crn: String) = getRequest<RisksToTheIndividual> {
+    path = "/risk-to-the-individual/$crn"
   }
 }
