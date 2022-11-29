5e9beab99afa2917593fdbcdc02f8beaf85f7042
Dan Livings <dan.livings@dxw.com>
2022-11-29 10:45:03 +0000

    Update JPA entities for departure reasons and move-on categories

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/MoveOnCategoryEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/MoveOnCategoryEntity.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.UUID
 import javax.persistence.Entity
@@ -8,7 +9,10 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 @Repository
-interface MoveOnCategoryRepository : JpaRepository<MoveOnCategoryEntity, UUID>
+interface MoveOnCategoryRepository : JpaRepository<MoveOnCategoryEntity, UUID> {
+  @Query("SELECT m FROM MoveOnCategoryEntity m WHERE m.serviceScope = :serviceName OR m.serviceScope = '*'")
+  fun findAllByServiceScope(serviceName: String): List<MoveOnCategoryEntity>
+}
 
 @Entity
 @Table(name = "move_on_categories")
@@ -16,7 +20,8 @@ data class MoveOnCategoryEntity(
   @Id
   val id: UUID,
   val name: String,
-  val isActive: Boolean
+  val isActive: Boolean,
+  val serviceScope: String,
 ) {
   override fun toString() = "MoveOnCategoryEntity:$id"
 }
