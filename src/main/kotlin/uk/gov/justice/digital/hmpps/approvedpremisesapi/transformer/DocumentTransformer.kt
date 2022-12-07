9b18386b1bf8d34d9948466d3d704490c0b600bb
brownbearsoftware <brownbearsoftware@gmail.com>
2022-12-07 11:50:12 +0000

    Add Documents Transformer

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/DocumentTransformer.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Document
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.DocumentLevel
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.GroupedDocuments
+import java.time.ZoneOffset
+
+@Component
+class DocumentTransformer {
+  fun transformToApi(groupedDocuments: GroupedDocuments, convictionId: Long): List<Document> {
+    val offenderDocuments = groupedDocuments.documents.map {
+      Document(
+        id = it.id,
+        level = DocumentLevel.offender,
+        fileName = it.documentName,
+        createdAt = it.createdAt.atOffset(ZoneOffset.UTC),
+        typeCode = it.type.code,
+        typeDescription = it.type.description,
+        description = it.extendedDescription
+      )
+    }
+
+    val convictionDocuments = groupedDocuments.convictions
+      .firstOrNull { it.convictionId == convictionId.toString() }
+      ?.documents
+      ?.map {
+        Document(
+          id = it.id,
+          level = DocumentLevel.conviction,
+          fileName = it.documentName,
+          createdAt = it.createdAt.atOffset(ZoneOffset.UTC),
+          typeCode = it.type.code,
+          typeDescription = it.type.description,
+          description = it.extendedDescription
+        )
+      } ?: emptyList()
+
+    return offenderDocuments + convictionDocuments
+  }
+}
