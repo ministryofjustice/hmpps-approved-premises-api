0d2594f3a759e6f3dad9ef7e758c5b1be21aab3c
brownbearsoftware <brownbearsoftware@gmail.com>
2022-12-12 17:32:37 +0000

    Persist Risk Ratings when creating an Application

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/PersonRisks.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/PersonRisks.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.model
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.time.LocalDate
+import javax.persistence.AttributeConverter
+import javax.persistence.Converter
 
 data class RiskWithStatus<T>(val status: RiskStatus, val value: T? = null) {
   constructor(value: T?) : this(RiskStatus.Retrieved, value)
@@ -37,3 +40,24 @@ data class RiskTier(
   val level: String,
   val lastUpdated: LocalDate
 )
+
+@Converter(autoApply = true)
+class PersonRisksConverter : AttributeConverter<PersonRisks, String?> {
+  private val objectMapper = jacksonObjectMapper()
+
+  override fun convertToDatabaseColumn(domainForm: PersonRisks?): String? {
+    return try {
+      objectMapper.writeValueAsString(domainForm)
+    } catch (exception: Exception) {
+      throw RuntimeException("Unable to serialize PersonRisks to JSON string for database", exception)
+    }
+  }
+
+  override fun convertToEntityAttribute(dbData: String?): PersonRisks? {
+    return try {
+      objectMapper.readValue(dbData, PersonRisks::class.java)
+    } catch (exception: Exception) {
+      throw RuntimeException("Unable to deserialize PersonRisks from JSON string", exception)
+    }
+  }
+}
