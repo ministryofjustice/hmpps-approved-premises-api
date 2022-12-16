9bfdd5863b1da48806a64ba0fba521ca12ca4bb2
brownbearsoftware <brownbearsoftware@gmail.com>
2022-12-16 12:05:43 +0000

    Accept Seed requests and start on background thread
    When a POST to /seed is made, take the request and start it on a background thread via Spring's @Asnyc support.  Return a 202 to indicate the task has been accepted.
    
    Only allow connections from localhost/127.0.0.1 so that the endpoint cannot be called from outside of the container.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/SeedTest.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
+
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SeedFileType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SeedRequest
+
+class SeedTest : IntegrationTestBase() {
+  @Test
+  fun `Requesting a Seed operation returns 202`() {
+    webTestClient.post()
+      .uri("/seed")
+      .bodyValue(
+        SeedRequest(
+          seedType = SeedFileType.premises,
+          fileName = "file.csv"
+        )
+      )
+      .exchange()
+      .expectStatus()
+      .isAccepted
+  }
+}
