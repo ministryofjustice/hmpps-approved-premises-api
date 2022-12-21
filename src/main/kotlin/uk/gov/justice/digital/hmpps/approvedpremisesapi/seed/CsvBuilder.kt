5960b518ae086f86621a7d1b7472b5aa6fe3b3f0
brownbearsoftware <brownbearsoftware@gmail.com>
2022-12-21 15:33:45 +0000

    Add CsvBuilder
    This will be used to build CSV strings in tests

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/CsvBuilder.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.seed
+
+class CsvBuilder {
+  private var csv = StringBuilder()
+
+  fun withUnquotedField(field: Any) = apply {
+    csv.append("$field,")
+  }
+
+  fun withHeader(vararg fields: Any) = withUnquotedFields(fields)
+  fun withUnquotedFields(vararg fields: Any) = apply {
+    fields.forEach {
+      csv.append("$it,")
+    }
+  }
+
+  fun withQuotedField(field: Any) = apply {
+    csv.append("\"$field\",")
+  }
+
+  fun withQuotedFields(vararg fields: Any) = apply {
+    fields.forEach {
+      csv.append("\"$it\",")
+    }
+  }
+
+  fun newRow() = apply {
+    csv.deleteCharAt(csv.length - 1)
+    csv.append("\n")
+  }
+
+  fun build() = csv.toString()
+}
