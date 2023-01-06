5143c30add330ad6f9cc0c3d899cefbb83b4f0d7
brownbearsoftware <brownbearsoftware@gmail.com>
2023-01-06 09:38:44 +0000

    Add teams property to existing response model for staff user endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/community/StaffUserDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/community/StaffUserDetails.kt
@@ -1,15 +1,36 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community
 
+import java.time.LocalDate
+
 data class StaffUserDetails(
   val username: String,
   val email: String,
   val telephoneNumber: String?,
   val staffCode: String,
   val staffIdentifier: Long,
-  val staff: StaffNames
+  val staff: StaffNames,
+  val teams: List<StaffUserTeamMembership>
 )
 
 data class StaffNames(
   val forenames: String,
   val surname: String
+)
+
+data class StaffUserTeamMembership(
+  val code: String,
+  val description: String,
+  val telephone: String?,
+  val emailAddress: String?,
+  val localDeliveryUnit: KeyValue,
+  val teamType: KeyValue,
+  val district: KeyValue,
+  val borough: KeyValue,
+  val startDate: LocalDate,
+  val endDate: LocalDate?
+)
+
+data class KeyValue(
+  val code: String,
+  val description: String
 )
