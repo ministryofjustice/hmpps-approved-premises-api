fa41da4d66187d4f32988a48fa30d86d62dbd1fa
brownbearsoftware <brownbearsoftware@gmail.com>
2023-01-24 06:58:48 +0000

    Add JPA entity for Offline Applications

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/OfflineApplicationEntity.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.time.OffsetDateTime
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Repository
+interface OfflineApplicationRepository : JpaRepository<OfflineApplicationEntity, UUID>
+
+@Entity
+@Table(name = "offline_applications")
+data class OfflineApplicationEntity(
+  @Id
+  val id: UUID,
+  val crn: String,
+  val service: String,
+  val submittedAt: OffsetDateTime,
+  val createdAt: OffsetDateTime
+)
