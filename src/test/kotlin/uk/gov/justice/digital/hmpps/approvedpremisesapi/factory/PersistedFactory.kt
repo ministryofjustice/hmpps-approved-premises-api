26d0238e745a0560492fa1cfbb1a0883e44b3645
brownbearsoftware <brownbearsoftware@gmail.com>
2023-01-26 11:24:06 +0000

    Update PersistedFactory to use new factory instance per call
    This changes PersistedFactory to take a producer function for the factory rather than a single instance.  This allows us to use a fresh factory for each call to `produceAndPersist` which in turn prevents hard to debug issues caused by earlier calls to the factory setting values to literals (e.g. two Users being produced with the same deliusUsername because the first call explicitly sets it).

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PersistedFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PersistedFactory.kt
@@ -3,16 +3,20 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 import io.github.bluegroundltd.kfactory.Factory
 import org.springframework.data.jpa.repository.JpaRepository
 
-class PersistedFactory<EntityType : Any, PrimaryKeyType : Any, FactoryType : Factory<EntityType>>(private val factory: FactoryType, private val repository: JpaRepository<EntityType, PrimaryKeyType>) {
-  fun produceAndPersist(): EntityType = repository.saveAndFlush(factory.produce())
+class PersistedFactory<EntityType : Any, PrimaryKeyType : Any, FactoryType : Factory<EntityType>>(private val factoryProducer: () -> FactoryType, private val repository: JpaRepository<EntityType, PrimaryKeyType>) {
+  fun produceAndPersist(): EntityType = repository.saveAndFlush(factoryProducer().produce())
   fun produceAndPersist(configuration: FactoryType.() -> Unit): EntityType {
+    val factory = factoryProducer()
     configuration(factory)
     return repository.saveAndFlush(factory.produce())
   }
 
   fun produceAndPersistMultiple(amount: Int) = (1..amount).map { produceAndPersist() }
   fun produceAndPersistMultiple(amount: Int, configuration: FactoryType.() -> Unit): List<EntityType> {
+    val factory = factoryProducer()
     configuration(factory)
-    return (1..amount).map { produceAndPersist() }
+    return (1..amount).map {
+      repository.saveAndFlush(factory.produce())
+    }
   }
 }
