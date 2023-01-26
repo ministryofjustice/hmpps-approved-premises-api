63ff11a0d8e547ea0e486b2998bb56c9b46ff26d
pezholio <pezholio@gmail.com>
2023-01-26 08:40:52 +0000

    Add an optional `EntityType` argument to results
    This will allow us to handle multiple failures, while still being able
    to maintain the contract that other service methods have (i.e. returning
    an `AuthorisableActionResult`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/results/AuthorisableActionResult.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/results/AuthorisableActionResult.kt
@@ -2,6 +2,6 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.results
 
 sealed interface AuthorisableActionResult<EntityType> {
   data class Success<EntityType>(val entity: EntityType) : AuthorisableActionResult<EntityType>
-  class Unauthorised<EntityType> : AuthorisableActionResult<EntityType>
-  class NotFound<EntityType> : AuthorisableActionResult<EntityType>
+  data class Unauthorised<EntityType>(val id: Any? = null, val entityType: String? = null) : AuthorisableActionResult<EntityType>
+  data class NotFound<EntityType>(val id: Any? = null, val entityType: String? = null) : AuthorisableActionResult<EntityType>
 }
