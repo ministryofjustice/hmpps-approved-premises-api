bfb81f0aa2e554f7804c991eb8b86a7434d1480d
brownbearsoftware <brownbearsoftware@gmail.com>
2023-01-30 12:56:28 +0000

    Refactor `PersonOASysSectionsTest`
    Use `Given a User` and `Given an Offender`
    Move HTTP mock methods to per-upstream-service mock files

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/httpmocks/APOASysContext.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks
+
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.NeedsDetails
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.OffenceDetails
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RiskManagementPlan
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RisksToTheIndividual
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.oasyscontext.RoshSummary
+
+fun IntegrationTestBase.APOASysContext_mockSuccessfulOffenceDetailsCall(crn: String, response: OffenceDetails) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/offence-details/$crn",
+    responseBody = response
+  )
+
+fun IntegrationTestBase.APOASysContext_mockSuccessfulRoSHSummaryCall(crn: String, response: RoshSummary) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/rosh-summary/$crn",
+    responseBody = response
+  )
+
+fun IntegrationTestBase.APOASysContext_mockSuccessfulRiskToTheIndividualCall(crn: String, response: RisksToTheIndividual) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/risk-to-the-individual/$crn",
+    responseBody = response
+  )
+
+fun IntegrationTestBase.APOASysContext_mockSuccessfulRiskManagementPlanCall(crn: String, response: RiskManagementPlan) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/risk-management-plan/$crn",
+    responseBody = response
+  )
+
+fun IntegrationTestBase.APOASysContext_mockSuccessfulNeedsDetailsCall(crn: String, response: NeedsDetails) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/needs-details/$crn",
+    responseBody = response
+  )
