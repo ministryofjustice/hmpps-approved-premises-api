975680ba1e360216959449df365359a0aa82b793
brownbearsoftware <brownbearsoftware@gmail.com>
2023-01-30 12:36:08 +0000

    Refactor `PersonAcctAlertsTest`
    Use `Given a User` and `Given an Offender`
    Move HTTP mock methods to per-upstream-service mock files

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/httpmocks/PrisonAPI.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/httpmocks/PrisonAPI.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks
 
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.Alert
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateDetail
 
 fun IntegrationTestBase.PrisonAPI_mockSuccessfulInmateDetailsCall(inmateDetail: InmateDetail) =
@@ -13,4 +14,10 @@ fun IntegrationTestBase.PrisonAPI_mockNotFoundInmateDetailsCall(offenderNo: Stri
   mockUnsuccessfulGetCall(
     url = "/api/offenders/$offenderNo",
     responseStatus = 404
+  )
+
+fun IntegrationTestBase.PrisonAPI_mockSuccessfulAlertsCall(nomsNumber: String, alerts: List<Alert>) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/api/offenders/$nomsNumber/alerts/v2?alertCodes=HA&sort=dateCreated&direction=DESC",
+    responseBody = alerts
   )
