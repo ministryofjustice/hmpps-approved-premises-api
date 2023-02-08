11f597d6db04edca4bd50690fc1c6cdcdfc8b783
brownbearsoftware <brownbearsoftware@gmail.com>
2023-02-08 12:55:46 +0000

    Emit SNS events for Application Submitted events

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/domainevent/SnsEvent.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent
+
+import java.time.OffsetDateTime
+import java.util.UUID
+
+data class SnsEvent(
+  val eventType: String,
+  val version: Int,
+  val description: String,
+  val detailUrl: String,
+  val occurredAt: OffsetDateTime,
+  val additionalInformation: SnsEventAdditionalInformation,
+  val personReference: SnsEventPersonReferenceCollection
+)
+
+data class SnsEventPersonReferenceCollection(
+  val identifiers: List<SnsEventPersonReference>
+)
+
+data class SnsEventPersonReference(
+  val type: String,
+  val value: String
+)
+
+data class SnsEventAdditionalInformation(
+  val applicationId: UUID
+)
