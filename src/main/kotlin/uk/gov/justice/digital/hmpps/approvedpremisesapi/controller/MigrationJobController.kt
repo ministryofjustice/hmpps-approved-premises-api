03f58edc81d6dc1cc1b9d93eda096ae04afae7aa
brownbearsoftware <brownbearsoftware@gmail.com>
2023-02-20 12:25:12 +0000

    Move `throwIfNotLoopbackRequest` to top level function
    This is used across multiple Controllers so move from a member function to a top level function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/MigrationJobController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/MigrationJobController.kt
@@ -3,11 +3,8 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.MigrationJobApiDelegate
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.MigrationJobRequest
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.UnauthenticatedProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.MigrationJobService
 
 @Service
@@ -18,14 +15,5 @@ class MigrationJobController(private val migrationJobService: MigrationJobServic
     migrationJobService.runMigrationJob(migrationJobRequest.jobType)
 
     return ResponseEntity(HttpStatus.ACCEPTED)
-  }
-
-  private fun throwIfNotLoopbackRequest() {
-    val request = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
-    val remoteAddress = request.remoteAddr
-
-    if (! listOf("127.0.0.1", "localhost").contains(remoteAddress)) {
-      throw UnauthenticatedProblem("This endpoint can only be called locally, was requested from: $remoteAddress")
-    }
   }
 }
