0820d8750c0de741599e3b2a9ca3ada3d8d08fba
Dan Livings <dan.livings@dxw.com>
2023-02-21 15:37:40 +0000

    Make existing logic handle service-specific lost bed JPA entities

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/LostBedsTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/LostBedsTransformer.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.LostBed
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesLostBedsEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.LostBedsEntity
 
 @Component
@@ -10,7 +11,7 @@ class LostBedsTransformer(private val lostBedReasonTransformer: LostBedReasonTra
     id = jpa.id,
     startDate = jpa.startDate,
     endDate = jpa.endDate,
-    numberOfBeds = jpa.numberOfBeds,
+    numberOfBeds = if (jpa is ApprovedPremisesLostBedsEntity) jpa.numberOfBeds else 1,
     reason = lostBedReasonTransformer.transformJpaToApi(jpa.reason),
     referenceNumber = jpa.referenceNumber,
     notes = jpa.notes
