3437cb55f0c0403e6399586fb9eb92ef05b7904b
brownbearsoftware <brownbearsoftware@gmail.com>
2023-03-02 15:29:36 +0000

    Normalise to uppercase when fetching Users
    As usernames are always uppercase in Delius this prevents against other entrypoints for usernames (e.g. seeding) making entries that won't match on case

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/UserCaseSensitivityTest.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given a User`
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
+
+class UserCaseSensitivityTest : IntegrationTestBase() {
+  @Autowired
+  lateinit var userService: UserService
+
+  @Test
+  fun `Fetching a user with lowercase username returns the user with normalised uppercase username`() {
+    `Given a User` { userEntity, _ ->
+      val returnedUser = userService.getUserForUsername(userEntity.deliusUsername.lowercase())
+
+      assertThat(returnedUser.id).isEqualTo(userEntity.id)
+    }
+  }
+}
