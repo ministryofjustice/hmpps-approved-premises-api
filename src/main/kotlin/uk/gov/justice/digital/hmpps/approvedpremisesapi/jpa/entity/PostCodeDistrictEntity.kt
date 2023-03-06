0ccd9f3b71644ff16159d0f88d829494b7e75e1b
brownbearsoftware <brownbearsoftware@gmail.com>
2023-03-06 15:12:01 +0000

    Add PostGIS point to post code districts

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/PostCodeDistrictEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/PostCodeDistrictEntity.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.locationtech.jts.geom.Point
 import java.math.BigDecimal
 import java.util.UUID
 import javax.persistence.Entity
@@ -21,5 +22,6 @@ data class PostCodeDistrictEntity(
   val id: UUID,
   val outcode: String,
   val latitude: BigDecimal,
-  val longitude: BigDecimal
+  val longitude: BigDecimal,
+  val point: Point
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PostCodeDistrictEntityFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/PostCodeDistrictEntityFactory.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.Point
+import org.locationtech.jts.geom.PrecisionModel
 import io.github.bluegroundltd.kfactory.Factory
 import io.github.bluegroundltd.kfactory.Yielded
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PostCodeDistrictEntity
@@ -13,6 +16,7 @@ class PostCodeDistrictEntityFactory : Factory<PostCodeDistrictEntity> {
   private var outcode: Yielded<String> = { randomOf(listOf("SW18", "SY11", "YO51", "AB24")) }
   private var latitude: Yielded<BigDecimal> = { randomDouble(49.0, 59.0).toBigDecimal() }
   private var longitude: Yielded<BigDecimal> = { randomDouble(2.0, 8.0).toBigDecimal() }
+  private var point: Yielded<Point> = { Point(Coordinate(1.0, 2.0), PrecisionModel(PrecisionModel.Type("FLOATING")), 4326) }
 
   fun withId(id: UUID) = apply {
     this.id = { id }
@@ -26,10 +30,15 @@ class PostCodeDistrictEntityFactory : Factory<PostCodeDistrictEntity> {
     this.longitude = { longitude }
   }
 
+  fun withPoint(point: Point) = apply {
+    this.point = { point }
+  }
+
   override fun produce() = PostCodeDistrictEntity(
     id = this.id(),
     outcode = this.outcode(),
     latitude = this.latitude(),
-    longitude = this.longitude()
+    longitude = this.longitude(),
+    point = this.point()
   )
 }
