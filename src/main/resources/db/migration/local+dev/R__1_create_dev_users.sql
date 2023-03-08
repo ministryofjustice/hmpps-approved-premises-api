19760b188726cfbfd98580ceb2dcdb0d8414a141
pezholio <pezholio@gmail.com>
2023-03-08 10:15:41 +0000

    Add all roles to all dev users
    This adds all the available roles to the development users. I’ve also
    renamed the Delius username for `JIMSNOWLDAP` as it comes back from
    Delius as all uppercase.

--- src/main/resources/db/migration/local+dev/R__1_create_dev_users.sql
+++ src/main/resources/db/migration/local+dev/R__1_create_dev_users.sql
@@ -3,10 +3,203 @@
 --These are randomly generated names
 
 INSERT INTO "users" (id, name, delius_username, delius_staff_identifier, probation_region_id, delius_staff_code) VALUES
-    ('aa30f20a-84e3-4baa-bef0-3c9bd51879ad', 'Default User', 'JimSnowLdap', 2500041001, '43606be0-9836-441d-9bc1-5586de9ac931', 'STAFFCODE'), -- South West
+    ('aa30f20a-84e3-4baa-bef0-3c9bd51879ad', 'Default User', 'JIMSNOWLDAP', 2500041001, '43606be0-9836-441d-9bc1-5586de9ac931', 'STAFFCODE'), -- South West
     ('f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d', 'A. E2etester', 'APPROVEDPREMISESTESTUSER', 2500041002, 'ca979718-b15d-4318-9944-69aaff281cad', 'STAFFCODE'), -- East of England
     ('0621c5b0-0028-40b7-87fb-53ec65704314', 'T. Tester', 'temporary-accommodation-e2e-tester', 2500041003, 'afee0696-8df3-4d9f-9d0c-268f17772e2c', 'STAFFCODE'), -- Wales
     ('b5825da0-1553-4398-90ac-6a8e0c8a4cae', 'Tester Testy', 'tester.testy', 2500043547, 'c5acff6c-d0d2-4b89-9f4d-89a15cfa3891', 'STAFFCODE'), -- North East
     ('695ba399-c407-4b66-aafc-e8835d72b8a7', 'Bernard Beaks', 'bernard.beaks', 2500057096, 'ca979718-b15d-4318-9944-69aaff281cad', 'STAFFCODE'), -- East of England
     ('045b71d3-9845-49b3-a79b-c7799a6bc7bc', 'Panesar Jaspal', 'panesar.jaspal', 2500054544, 'afee0696-8df3-4d9f-9d0c-268f17772e2c', 'STAFFCODE') -- Wales
 ON CONFLICT (id) DO NOTHING;
+
+UPDATE
+  users
+SET
+  "delius_username" = 'JIMSNOWLDAP'
+WHERE
+  id = 'aa30f20a-84e3-4baa-bef0-3c9bd51879ad';
+
+INSERT INTO
+  "user_role_assignments" ("id", "role", "user_id")
+VALUES
+  (
+    '4d6500ff-0670-4a8e-8581-dff45292b2e4',
+    'ASSESSOR',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    'f78a5a6d-7d15-415c-b10f-91c0dff16753',
+    'MATCHER',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    'b7f476af-0e7e-44ba-9c66-abe27d8c70ba',
+    'MANAGER',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    '0354ff61-295a-4d06-b9e7-ae9fd53eba12',
+    'WORKFLOW_MANAGER',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    'dd949e2b-5f9b-4560-b3bc-c3ad6793fb28',
+    'APPLICANT',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    '81e7c996-4e20-4c4f-94c4-1acd4e5bce33',
+    'ROLE_ADMIN',
+    'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+  ),
+  (
+    'c729526f-1135-4383-ee7b-4326e5517ba0',
+    'ASSESSOR',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    'ac554076-cb51-454d-ae1e-ba466971dfe2',
+    'MATCHER',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    '7a88a8db-4ced-49a0-e4a4-45fce098d7e4',
+    'MANAGER',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    '71290b6b-67e1-4747-f6db-e389a83433dc',
+    'WORKFLOW_MANAGER',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    'd7d7ffe2-e975-4655-fb8b-96f2f23be682',
+    'APPLICANT',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    '80c184f8-43f3-4ccb-e726-77d9b89a8b2a',
+    'ROLE_ADMIN',
+    'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+  ),
+  (
+    '28f03d11-c430-4dea-e0e3-bec6ea67d2d9',
+    'ASSESSOR',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '8adb9e9b-fd29-475d-e603-bf231500a3e7',
+    'MATCHER',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '208f58d7-2979-4cea-acb9-1c54e9d66193',
+    'MANAGER',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '492d40dc-5554-4476-e8ad-1ec0f309450a',
+    'WORKFLOW_MANAGER',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '830d7aab-96e1-4056-be91-045e089378b8',
+    'APPLICANT',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '29ffb4f6-d72a-47e0-9ece-0797c5ff2169',
+    'ROLE_ADMIN',
+    '0621c5b0-0028-40b7-87fb-53ec65704314'
+  ),
+  (
+    '14bcef1a-fc78-4c42-f8d4-e35da461b837',
+    'ASSESSOR',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    'eec1afe7-4659-4ed5-d83f-a13ee89641b0',
+    'MATCHER',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    'e2dc3c5c-c9b7-4754-941d-c6e6043a7b26',
+    'MANAGER',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    '2d8000ed-67f9-4370-adac-234e1f4264a5',
+    'WORKFLOW_MANAGER',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    '50b8779d-487e-494f-dff5-f9958b9d0eae',
+    'APPLICANT',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    'afa2bec7-2820-4fd8-bd6d-fc1f610442f3',
+    'ROLE_ADMIN',
+    'b5825da0-1553-4398-90ac-6a8e0c8a4cae'
+  ),
+  (
+    '739b27c5-d17b-4fe7-d0a4-162eb3268e1b',
+    'ASSESSOR',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    '77635823-d53c-4e2e-b388-d120107f23c3',
+    'MATCHER',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    '15c9393a-c0fb-4506-b56a-e2b0054bb166',
+    'MANAGER',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    'b58c7932-4925-4fc5-c6bf-c1a7a34ce4ed',
+    'WORKFLOW_MANAGER',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    'cd8a76d2-35ff-4d9b-bf62-1e26911e8ada',
+    'APPLICANT',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    '28b39733-8955-4465-dd8d-4a58e2fba753',
+    'ROLE_ADMIN',
+    '695ba399-c407-4b66-aafc-e8835d72b8a7'
+  ),
+  (
+    '38f7f97a-c9d0-4521-dbf1-79acafe8a20f',
+    'ASSESSOR',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ),
+  (
+    '26dd8587-20a3-4925-c6fa-db15c57b41dd',
+    'MATCHER',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ),
+  (
+    'b0392e1e-c200-46eb-e42c-04d89f48febb',
+    'MANAGER',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ),
+  (
+    '60b95db2-bd09-4d7d-ca29-356b962e99c6',
+    'WORKFLOW_MANAGER',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ),
+  (
+    '2ec8c941-18e1-4715-8b4c-2a0d51050dfa',
+    'APPLICANT',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ),
+  (
+    'c3205409-b5d8-4945-ed5f-8ee9dba4ebe3',
+    'ROLE_ADMIN',
+    '045b71d3-9845-49b3-a79b-c7799a6bc7bc'
+  ) ON CONFLICT (id)
+DO
+  NOTHING;
