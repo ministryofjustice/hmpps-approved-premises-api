5763528adf63c42d1e031c58faffc9b58b194301
brownbearsoftware <brownbearsoftware@gmail.com>
2023-03-09 15:55:02 +0000

    Add `withCharacteristics` to AP/Bed factories

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/RoomEntityFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/RoomEntityFactory.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 import io.github.bluegroundltd.kfactory.Factory
 import io.github.bluegroundltd.kfactory.Yielded
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BedEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.CharacteristicEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.RoomEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringMultiCaseWithNumbers
@@ -14,6 +15,7 @@ class RoomEntityFactory : Factory<RoomEntity> {
   private var code: Yielded<String?> = { randomStringMultiCaseWithNumbers(6) }
   private var notes: Yielded<String?> = { randomStringMultiCaseWithNumbers(20) }
   private var premises: Yielded<PremisesEntity>? = null
+  private var characteristics: Yielded<MutableList<CharacteristicEntity>> = { mutableListOf() }
 
   fun withId(id: UUID) = apply {
     this.id = { id }
@@ -39,6 +41,12 @@ class RoomEntityFactory : Factory<RoomEntity> {
     this.premises = premises
   }
 
+  fun withCharacteristics(characteristics: MutableList<CharacteristicEntity>) = apply {
+    this.characteristics = { characteristics }
+  }
+
+  fun withCharacteristics(characteristics: List<CharacteristicEntity>) = withCharacteristics(characteristics.toMutableList())
+
   override fun produce() = RoomEntity(
     id = this.id(),
     name = this.name(),
@@ -46,7 +54,7 @@ class RoomEntityFactory : Factory<RoomEntity> {
     notes = this.notes(),
     beds = mutableListOf(),
     premises = this.premises?.invoke() ?: throw RuntimeException("Must provide a premises"),
-    characteristics = mutableListOf(),
+    characteristics = this.characteristics(),
   )
 }
 
@@ -75,6 +83,7 @@ class BedEntityFactory : Factory<BedEntity> {
   fun withYieldedRoom(room: Yielded<RoomEntity>) = apply {
     this.room = room
   }
+
   override fun produce() = BedEntity(
     id = this.id(),
     name = this.name(),
