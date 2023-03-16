23e463c04b4f6cf719dcb24bb5c8e76b899a4467
pezholio <pezholio@gmail.com>
2023-03-16 16:06:51 +0000

    Add some PersonUtils
    We seem to be doing a lot of fetching of people from CRNs, this adds a
    new `getPersonDetailsForCrn` helper and uses it in the Assessment helper
    util. This will be used in the next commit too.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/util/AssessmentUtils.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/util/AssessmentUtils.kt
@@ -4,7 +4,6 @@ import org.slf4j.Logger
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateDetail
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.OffenderService
 
 fun mapAndTransformAssessments(
@@ -15,33 +14,12 @@ fun mapAndTransformAssessments(
   transformer: (AssessmentEntity, OffenderDetailSummary, InmateDetail) -> Any
 ): List<Any> {
   return assessments.mapNotNull {
-    val applicationCrn = it.application.crn
+    val personDetail = getPersonDetailsForCrn(log, it.application.crn, deliusUsername, offenderService)
 
-    val offenderDetails = when (val offenderDetailsResult = offenderService.getOffenderByCrn(applicationCrn, deliusUsername)) {
-      is AuthorisableActionResult.Success -> offenderDetailsResult.entity
-      is AuthorisableActionResult.NotFound -> {
-        log.error("Could not get Offender Details for CRN: $applicationCrn")
-        return@mapNotNull null
-      }
-
-      is AuthorisableActionResult.Unauthorised -> return@mapNotNull null
-    }
-
-    if (offenderDetails.otherIds.nomsNumber == null) {
-      log.error("No NOMS number for CRN: $applicationCrn")
+    if (personDetail === null) {
       return@mapNotNull null
     }
 
-    val inmateDetails = when (val inmateDetailsResult = offenderService.getInmateDetailByNomsNumber(offenderDetails.otherIds.nomsNumber)) {
-      is AuthorisableActionResult.Success -> inmateDetailsResult.entity
-      is AuthorisableActionResult.NotFound -> {
-        log.error("Could not get Inmate Details for NOMS number: ${offenderDetails.otherIds.nomsNumber}")
-        return@mapNotNull null
-      }
-
-      is AuthorisableActionResult.Unauthorised -> return@mapNotNull null
-    }
-
-    transformer(it, offenderDetails, inmateDetails)
+    transformer(it, personDetail.first, personDetail.second)
   }
 }
