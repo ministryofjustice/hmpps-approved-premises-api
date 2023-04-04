99584703671670ca303734338673b087ef8897ab
brownbearsoftware <brownbearsoftware@gmail.com>
2023-04-04 14:45:55 +0100

    Add transformer for Booking Not Mades

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/BookingNotMadeTransformerTest.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.transformer
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ApprovedPremisesApplicationEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.AssessmentEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.BookingNotMadeEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.PlacementRequestEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.UserEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.BookingNotMadeTransformer
+import java.time.OffsetDateTime
+
+class BookingNotMadeTransformerTest {
+  private val bookingNotMadeTransformer = BookingNotMadeTransformer()
+
+  @Test
+  fun `transformJpaToApi transforms correctly`() {
+    val requestingUser = UserEntityFactory()
+      .withUnitTestControlProbationRegion()
+      .produce()
+
+    val otherUser = UserEntityFactory()
+      .withUnitTestControlProbationRegion()
+      .produce()
+
+    val application = ApprovedPremisesApplicationEntityFactory()
+      .withCreatedByUser(otherUser)
+      .produce()
+
+    val placementRequest = PlacementRequestEntityFactory()
+      .withAllocatedToUser(requestingUser)
+      .withApplication(application)
+      .withAssessment(
+        AssessmentEntityFactory()
+          .withApplication(application)
+          .withAllocatedToUser(otherUser)
+          .produce()
+      )
+      .produce()
+
+    val bookingNotMade = BookingNotMadeEntityFactory()
+      .withPlacementRequest(placementRequest)
+      .withNotes("some notes")
+      .withCreatedAt(OffsetDateTime.now())
+      .produce()
+
+    val result = bookingNotMadeTransformer.transformJpaToApi(bookingNotMade)
+
+    assertThat(result.notes).isEqualTo("some notes")
+    assertThat(result.placementRequestId).isEqualTo(placementRequest.id)
+  }
+}
