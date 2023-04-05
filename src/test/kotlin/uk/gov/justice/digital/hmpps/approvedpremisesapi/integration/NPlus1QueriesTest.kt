fd3cb8dcd4878afad64fffc5473eb3e50b50af42
Tom Hipkin <tomhipkin@gmail.com>
2023-04-05 16:42:49 +0100

    Include a load test for the summary endpoint
    Given the nature of this test is to check how the service performs when lots of data is present it is intentionally slow. It creates 200 premises which takes time. We want this confidence but we don't want the cost of running it on every test run. For now it's excluded from the default test suite.
    
    In future we could add this in as a parallel CI test.
    
    This borrows from the first PR that was addressing n+1 queries [1].
    
    [1] https://github.com/ministryofjustice/hmpps-approved-premises-api/pull/493

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/NPlus1QueriesTest.kt
@@ -0,0 +1,78 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.Appender
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given a User`
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+@ActiveProfiles("test", "log-sql")
+class NPlus1QueriesTest : IntegrationTestBase() {
+  val appender = mockk<Appender<ILoggingEvent>>()
+
+  val capturedLogs = mutableListOf<ILoggingEvent>()
+
+  val log = LoggerFactory.getLogger(this::class.java)
+
+  @BeforeEach
+  fun setup() {
+    val logger = LoggerFactory.getLogger("org.hibernate.SQL") as ch.qos.logback.classic.Logger
+    logger.addAppender(appender)
+
+    every { appender.doAppend(capture(capturedLogs)) } returns Unit
+  }
+
+  @Test
+  @Disabled("Load tests are not run by default")
+  fun `Listing a large number of Temporary Accommodation premises does not result in the N+1 queries problem`() {
+    `Given a User` { user, jwt ->
+      val premises = temporaryAccommodationPremisesEntityFactory.produceAndPersistMultiple(200) {
+        withYieldedLocalAuthorityArea {
+          localAuthorityEntityFactory.produceAndPersist()
+        }
+        withProbationRegion(user.probationRegion)
+        withService("CAS3")
+        withPdu("Some Location")
+      }
+
+      premises.forEach { p ->
+        val rooms = roomEntityFactory.produceAndPersistMultiple(20) {
+          withPremises(p)
+        }
+
+        rooms.forEach { r ->
+          bedEntityFactory.produceAndPersist {
+            withRoom(r)
+          }
+        }
+      }
+
+      capturedLogs.clear()
+
+      val requestTime = measureTimeMillis {
+        webTestClient.get()
+          .uri("/premises/summary")
+          .header("Authorization", "Bearer $jwt")
+          .header("X-Service-Name", "temporary-accommodation")
+          .header("X-User-Region", "${user.probationRegion.id}")
+          .exchange()
+          .expectStatus()
+          .isOk
+      }.milliseconds
+
+      log.info("Request took $requestTime (executed ${capturedLogs.size} queries)")
+
+      assertThat(capturedLogs).size().isEqualTo(1)
+      assertThat(requestTime).isLessThan(10.seconds)
+    }
+  }
+}
