69ed3ad62345dc0685b7148102e7aeebfb9c6921
Tom Hipkin <tomhipkin@gmail.com>
2023-04-05 16:23:25 +0100

    PremisesSummaryTransformer outputs a premises object that the API can return

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/PremisesSummaryTransformerTest.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.transformer
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PropertyStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TemporaryAccommodationPremisesSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.PremisesSummaryTransformer
+import java.util.UUID
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.PremisesSummary as DomainPremisesSummary
+
+class PremisesSummaryTransformerTest {
+  private val premisesSummaryTransformer = PremisesSummaryTransformer()
+
+  @Test
+  fun `transformDomainToApi transforms the DomainPremisesSummary into a TemporaryAccommodationPremisesSummary`() {
+    val uuid = UUID.randomUUID()
+    val domainPremisesSummary = DomainPremisesSummary(
+      id = uuid,
+      name = "bob",
+      addressLine1 = "address",
+      addressLine2 = "address line 2",
+      postcode = "123ABC",
+      pdu = "North east",
+      status = PropertyStatus.active,
+      bedCount = 1,
+    )
+
+    val result = premisesSummaryTransformer.transformDomainToApi(domainPremisesSummary)
+
+    assertThat(result).isEqualTo(
+      TemporaryAccommodationPremisesSummary(
+        id = uuid,
+        name = "bob",
+        addressLine1 = "address",
+        addressLine2 = "address line 2",
+        postcode = "123ABC",
+        pdu = "North east",
+        status = PropertyStatus.active,
+        bedCount = 1,
+      )
+    )
+  }
+}
