1b7c0981f3e620e94a70a09ddccb6d972ac6d0ed
Dan Livings <dan.livings@dxw.com>
2023-04-06 16:32:49 +0100

    Create ProbationDeliveryUnit JPA entity

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/ProbationDeliveryUnitEntity.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Repository
+interface ProbationDeliveryUnitRepository : JpaRepository<ProbationDeliveryUnitEntity, UUID> {
+  fun findAllByProbationRegion_Id(probationRegionId: UUID): List<ProbationDeliveryUnitEntity>
+}
+
+@Entity
+@Table(name = "probation_delivery_units")
+data class ProbationDeliveryUnitEntity(
+  @Id
+  val id: UUID,
+  val name: String,
+  @ManyToOne
+  @JoinColumn(name = "probation_region_id")
+  val probationRegion: ProbationRegionEntity,
+)
