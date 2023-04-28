866bb2d1c2da47bd953c4e3c70058e64f2b390c2
brownbearsoftware <brownbearsoftware@gmail.com>
2023-04-28 11:08:18 +0100

    Change Reports to support 1 input entity producing more than 1 output row
    Bedspace reports will need to produce multiple rows from each BedEntity input.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/generator/ReportGenerator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/generator/ReportGenerator.kt
@@ -1,17 +1,17 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.generator
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.api.CreateDataFrameDsl
 import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.toDataFrame
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
 
-abstract class ReportGenerator<Input, Output, Properties> {
+abstract class ReportGenerator<Input : Any, Output : Any, Properties>(private val outputType: KClass<Output>) {
   protected abstract fun filter(properties: Properties): (Input) -> Boolean
-  protected abstract val convert: CreateDataFrameDsl<Input>.() -> Unit
+  protected abstract val convert: Input.() -> List<Output>
 
   fun createReport(data: List<Input>, properties: Properties): DataFrame<Output> {
     val filter = filter(properties)
-    val convert = convertUnchecked()
 
     // Small amount of jiggery-pokery here to coerce the DataFrame into the desired type.
     // The extension method `Iterable<T>.toDataFrame()` requires its type parameters to be
@@ -21,14 +21,12 @@ abstract class ReportGenerator<Input, Output, Properties> {
     // closure is also cast to accept an Any.
     // From there we can call `DataFrame.cast()` to get it to be the desired `DataFrame<Output>`
     // type.
-    return data.filter(filter)
+    val converted = data.filter(filter)
+      .flatMap(convert)
+
+    return converted
       .map { it as Any }
-      .toDataFrame(convert)
+      .toDataFrame(*outputType.memberProperties.toTypedArray())
       .cast()
   }
-
-  // This cast of the `convert` closure to use Any as the type parameter is safe as in practice
-  // it will only ever have items of the type specified by the type parameter `Input`.
-  @Suppress("UNCHECKED_CAST")
-  private fun convertUnchecked(): CreateDataFrameDsl<Any>.() -> Unit = convert as CreateDataFrameDsl<Any>.() -> Unit
 }
