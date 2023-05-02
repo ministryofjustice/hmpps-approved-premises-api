62f7a4f8934a2f277d64d3a1d9b01dba89527546
Dan Livings <dan.livings@dxw.com>
2023-05-02 15:03:05 +0100

    Allow Wiremock to mock GOV.UK Bank Holidays API in tests

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/httpmocks/GovUKBankHolidaysAPI.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks
+
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.bankholidaysapi.CountryBankHolidays
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.bankholidaysapi.UKBankHolidays
+
+fun IntegrationTestBase.GovUKBankHolidaysAPI_mockSuccessfulCall(bankHolidays: UKBankHolidays) =
+  mockSuccessfulGetCallWithJsonResponse(
+    url = "/bank-holidays.json",
+    responseBody = bankHolidays
+  )
+
+fun IntegrationTestBase.GovUKBankHolidaysAPI_mockSuccessfullCallWithEmptyResponse() =
+  GovUKBankHolidaysAPI_mockSuccessfulCall(
+    UKBankHolidays(
+      englandAndWales = CountryBankHolidays("england-and-wales", listOf()),
+      scotland = CountryBankHolidays("scotland", listOf()),
+      northernIreland = CountryBankHolidays("northern-ireland", listOf()),
+    )
+  )
