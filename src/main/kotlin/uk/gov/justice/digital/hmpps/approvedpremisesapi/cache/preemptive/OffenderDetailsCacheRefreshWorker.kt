f867e0600195b08c0b46b688ce1b129b2642f2c3
brownbearsoftware <brownbearsoftware@gmail.com>
2023-05-02 11:10:10 +0100

    Add support for preemptively cached Inmate Details
    `PrisonsApiClient::getInmateDetailsWithWait` will replace `getInmateDetails` whilst `getInmateDetailsWithCall` is used by `InmateDetailsCacheRefreshWorker` to populate the preemptive cache.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cache/preemptive/OffenderDetailsCacheRefreshWorker.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cache/preemptive/OffenderDetailsCacheRefreshWorker.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.cache.preemptive
 import redis.lock.redlock.RedLock
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ClientResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.CommunityApiClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PrisonsApiClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingRepository
 
@@ -10,8 +11,9 @@ class OffenderDetailsCacheRefreshWorker(
   private val applicationRepository: ApplicationRepository,
   private val bookingRepository: BookingRepository,
   private val communityApiClient: CommunityApiClient,
+  private val prisonsApiClient: PrisonsApiClient,
   redLock: RedLock
-) : CacheRefreshWorker(redLock, "offenderDetails") {
+) : CacheRefreshWorker(redLock, "offenderDetailsAndInmateDetails") {
   override fun work(checkShouldStop: () -> Boolean) {
     val distinctCrns = (applicationRepository.getDistinctCrns() + bookingRepository.getDistinctCrns()).distinct()
 
@@ -20,14 +22,28 @@ class OffenderDetailsCacheRefreshWorker(
 
       interruptableSleep(50)
 
-      // A potential further improvement is to do these calls in concurrent batches
-      val result = communityApiClient.getOffenderDetailSummaryWithCall(it)
-      if (result is ClientResult.Failure.StatusCode) {
-        log.error("Unable to refresh Offender Details for $it, response status: ${result.status}")
+      val communityApiResult = communityApiClient.getOffenderDetailSummaryWithCall(it)
+      if (communityApiResult is ClientResult.Failure.StatusCode) {
+        log.error("Unable to refresh Offender Details for $it, response status: ${communityApiResult.status}")
       }
 
-      if (result is ClientResult.Failure.Other) {
-        log.error("Unable to refresh Offender Details for $it: ${result.exception.message}")
+      if (communityApiResult is ClientResult.Failure.Other) {
+        log.error("Unable to refresh Offender Details for $it: ${communityApiResult.exception.message}")
+      }
+
+      if (communityApiResult is ClientResult.Success) {
+        val nomsNumber = communityApiResult.body.otherIds.nomsNumber
+
+        if (nomsNumber != null) {
+          val prisonsApiResult = prisonsApiClient.getInmateDetailsWithCall(nomsNumber)
+          if (prisonsApiResult is ClientResult.Failure.StatusCode) {
+            log.error("Unable to refresh Inmate Details for $nomsNumber, response status: ${prisonsApiResult.status}")
+          }
+
+          if (prisonsApiResult is ClientResult.Failure.Other) {
+            log.error("Unable to refresh Inmate Details for $nomsNumber: ${prisonsApiResult.exception.message}")
+          }
+        }
       }
     }
   }
