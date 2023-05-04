b0b626faa91a344ae8e71e1e5a219cc37244b1da
brownbearsoftware <brownbearsoftware@gmail.com>
2023-05-04 10:22:46 +0100

    Update Gradle config to allow static mocking java.time classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ tasks.register("bootRunLocal") {
 }
 
 tasks.withType<Test> {
-  jvmArgs("--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED")
+  jvmArgs("--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED", "--add-opens", "java.base/java.time=ALL-UNNAMED")
 
   if (environment["GITHUB_ACTION"] != null) {
     maxParallelForks = Runtime.getRuntime().availableProcessors()
