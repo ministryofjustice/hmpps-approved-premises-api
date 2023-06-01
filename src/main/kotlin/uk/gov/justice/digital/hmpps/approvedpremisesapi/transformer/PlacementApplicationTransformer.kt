a3843ac2754e7799503ed1b81f0e9231e2252c54
pezholio <pezholio@gmail.com>
2023-06-01 08:19:08 +0100

    Add a Transformer

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementApplicationTransformer.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementApplication
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PlacementApplicationEntity
+
+@Component
+class PlacementApplicationTransformer(
+  private val objectMapper: ObjectMapper,
+) {
+  fun transformJpaToApi(jpa: PlacementApplicationEntity): PlacementApplication = PlacementApplication(
+    id = jpa.id,
+    applicationId = jpa.application.id,
+    createdByUserId = jpa.createdByUser.id,
+    schemaVersion = jpa.schemaVersion.id,
+    createdAt = jpa.createdAt.toInstant(),
+    data = if (jpa.data != null) objectMapper.readTree(jpa.data) else null,
+    document = if (jpa.document != null) objectMapper.readTree(jpa.document) else null,
+    outdatedSchema = !jpa.schemaUpToDate,
+    submittedAt = jpa.submittedAt?.toInstant(),
+  )
+}
