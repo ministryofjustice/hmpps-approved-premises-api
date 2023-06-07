04eba0e23794a9062071e70fd88493bef5bd7663
Dan Livings <dan.livings@dxw.com>
2023-06-07 15:30:24 +0100

    Implement transformation of CAS3 roles to API response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/UserTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/UserTransformer.kt
@@ -2,15 +2,16 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApprovedPremisesUser
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApprovedPremisesUserRole
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TemporaryAccommodationUser
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TemporaryAccommodationUserRole
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserQualification
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserQualificationAssignmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRole
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRoleAssignmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.UserQualification as ApiUserQualification
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.UserRole as ApiUserRole
 
 @Component
 class UserTransformer(
@@ -20,7 +21,7 @@ class UserTransformer(
     ServiceName.approvedPremises, ServiceName.cas2 -> ApprovedPremisesUser(
       id = jpa.id,
       deliusUsername = jpa.deliusUsername,
-      roles = jpa.roles.mapNotNull(::transformRoleToApi),
+      roles = jpa.roles.mapNotNull(::transformApprovedPremisesRoleToApi),
       email = jpa.email,
       name = jpa.name,
       telephoneNumber = jpa.telephoneNumber,
@@ -30,19 +31,25 @@ class UserTransformer(
     )
     ServiceName.temporaryAccommodation -> TemporaryAccommodationUser(
       id = jpa.id,
-      roles = jpa.roles.mapNotNull(::transformRoleToApi),
+      roles = jpa.roles.mapNotNull(::transformTemporaryAccommodationRoleToApi),
       region = probationRegionTransformer.transformJpaToApi(jpa.probationRegion),
       service = ServiceName.temporaryAccommodation.value,
     )
   }
 
-  private fun transformRoleToApi(userRole: UserRoleAssignmentEntity): ApiUserRole? = when (userRole.role) {
-    UserRole.CAS1_ADMIN -> ApiUserRole.roleAdmin
-    UserRole.CAS1_ASSESSOR -> ApiUserRole.assessor
-    UserRole.CAS1_MATCHER -> ApiUserRole.matcher
-    UserRole.CAS1_MANAGER -> ApiUserRole.manager
-    UserRole.CAS1_WORKFLOW_MANAGER -> ApiUserRole.workflowManager
-    UserRole.CAS1_APPLICANT -> ApiUserRole.applicant
+  private fun transformApprovedPremisesRoleToApi(userRole: UserRoleAssignmentEntity): ApprovedPremisesUserRole? = when (userRole.role) {
+    UserRole.CAS1_ADMIN -> ApprovedPremisesUserRole.roleAdmin
+    UserRole.CAS1_ASSESSOR -> ApprovedPremisesUserRole.assessor
+    UserRole.CAS1_MATCHER -> ApprovedPremisesUserRole.matcher
+    UserRole.CAS1_MANAGER -> ApprovedPremisesUserRole.manager
+    UserRole.CAS1_WORKFLOW_MANAGER -> ApprovedPremisesUserRole.workflowManager
+    UserRole.CAS1_APPLICANT -> ApprovedPremisesUserRole.applicant
+    else -> null
+  }
+
+  private fun transformTemporaryAccommodationRoleToApi(userRole: UserRoleAssignmentEntity): TemporaryAccommodationUserRole? = when (userRole.role) {
+    UserRole.CAS3_ASSESSOR -> TemporaryAccommodationUserRole.assessor
+    UserRole.CAS3_REFERRER -> TemporaryAccommodationUserRole.referrer
     else -> null
   }
 
