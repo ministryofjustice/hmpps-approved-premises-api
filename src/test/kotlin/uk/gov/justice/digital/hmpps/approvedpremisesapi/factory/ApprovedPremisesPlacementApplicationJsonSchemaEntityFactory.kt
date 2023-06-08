7fee83b190f89520d0e9f931e9309f5946e1670b
pezholio <pezholio@gmail.com>
2023-06-08 14:46:23 +0100

    Update addedAt in factory
    We always want the schema we create in test to be the newest one by
    default.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/ApprovedPremisesPlacementApplicationJsonSchemaEntityFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/ApprovedPremisesPlacementApplicationJsonSchemaEntityFactory.kt
@@ -3,13 +3,12 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 import io.github.bluegroundltd.kfactory.Factory
 import io.github.bluegroundltd.kfactory.Yielded
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesPlacementApplicationJsonSchemaEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomDateTimeBefore
 import java.time.OffsetDateTime
 import java.util.UUID
 
 class ApprovedPremisesPlacementApplicationJsonSchemaEntityFactory : Factory<ApprovedPremisesPlacementApplicationJsonSchemaEntity> {
   private var id: Yielded<UUID> = { UUID.randomUUID() }
-  private var addedAt: Yielded<OffsetDateTime> = { OffsetDateTime.now().randomDateTimeBefore(7) }
+  private var addedAt: Yielded<OffsetDateTime> = { OffsetDateTime.now() }
   private var schema: Yielded<String> = { "{}" }
 
   fun withId(id: UUID) = apply {
