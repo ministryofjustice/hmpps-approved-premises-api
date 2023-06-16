905ebbdb029a9af20871334f89b25fb8c244eea8
pezholio <pezholio@gmail.com>
2023-06-16 16:02:26 +0100

    Add PlacementRequestDetail object and transformer
    This extends the Placement Request with an array of cancellations

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementRequestDetailTransformer.kt
@@ -0,0 +1,41 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementRequestDetail
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.CancellationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PlacementRequestEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateDetail
+
+@Component
+class PlacementRequestDetailTransformer(
+  private val placementRequestTransformer: PlacementRequestTransformer,
+  private val cancellationTransformer: CancellationTransformer,
+) {
+  fun transformJpaToApi(jpa: PlacementRequestEntity, offenderDetailSummary: OffenderDetailSummary, inmateDetail: InmateDetail, cancellations: List<CancellationEntity>): PlacementRequestDetail {
+    val placementRequest = placementRequestTransformer.transformJpaToApi(jpa, offenderDetailSummary, inmateDetail)
+
+    return PlacementRequestDetail(
+      id = placementRequest.id,
+      gender = placementRequest.gender,
+      type = placementRequest.type,
+      expectedArrival = placementRequest.expectedArrival,
+      duration = placementRequest.duration,
+      location = placementRequest.location,
+      radius = placementRequest.radius,
+      essentialCriteria = placementRequest.essentialCriteria,
+      desirableCriteria = placementRequest.desirableCriteria,
+      person = placementRequest.person,
+      risks = placementRequest.risks,
+      applicationId = placementRequest.applicationId,
+      assessmentId = placementRequest.assessmentId,
+      releaseType = placementRequest.releaseType,
+      status = placementRequest.status,
+      assessmentDecision = placementRequest.assessmentDecision,
+      assessmentDate = placementRequest.assessmentDate,
+      assessor = placementRequest.assessor,
+      notes = placementRequest.notes,
+      cancellations = cancellations.mapNotNull { cancellationTransformer.transformJpaToApi(it) },
+    )
+  }
+}
