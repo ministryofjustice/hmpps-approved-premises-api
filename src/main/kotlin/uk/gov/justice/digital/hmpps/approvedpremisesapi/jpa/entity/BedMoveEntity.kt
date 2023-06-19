4ef6cc47abbc97cf9bf51529fe7c440cf6fd35b9
pezholio <pezholio@gmail.com>
2023-06-19 11:58:50 +0100

    Add a BedMoveEntity
    This will allow us to record the history of a booking, and
    when they were moved from one bed to another

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/BedMoveEntity.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.time.OffsetDateTime
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
+import javax.persistence.Table
+
+@Repository
+interface BedMoveRepository : JpaRepository<BedMoveEntity, UUID>
+
+@Entity
+@Table(name = "bed_moves")
+data class BedMoveEntity(
+  @Id
+  val id: UUID,
+
+  @OneToOne
+  @JoinColumn(name = "booking_id")
+  val booking: BookingEntity,
+
+  @OneToOne
+  @JoinColumn(name = "previous_bed_id")
+  val previousBed: BedEntity,
+
+  @OneToOne
+  @JoinColumn(name = "new_bed_id")
+  val newBed: BedEntity,
+
+  var createdAt: OffsetDateTime,
+
+  val notes: String?,
+) {
+
+  override fun toString() = "BedMovesEntity: $id"
+}
