f07ead8dd57a8222c249278789d297d0c4f88936
pezholio <pezholio@gmail.com>
2023-06-28 14:29:16 +0100

    Return Arrival Time in transformer
    We need to ensure the Instant is UTC, then we format just returning
    the time.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/ArrivalTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/ArrivalTransformer.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Arrival
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ArrivalEntity
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 @Component
 class ArrivalTransformer() {
@@ -10,6 +12,7 @@ class ArrivalTransformer() {
     Arrival(
       bookingId = jpa.booking.id,
       arrivalDate = jpa.arrivalDate,
+      arrivalTime = DateTimeFormatter.ISO_LOCAL_TIME.format(jpa.arrivalDateTime.atZone(ZoneOffset.UTC)),
       expectedDepartureDate = jpa.expectedDepartureDate,
       notes = jpa.notes,
       createdAt = jpa.createdAt.toInstant(),
