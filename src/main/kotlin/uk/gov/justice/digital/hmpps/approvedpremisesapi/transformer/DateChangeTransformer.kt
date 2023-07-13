8aacc4788052a302c0de92873fe1de65de9281bd
brownbearsoftware <brownbearsoftware@gmail.com>
2023-07-13 10:25:39 +0100

    Add transformer for Date Changes

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/DateChangeTransformer.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.DateChange
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DateChangeEntity
+
+@Component
+class DateChangeTransformer() {
+  fun transformJpaToApi(jpa: DateChangeEntity) = DateChange(
+    id = jpa.id,
+    bookingId = jpa.booking.id,
+    previousDepartureDate = jpa.previousDepartureDate,
+    newDepartureDate = jpa.newDepartureDate,
+    previousArrivalDate = jpa.previousArrivalDate,
+    newArrivalDate = jpa.newArrivalDate,
+    createdAt = jpa.changedAt.toInstant(),
+  )
+}
