0cb279d75a60cbf51afdce10577db70be1fa7ddd
Dan Livings <dan.livings@dxw.com>
2023-08-24 11:30:24 +0100

    Implement JPA-to-API transformation for system notes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/AssessmentReferralHistoryNoteTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/AssessmentReferralHistoryNoteTransformer.kt
@@ -2,9 +2,12 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ReferralHistoryNote
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ReferralHistorySystemNote
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ReferralHistoryUserNote
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentReferralHistoryNoteEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentReferralHistorySystemNoteEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentReferralHistoryUserNoteEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ReferralHistorySystemNoteType
 
 @Component
 class AssessmentReferralHistoryNoteTransformer {
@@ -14,7 +17,25 @@ class AssessmentReferralHistoryNoteTransformer {
       createdAt = jpa.createdAt.toInstant(),
       message = jpa.message,
       createdByUserName = jpa.createdByUser.name,
+      type = "user",
+    )
+    is AssessmentReferralHistorySystemNoteEntity -> ReferralHistorySystemNote(
+      id = jpa.id,
+      createdAt = jpa.createdAt.toInstant(),
+      message = jpa.message,
+      createdByUserName = jpa.createdByUser.name,
+      type = "system",
+      category = transformSystemNoteTypeToCategory(jpa.type),
     )
     else -> throw RuntimeException("Unsupported ReferralHistoryNote type: ${jpa::class.qualifiedName}")
+  }
+
+  private fun transformSystemNoteTypeToCategory(type: ReferralHistorySystemNoteType): ReferralHistorySystemNote.Category = when (type) {
+    ReferralHistorySystemNoteType.SUBMITTED -> ReferralHistorySystemNote.Category.submitted
+    ReferralHistorySystemNoteType.UNALLOCATED -> ReferralHistorySystemNote.Category.unallocated
+    ReferralHistorySystemNoteType.IN_REVIEW -> ReferralHistorySystemNote.Category.inReview
+    ReferralHistorySystemNoteType.READY_TO_PLACE -> ReferralHistorySystemNote.Category.readyToPlace
+    ReferralHistorySystemNoteType.REJECTED -> ReferralHistorySystemNote.Category.rejected
+    ReferralHistorySystemNoteType.COMPLETED -> ReferralHistorySystemNote.Category.completed
   }
 }
