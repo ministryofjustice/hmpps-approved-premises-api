dc8b301715aeaf292ac545b3992bf87f383630d5
pezholio <pezholio@gmail.com>
2023-09-05 17:15:36 +0100

    Remove users not in Delius from Dev and Test
    The users  “tester.testy”, “bernard.beaks” and “panesar.jaspal” do not
    exist in Delius. Now we call Delius to check if a user can access
    the offender for a assessment / placement request, we need to ensure
    they exist in Delius, otherwise the call will fail.

---
+++ src/main/resources/db/migration/dev+test/R__6_create_users.sql
@@ -0,0 +1,112 @@
+-- ${flyway:timestamp}
+
+--These are randomly generated names
+
+INSERT INTO "users" (id, name, delius_username, delius_staff_identifier, probation_region_id, delius_staff_code) VALUES
+ ('aa30f20a-84e3-4baa-bef0-3c9bd51879ad', 'Default User', 'JIMSNOWLDAP', 2500041001, '43606be0-9836-441d-9bc1-5586de9ac931', 'STAFFCODE'), -- South West
+ ('f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d', 'A. E2etester', 'APPROVEDPREMISESTESTUSER', 2500041002, 'ca979718-b15d-4318-9944-69aaff281cad', 'STAFFCODE'), -- East of England
+ ('0621c5b0-0028-40b7-87fb-53ec65704314', 'T. Tester', 'temporary-accommodation-e2e-tester', 2500041003, 'afee0696-8df3-4d9f-9d0c-268f17772e2c', 'STAFFCODE'), -- Wales
+    ON CONFLICT (id) DO NOTHING;
+
+UPDATE
+    users
+SET
+    "delius_username" = 'JIMSNOWLDAP'
+WHERE
+        id = 'aa30f20a-84e3-4baa-bef0-3c9bd51879ad';
+
+INSERT INTO
+    "user_role_assignments" ("id", "role", "user_id")
+VALUES
+    (
+        '4d6500ff-0670-4a8e-8581-dff45292b2e4',
+        'CAS1_ASSESSOR',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        'f78a5a6d-7d15-415c-b10f-91c0dff16753',
+        'CAS1_MATCHER',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        'b7f476af-0e7e-44ba-9c66-abe27d8c70ba',
+        'CAS1_MANAGER',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        '0354ff61-295a-4d06-b9e7-ae9fd53eba12',
+        'CAS1_WORKFLOW_MANAGER',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        'dd949e2b-5f9b-4560-b3bc-c3ad6793fb28',
+        'CAS1_APPLICANT',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        '81e7c996-4e20-4c4f-94c4-1acd4e5bce33',
+        'CAS1_ADMIN',
+        'aa30f20a-84e3-4baa-bef0-3c9bd51879ad'
+    ),
+    (
+        'c729526f-1135-4383-ee7b-4326e5517ba0',
+        'CAS1_ASSESSOR',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        'ac554076-cb51-454d-ae1e-ba466971dfe2',
+        'CAS1_MATCHER',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        '7a88a8db-4ced-49a0-e4a4-45fce098d7e4',
+        'CAS1_MANAGER',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        '71290b6b-67e1-4747-f6db-e389a83433dc',
+        'CAS1_WORKFLOW_MANAGER',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        'd7d7ffe2-e975-4655-fb8b-96f2f23be682',
+        'CAS1_APPLICANT',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        '80c184f8-43f3-4ccb-e726-77d9b89a8b2a',
+        'CAS1_ADMIN',
+        'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d'
+    ),
+    (
+        '28f03d11-c430-4dea-e0e3-bec6ea67d2d9',
+        'CAS1_ASSESSOR',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ),
+    (
+        '8adb9e9b-fd29-475d-e603-bf231500a3e7',
+        'CAS1_MATCHER',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ),
+    (
+        '208f58d7-2979-4cea-acb9-1c54e9d66193',
+        'CAS1_MANAGER',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ),
+    (
+        '492d40dc-5554-4476-e8ad-1ec0f309450a',
+        'CAS1_WORKFLOW_MANAGER',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ),
+    (
+        '830d7aab-96e1-4056-be91-045e089378b8',
+        'CAS1_APPLICANT',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ),
+    (
+        '29ffb4f6-d72a-47e0-9ece-0797c5ff2169',
+        'CAS1_ADMIN',
+        '0621c5b0-0028-40b7-87fb-53ec65704314'
+    ) ON CONFLICT (id)
+DO
+  NOTHING;
