2b7742fa65d5313b2c2e7ed0a2781dc9bec186ba
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-09-25 18:02:11 +0100

    Switch UserAccessService to handle NomisUser

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/UserAccessService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/UserAccessService.kt
@@ -2,11 +2,12 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.NomisUserEntity
 
 @Service("Cas2UserAccessService")
 class UserAccessService() {
-  fun userCanViewApplication(user: UserEntity, application: Cas2ApplicationEntity): Boolean {
+  fun userCanViewApplication(user: NomisUserEntity, application: Cas2ApplicationEntity):
+    Boolean {
     return (user.id == application.createdByUser.id)
   }
 }
