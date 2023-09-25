0716e0986e5db92fe0e94db838e27a90a26e0e07
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-09-25 17:26:33 +0100

    Add Nomis User Entity

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/NomisUserEntity.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Repository
+interface NomisUserRepository : JpaRepository<NomisUserEntity, UUID> {
+  fun findByNomisUsername(nomisUserName: String): NomisUserEntity?
+}
+
+@Entity
+@Table(name = "nomis_users")
+data class NomisUserEntity(
+  @Id
+  val id: UUID,
+  val nomisUsername: String,
+  var nomisStaffId: Long,
+  var name: String,
+  var accountType: String,
+  var isEnabled: Boolean,
+  var isActive: Boolean,
+  var email: String?,
+
+  @OneToMany(mappedBy = "createdByUser")
+  val applications: MutableList<Cas2ApplicationEntity>,
+) {
+  override fun toString() = "Nomis user $id"
+}
