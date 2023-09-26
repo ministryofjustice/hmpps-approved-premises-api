f00dba87d87a340a24dffa6320c316355885213f
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-09-26 12:11:59 +0100

    Set nomis_users.created_at automatically

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/NomisUserEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/NomisUserEntity.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
 
+import org.hibernate.annotations.CreationTimestamp
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.OffsetDateTime
 import java.util.UUID
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -25,6 +27,9 @@ data class NomisUserEntity(
   var isEnabled: Boolean,
   var isActive: Boolean,
   var email: String?,
+
+  @CreationTimestamp
+  private val createdAt: OffsetDateTime? = null,
 
   @OneToMany(mappedBy = "createdByUser")
   val applications: MutableList<Cas2ApplicationEntity>,
