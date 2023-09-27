9cb0d5b9d639815e6caee0a07b9dbed1c3628e6e
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-09-27 11:59:54 +0100

    Fetch OASys 'risks to self' without using Delius user

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas2/PeopleController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas2/PeopleController.kt
@@ -1,19 +1,30 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller.cas2
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2.PeopleCas2Delegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.OASysRiskToSelf
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Person
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.ForbiddenProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.NotFoundProblem
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.NomisUserService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.OffenderService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.OASysSectionsTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.PersonTransformer
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.OffenderService as OASysOffenderService
 
 @Service("Cas2PeopleController")
 class PeopleController(
   private val offenderService: OffenderService,
+  private val oaSysOffenderService: OASysOffenderService,
+  private val oaSysSectionsTransformer: OASysSectionsTransformer,
   private val personTransformer: PersonTransformer,
   private val userService: NomisUserService,
 ) : PeopleCas2Delegate {
@@ -31,5 +42,42 @@ class PeopleController(
         personTransformer.transformModelToPersonApi(personInfo),
       )
     }
+  }
+
+  override fun peopleCrnOasysRiskToSelfGet(crn: String): ResponseEntity<OASysRiskToSelf> {
+    getOffenderDetails(crn)
+
+    return runBlocking(context = Dispatchers.IO) {
+      val offenceDetailsResult = async {
+        oaSysOffenderService.getOASysOffenceDetails(crn)
+      }
+
+      val riskToTheIndividualResult = async {
+        oaSysOffenderService.getOASysRiskToTheIndividual(crn)
+      }
+
+      val offenceDetails = getSuccessEntityOrThrow(crn, offenceDetailsResult.await())
+      val riskToTheIndividual = getSuccessEntityOrThrow(crn, riskToTheIndividualResult.await())
+
+      ResponseEntity.ok(
+        oaSysSectionsTransformer.transformRiskToIndividual(offenceDetails, riskToTheIndividual),
+      )
+    }
+  }
+
+  private fun getOffenderDetails(crn: String): OffenderDetailSummary {
+    val offenderDetails = when (val offenderDetailsResult = offenderService.getOffenderByCrn(crn)) {
+      is AuthorisableActionResult.NotFound -> throw NotFoundProblem(crn, "Person")
+      is AuthorisableActionResult.Unauthorised -> throw ForbiddenProblem()
+      is AuthorisableActionResult.Success -> offenderDetailsResult.entity
+    }
+
+    return offenderDetails
+  }
+
+  private fun <T> getSuccessEntityOrThrow(crn: String, authorisableActionResult: AuthorisableActionResult<T>): T = when (authorisableActionResult) {
+    is AuthorisableActionResult.NotFound -> throw NotFoundProblem(crn, "Person")
+    is AuthorisableActionResult.Unauthorised -> throw ForbiddenProblem()
+    is AuthorisableActionResult.Success -> authorisableActionResult.entity
   }
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2PersonOASysRiskToSelfTest.kt
@@ -0,0 +1,123 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.OffenceDetailsFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.RiskToTheIndividualFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given a CAS2 User`
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given an Offender`
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.APOASysContext_mockSuccessfulOffenceDetailsCall
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.APOASysContext_mockSuccessfulRiskToTheIndividualCall
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.APOASysContext_mockUnsuccessfulRisksToTheIndividualCallWithDelay
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.CommunityAPI_mockNotFoundOffenderDetailsCall
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.OASysSectionsTransformer
+
+class Cas2PersonOASysRiskToSelfTest : IntegrationTestBase() {
+  @Autowired
+  lateinit var oaSysSectionsTransformer: OASysSectionsTransformer
+
+  @Test
+  fun `Getting Risk to Self by CRN without a JWT returns 401`() {
+    webTestClient.get()
+      .uri("/cas2/people/CRN/oasys/risk-to-self")
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+
+  @Test
+  fun `Getting Risk to Self for a CRN with an invalid auth-source JWT returns 403`() {
+    val jwt = jwtAuthHelper.createClientCredentialsJwt(
+      username = "username",
+      authSource = "bananas",
+    )
+
+    webTestClient.get()
+      .uri("/cas2/people/CRN/oasys/risk-to-self")
+      .header("Authorization", "Bearer $jwt")
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `Getting oasys sections for a CRN without ROLE_PROBATION or ROLE_PRISON returns 403`
+  () {
+    val jwt = jwtAuthHelper.createAuthorizationCodeJwt(
+      subject = "username",
+      authSource = "delius",
+      roles = listOf("ROLE_OTHER"),
+    )
+
+    webTestClient.get()
+      .uri("/cas2/people/CRN/oasys/risk-to-self")
+      .header("Authorization", "Bearer $jwt")
+      .exchange()
+      .expectStatus()
+      .isForbidden
+  }
+
+  @Test
+  fun `Getting Risk To Self for a CRN that does not exist returns 404`() {
+    `Given a CAS2 User` { userEntity, jwt ->
+      val crn = "CRN123"
+
+      CommunityAPI_mockNotFoundOffenderDetailsCall(crn)
+      loadPreemptiveCacheForOffenderDetails(crn)
+
+      webTestClient.get()
+        .uri("/cas2/people/$crn/oasys/risk-to-self")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+  }
+
+  @Test
+  fun `Getting Risk to Self for a CRN returns OK with correct body`() {
+    `Given a CAS2 User` { userEntity, jwt ->
+      `Given an Offender` { offenderDetails, inmateDetails ->
+        val offenceDetails = OffenceDetailsFactory().produce()
+        APOASysContext_mockSuccessfulOffenceDetailsCall(offenderDetails.otherIds.crn, offenceDetails)
+
+        val risksToTheIndividual = RiskToTheIndividualFactory().produce()
+        APOASysContext_mockSuccessfulRiskToTheIndividualCall(offenderDetails.otherIds.crn, risksToTheIndividual)
+
+        webTestClient.get()
+          .uri("/cas2/people/${offenderDetails.otherIds.crn}/oasys/risk-to-self")
+          .header("Authorization", "Bearer $jwt")
+          .exchange()
+          .expectStatus()
+          .isOk
+          .expectBody()
+          .json(
+            objectMapper.writeValueAsString(
+              oaSysSectionsTransformer.transformRiskToIndividual(
+                offenceDetails,
+                risksToTheIndividual,
+              ),
+            ),
+          )
+      }
+    }
+  }
+
+  @Test
+  fun `Getting Risk to Self when upstream times out returns 404`() {
+    `Given a CAS2 User` { userEntity, jwt ->
+      `Given an Offender` { offenderDetails, inmateDetails ->
+        val risksToTheIndividual = RiskToTheIndividualFactory().produce()
+        APOASysContext_mockUnsuccessfulRisksToTheIndividualCallWithDelay(offenderDetails.otherIds.crn, risksToTheIndividual, 2500)
+
+        webTestClient.get()
+          .uri("/cas2/people/${offenderDetails.otherIds.crn}/oasys/risk-to-self")
+          .header("Authorization", "Bearer $jwt")
+          .exchange()
+          .expectStatus()
+          .isNotFound
+      }
+    }
+  }
+}
