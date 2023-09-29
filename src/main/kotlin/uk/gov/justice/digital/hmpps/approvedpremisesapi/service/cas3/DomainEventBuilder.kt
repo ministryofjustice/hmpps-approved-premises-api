b0b8f8ddd8d98a3d4c5b737e5e29f52d6833ae00
Dan Livings <dan.livings@dxw.com>
2023-09-29 16:58:07 +0100

    Implement service layer scaffold for creating and getting CAS3 events

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas3/DomainEventBuilder.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3
+
+import org.springframework.stereotype.Component
+
+@Component
+class DomainEventBuilder
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas3/DomainEventServiceTest.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.service.cas3
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.mockk.mockk
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3.DomainEventBuilder
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3.DomainEventService
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+
+class DomainEventServiceTest {
+  private val domainEventRepositoryMock = mockk<DomainEventRepository>()
+  private val domainEventBuilderMock = mockk<DomainEventBuilder>()
+  private val hmppsQueueServiceMock = mockk<HmppsQueueService>()
+
+  private val objectMapper = ObjectMapper().apply {
+    registerModule(Jdk8Module())
+    registerModule(JavaTimeModule())
+    registerKotlinModule()
+  }
+
+  private val domainEventService = DomainEventService(
+    objectMapper = objectMapper,
+    domainEventRepository = domainEventRepositoryMock,
+    domainEventBuilder = domainEventBuilderMock,
+    hmppsQueueService = hmppsQueueServiceMock,
+    emitDomainEventsEnabled = true,
+  )
+}
