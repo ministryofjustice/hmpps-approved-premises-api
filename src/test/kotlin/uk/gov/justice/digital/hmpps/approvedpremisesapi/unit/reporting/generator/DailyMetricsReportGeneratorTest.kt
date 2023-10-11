32a6ca83e2651a2f218be68eb63f82dfa2bfdf80
pezholio <pezholio@gmail.com>
2023-10-11 09:41:22 +0100

    Add a generator for daily metrics
    This is a little bit more fiddly than other metrics generators, as we
    need a row for each date. To get around this, we pass a list of domain
    events and applications to the class construtor and then `createReport`
    function accepts an array of dates, we then loop through each date, and
    filter the applications and domain events accordingly.
    
    To get around having to use magic numbers I’ve set the tests up to
    build a Map of Keys, which are mapped to dates and unique users. That
    way we can count all the mapped values for a particular date to get the
    total number of events, and then a count of the keys for a particular
    date gives us the total number of unique users.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/reporting/generator/DailyMetricsReportGeneratorTest.kt
@@ -0,0 +1,243 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.reporting.generator
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.ApplicationAssessedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.ApplicationSubmittedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.BookingMadeEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ApAreaEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ApprovedPremisesApplicationEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.DomainEventEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ProbationRegionEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.UserEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.ApplicationAssessedAssessedByFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.ApplicationAssessedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.ApplicationSubmittedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.BookingMadeBookedByFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.BookingMadeFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.StaffMemberFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.generator.DailyMetricsReportGenerator
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.properties.DailyMetricReportProperties
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.toLocalDateTime
+import java.time.LocalDate
+import java.util.UUID
+
+class DailyMetricsReportGeneratorTest {
+  private val objectMapper = ObjectMapper().apply {
+    registerModule(Jdk8Module())
+    registerModule(JavaTimeModule())
+    registerKotlinModule()
+  }
+
+  private val dates = listOf(
+    LocalDate.parse("2023-01-01"),
+    LocalDate.parse("2023-01-02"),
+    LocalDate.parse("2023-01-03"),
+  )
+
+  private val properties = DailyMetricReportProperties(
+    serviceName = ServiceName.approvedPremises,
+    month = 1,
+    year = 2023,
+  )
+
+  private val user1 = UserEntityFactory()
+    .withYieldedProbationRegion {
+      ProbationRegionEntityFactory()
+        .withYieldedApArea { ApAreaEntityFactory().produce() }
+        .produce()
+    }
+    .produce()
+
+  private val user2 = UserEntityFactory()
+    .withYieldedProbationRegion {
+      ProbationRegionEntityFactory()
+        .withYieldedApArea { ApAreaEntityFactory().produce() }
+        .produce()
+    }
+    .produce()
+
+  private val user3 = UserEntityFactory()
+    .withYieldedProbationRegion {
+      ProbationRegionEntityFactory()
+        .withYieldedApArea { ApAreaEntityFactory().produce() }
+        .produce()
+    }
+    .produce()
+
+  @Test
+  fun `it groups applications and domain events by date`() {
+    val applications = mapOf(
+      dates[0] to mapOf(
+        user1 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user1)
+          .withCreatedAt(dates[0].toLocalDateTime()).produceMany().take(4).toList(),
+        user2 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user2)
+          .withCreatedAt(dates[0].toLocalDateTime()).produceMany().take(1).toList(),
+        user3 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user3)
+          .withCreatedAt(dates[0].toLocalDateTime()).produceMany().take(3).toList(),
+      ),
+      dates[1] to mapOf(
+        user1 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user1)
+          .withCreatedAt(dates[1].toLocalDateTime()).produceMany().take(3).toList(),
+      ),
+      dates[2] to mapOf(
+        user2 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user2)
+          .withCreatedAt(dates[2].toLocalDateTime()).produceMany().take(3).toList(),
+        user3 to ApprovedPremisesApplicationEntityFactory()
+          .withCreatedByUser(user3)
+          .withCreatedAt(dates[2].toLocalDateTime()).produceMany().take(3).toList(),
+      ),
+    )
+
+    val applicationSubmittedEvents = mapOf(
+      dates[0] to mapOf(
+        user1 to createApplicationSubmittedEvents(user1, dates[0], 3),
+        user2 to createApplicationSubmittedEvents(user2, dates[0], 1),
+        user3 to createApplicationSubmittedEvents(user3, dates[0], 2),
+      ),
+      dates[1] to mapOf(
+        user1 to createApplicationSubmittedEvents(user1, dates[1], 3),
+      ),
+      dates[2] to mapOf(
+        user2 to createApplicationSubmittedEvents(user2, dates[2], 1),
+        user3 to createApplicationSubmittedEvents(user3, dates[2], 2),
+      ),
+    )
+
+    val assessmentCompletedEvents = mapOf(
+      dates[0] to mapOf(
+        user3 to createAssessmentCompletedEvents(user3, dates[0], 1),
+      ),
+      dates[1] to mapOf(
+        user1 to createAssessmentCompletedEvents(user1, dates[1], 4),
+        user2 to createAssessmentCompletedEvents(user2, dates[1], 1),
+        user3 to createAssessmentCompletedEvents(user3, dates[1], 3),
+      ),
+    )
+
+    val bookingMadeEvents = mapOf(
+      dates[0] to mapOf(
+        user1 to createBookingMadeEvents(user1, dates[0], 1),
+      ),
+      dates[1] to mapOf(
+        user1 to createBookingMadeEvents(user1, dates[1], 3),
+        user2 to createBookingMadeEvents(user2, dates[1], 3),
+      ),
+      dates[2] to mapOf(
+        user1 to createBookingMadeEvents(user1, dates[2], 4),
+      ),
+    )
+
+    val allApplicationSubmittedEvents = applicationSubmittedEvents.flatMap { events -> events.value.flatMap { it.value } }
+    val allAssessmentCompletedEvents = assessmentCompletedEvents.flatMap { events -> events.value.flatMap { it.value } }
+    val allBookingMadeEvents = bookingMadeEvents.flatMap { events -> events.value.flatMap { it.value } }
+
+    val generator = DailyMetricsReportGenerator(
+      listOf(
+        allApplicationSubmittedEvents,
+        allAssessmentCompletedEvents,
+        allBookingMadeEvents,
+      ).flatten(),
+      applications.flatMap { a -> a.value.flatMap { it.value } },
+      objectMapper,
+    )
+    val results = generator.createReport(dates, properties)
+
+    dates.forEachIndexed { i, date ->
+      assertThat(results[i]["date"]).isEqualTo(date)
+      assertThat(results[i]["applicationsStarted"]).isEqualTo(countEntitiesForDate(applications, date))
+      assertThat(results[i]["uniqueUsersStartingApplications"]).isEqualTo(countUniqueUsersForDate(applications, date))
+      assertThat(results[i]["applicationsSubmitted"]).isEqualTo(countEntitiesForDate(applicationSubmittedEvents, date))
+      assertThat(results[i]["uniqueUsersSubmittingApplications"]).isEqualTo(countUniqueUsersForDate(applicationSubmittedEvents, date))
+      assertThat(results[i]["assessmentsCompleted"]).isEqualTo(countEntitiesForDate(assessmentCompletedEvents, date))
+      assertThat(results[i]["uniqueUsersCompletingAssessments"]).isEqualTo(countUniqueUsersForDate(assessmentCompletedEvents, date))
+      assertThat(results[i]["bookingsMade"]).isEqualTo(countEntitiesForDate(bookingMadeEvents, date))
+      assertThat(results[i]["uniqueUsersMakingBookings"]).isEqualTo(countUniqueUsersForDate(bookingMadeEvents, date))
+    }
+  }
+
+  private fun createApplicationSubmittedEvents(user: UserEntity, date: LocalDate, count: Int) = DomainEventEntityFactory()
+    .withOccurredAt(date.toLocalDateTime())
+    .withType(DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED)
+    .withData(
+      objectMapper.writeValueAsString(
+        ApplicationSubmittedEnvelope(
+          id = UUID.randomUUID(),
+          timestamp = date.toLocalDateTime().toInstant(),
+          eventType = "approved-premises.application.submitted",
+          eventDetails = ApplicationSubmittedFactory()
+            .withSubmittedByStaffMember(
+              StaffMemberFactory()
+                .withStaffIdentifier(
+                  user.deliusStaffIdentifier,
+                )
+                .produce(),
+            )
+            .produce(),
+        ),
+      ),
+    ).produceMany().take(count).toList()
+
+  private fun createAssessmentCompletedEvents(user: UserEntity, date: LocalDate, count: Int) = DomainEventEntityFactory()
+    .withOccurredAt(date.toLocalDateTime())
+    .withType(DomainEventType.APPROVED_PREMISES_APPLICATION_ASSESSED)
+    .withData(
+      objectMapper.writeValueAsString(
+        ApplicationAssessedEnvelope(
+          id = UUID.randomUUID(),
+          timestamp = date.toLocalDateTime().toInstant(),
+          eventType = "approved-premises.application.submitted",
+          eventDetails = ApplicationAssessedFactory()
+            .withAssessedBy(
+              ApplicationAssessedAssessedByFactory()
+                .withStaffMember(
+                  StaffMemberFactory()
+                    .withStaffIdentifier(
+                      user.deliusStaffIdentifier,
+                    ).produce(),
+                ).produce(),
+            )
+            .produce(),
+        ),
+      ),
+    ).produceMany().take(count).toList()
+
+  private fun createBookingMadeEvents(user: UserEntity, date: LocalDate, count: Int) = DomainEventEntityFactory()
+    .withOccurredAt(date.toLocalDateTime())
+    .withType(DomainEventType.APPROVED_PREMISES_BOOKING_MADE)
+    .withData(
+      objectMapper.writeValueAsString(
+        BookingMadeEnvelope(
+          id = UUID.randomUUID(),
+          timestamp = date.toLocalDateTime().toInstant(),
+          eventType = "approved-premises.application.submitted",
+          eventDetails = BookingMadeFactory()
+            .withBookedBy(
+              BookingMadeBookedByFactory()
+                .withStaffMember(
+                  StaffMemberFactory()
+                    .withStaffIdentifier(
+                      user.deliusStaffIdentifier,
+                    ).produce(),
+                ).produce(),
+            )
+            .produce(),
+        ),
+      ),
+    ).produceMany().take(count).toList()
+
+  private fun <T> countEntitiesForDate(events: Map<LocalDate, Map<UserEntity, List<T>>>, date: LocalDate) = events[date]?.flatMap { it.value }?.count() ?: 0
+  private fun <T> countUniqueUsersForDate(events: Map<LocalDate, Map<UserEntity, List<T>>>, date: LocalDate) = events[date]?.keys?.count() ?: 0
+}
