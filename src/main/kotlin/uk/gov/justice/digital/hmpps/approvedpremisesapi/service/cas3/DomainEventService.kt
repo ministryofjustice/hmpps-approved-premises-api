fbe9638a4c2cd347903b4420e6ee183d6bf04f0e
Dan Livings <dan.livings@dxw.com>
2023-10-17 12:22:04 +0100

    Implement service layer for creating and getting 'booking cancelled' events

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas3/DomainEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas3/DomainEventService.kt
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.CAS3BookingCancelledEvent
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.CAS3BookingProvisionallyMadeEvent
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.CAS3Event
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.CAS3PersonArrivedEvent
@@ -50,6 +51,8 @@ class DomainEventService(
       ?: throw MissingTopicException("domainevents not found")
   }
 
+  fun getBookingCancelledEvent(id: UUID) = get<CAS3BookingCancelledEvent>(id)
+
   fun getBookingProvisionallyMadeEvent(id: UUID) = get<CAS3BookingProvisionallyMadeEvent>(id)
 
   fun getPersonArrivedEvent(id: UUID) = get<CAS3PersonArrivedEvent>(id)
@@ -71,6 +74,20 @@ class DomainEventService(
       crn = domainEventEntity.crn,
       occurredAt = domainEventEntity.occurredAt.toInstant(),
       data = data,
+    )
+  }
+
+  @Transactional
+  fun saveBookingCancelledEvent(booking: BookingEntity) {
+    val domainEvent = domainEventBuilder.getBookingCancelledDomainEvent(booking)
+
+    saveAndEmit(
+      domainEvent = domainEvent,
+      typeName = "accommodation.cas3.booking.cancelled",
+      typeDescription = "A booking for a Transitional Accommodation premises has been cancelled",
+      detailUrl = bookingCancelledDetailUrlTemplate.replace("#eventId", domainEvent.id.toString()),
+      crn = domainEvent.data.eventDetails.personReference.crn,
+      nomsNumber = domainEvent.data.eventDetails.personReference.noms,
     )
   }
 
@@ -176,6 +193,7 @@ class DomainEventService(
   }
 
   private fun <T : CAS3Event> enumTypeFromDataType(type: KClass<T>): DomainEventType = when (type) {
+    CAS3BookingCancelledEvent::class -> DomainEventType.CAS3_BOOKING_CANCELLED
     CAS3BookingProvisionallyMadeEvent::class -> DomainEventType.CAS3_BOOKING_PROVISIONALLY_MADE
     CAS3PersonArrivedEvent::class -> DomainEventType.CAS3_PERSON_ARRIVED
     CAS3PersonDepartedEvent::class -> DomainEventType.CAS3_PERSON_DEPARTED
