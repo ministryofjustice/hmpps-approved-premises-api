629ca905e901bfd7390c7d0a96c85e9099d2e781
pezholio <pezholio@gmail.com>
2023-10-18 09:53:45 +0100

    Add a query and interface to get timeliness data
    This adds a simple interface to get all applications submitted
    in a particular month, as well when a booking was made, and
    when the application was submitted. We then get the difference
    in days between when the application was submitted and the booking
    was made to tell us how long it took the application to be asssesd and
    matched (the overallTimeliness), and the difference in days between when
    the application was assessed and the  booking was made to tell us how
    long it took to make a booking (the placementMatchingTimeliness)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/ApplicationTimelinessEntity.kt
@@ -0,0 +1,53 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+import java.sql.Timestamp
+import java.util.UUID
+
+@Repository
+interface ApplicationTimelinessEntityRepository : JpaRepository<ApplicationEntity, UUID> {
+  @Query(
+    """
+    SELECT
+      CAST(application.id as TEXT) as id, 
+      apa.risk_ratings -> 'tier' -> 'value' ->> 'level' as tier,
+      submission_event.occurred_at as applicationSubmittedAt,
+      booking_made_event.occurred_at as bookingMadeAt,
+      DATE_PART(
+        'day',
+        booking_made_event.occurred_at - submission_event.occurred_at
+      ) as overallTimeliness,
+      DATE_PART(
+        'day',
+        booking_made_event.occurred_at - assesment_event.occurred_at
+      ) as placementMatchingTimeliness
+    from
+      applications application
+      left join approved_premises_applications apa on application.id = apa.id
+      left join domain_events submission_event on submission_event.type = 'APPROVED_PREMISES_APPLICATION_SUBMITTED'
+      and application.id = submission_event.application_id
+      left join domain_events assesment_event on assesment_event.type = 'APPROVED_PREMISES_APPLICATION_ASSESSED'
+      and application.id = assesment_event.application_id
+      left join domain_events booking_made_event on booking_made_event.type = 'APPROVED_PREMISES_BOOKING_MADE'
+      and application.id = booking_made_event.application_id
+    where 
+      date_part('month', application.submitted_at) = :month
+      AND date_part('year', application.submitted_at) = :year
+  """,
+    nativeQuery = true,
+  )
+  fun findAllForMonthAndYear(month: Int, year: Int): List<ApplicationTimelinessEntity>
+}
+
+interface ApplicationTimelinessEntity {
+  var overallTimelinessInWorkingDays: Int?
+
+  fun getId(): String
+  fun getTier(): String?
+  fun getApplicationSubmittedAt(): Timestamp?
+  fun getBookingMadeAt(): Timestamp?
+  fun getOverallTimeliness(): Int?
+  fun getPlacementMatchingTimeliness(): Int?
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/ApplicationTimelinessTest.kt
@@ -0,0 +1,141 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.PersonRisksFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationTimelinessEntityRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.RiskTier
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.RiskWithStatus
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+class ApplicationTimelinessTest : IntegrationTestBase() {
+  @Autowired
+  lateinit var applicationTimelinessEntityRepository: ApplicationTimelinessEntityRepository
+
+  @Test()
+  fun `it returns application timeliness data`() {
+    val applicationSchema = approvedPremisesApplicationJsonSchemaEntityFactory.produceAndPersist {
+      withPermissiveSchema()
+    }
+
+    val user = userEntityFactory.produceAndPersist {
+      withProbationRegion(
+        probationRegionEntityFactory.produceAndPersist {
+          withApArea(apAreaEntityFactory.produceAndPersist())
+        },
+      )
+    }
+
+    val month = 4
+    val year = 2022
+
+    val submittedDate = OffsetDateTime.of(LocalDate.of(year, month, 22), LocalTime.MIDNIGHT, ZoneOffset.UTC)
+
+    val risks = PersonRisksFactory()
+      .withTier(
+        RiskWithStatus(
+          RiskTier(
+            level = "A1",
+            lastUpdated = LocalDate.now(),
+          ),
+        ),
+      ).produce()
+
+    val submittedAndBookedApplication = approvedPremisesApplicationEntityFactory.produceAndPersist {
+      withCreatedByUser(user)
+      withApplicationSchema(applicationSchema)
+      withSubmittedAt(submittedDate)
+      withRiskRatings(risks)
+    }
+
+    val submittedAndAssessedApplication = approvedPremisesApplicationEntityFactory.produceAndPersist {
+      withCreatedByUser(user)
+      withApplicationSchema(applicationSchema)
+      withSubmittedAt(submittedDate)
+      withRiskRatings(risks)
+    }
+
+    val submittedAndUnassessedApplication = approvedPremisesApplicationEntityFactory.produceAndPersist {
+      withCreatedByUser(user)
+      withApplicationSchema(applicationSchema)
+      withSubmittedAt(submittedDate)
+      withRiskRatings(risks)
+    }
+
+    val unSubmittedApplication = approvedPremisesApplicationEntityFactory.produceAndPersist {
+      withCreatedByUser(user)
+      withApplicationSchema(applicationSchema)
+      withSubmittedAt(null)
+      withRiskRatings(risks)
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndBookedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED)
+      withOccurredAt(submittedDate)
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndAssessedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED)
+      withOccurredAt(submittedDate)
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndUnassessedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED)
+      withOccurredAt(submittedDate)
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndBookedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_APPLICATION_ASSESSED)
+      withOccurredAt(submittedDate.plusDays(3))
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndAssessedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_APPLICATION_ASSESSED)
+      withOccurredAt(submittedDate.plusDays(4))
+    }
+
+    domainEventFactory.produceAndPersist {
+      withApplicationId(submittedAndBookedApplication.id)
+      withType(DomainEventType.APPROVED_PREMISES_BOOKING_MADE)
+      withOccurredAt(submittedDate.plusDays(22))
+    }
+
+    val result = applicationTimelinessEntityRepository.findAllForMonthAndYear(month, year)
+
+    assertThat(result.size).isEqualTo(3)
+
+    val submittedAndBookedApplicationTimelinessEntity = result.find { it.getId() == submittedAndBookedApplication.id.toString() }
+    assertThat(submittedAndBookedApplicationTimelinessEntity).isNotNull()
+    assertThat(submittedAndBookedApplicationTimelinessEntity!!.getTier()).isEqualTo("A1")
+    assertThat(submittedAndBookedApplicationTimelinessEntity.getApplicationSubmittedAt()!!.toInstant()).isEqualTo(submittedDate.toInstant())
+    assertThat(submittedAndBookedApplicationTimelinessEntity.getBookingMadeAt()!!.toInstant()).isEqualTo(submittedDate.plusDays(22).toInstant())
+    assertThat(submittedAndBookedApplicationTimelinessEntity.getOverallTimeliness()).isEqualTo(22)
+    assertThat(submittedAndBookedApplicationTimelinessEntity.getPlacementMatchingTimeliness()).isEqualTo(19)
+
+    val submittedAndAssessedApplicationTimelinessEntity = result.find { it.getId() == submittedAndAssessedApplication.id.toString() }
+    assertThat(submittedAndAssessedApplicationTimelinessEntity).isNotNull()
+    assertThat(submittedAndAssessedApplicationTimelinessEntity!!.getTier()).isEqualTo("A1")
+    assertThat(submittedAndAssessedApplicationTimelinessEntity.getApplicationSubmittedAt()!!.toInstant()).isEqualTo(submittedDate.toInstant())
+    assertThat(submittedAndAssessedApplicationTimelinessEntity.getBookingMadeAt()).isNull()
+    assertThat(submittedAndAssessedApplicationTimelinessEntity.getOverallTimeliness()).isNull()
+    assertThat(submittedAndAssessedApplicationTimelinessEntity.getPlacementMatchingTimeliness()).isNull()
+
+    val submittedAndUnassessedApplicationTimelinessEntity = result.find { it.getId() == submittedAndUnassessedApplication.id.toString() }
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity).isNotNull()
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity!!.getTier()).isEqualTo("A1")
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity.getApplicationSubmittedAt()!!.toInstant()).isEqualTo(submittedDate.toInstant())
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity.getBookingMadeAt()).isNull()
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity.getOverallTimeliness()).isNull()
+    assertThat(submittedAndUnassessedApplicationTimelinessEntity.getPlacementMatchingTimeliness()).isNull()
+  }
+}
