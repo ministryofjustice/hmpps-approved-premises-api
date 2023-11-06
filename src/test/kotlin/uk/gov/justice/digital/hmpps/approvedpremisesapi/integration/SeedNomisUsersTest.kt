59eac5c5675976229dac71e853592ec9e12d63f7
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-11-06 17:21:38 +0000

    Seed (fake) nomis users
    We can’t currently log in as these prison officers. They are
    purely for associating with applications which will be seeded.
    
    As a further step we could seed these in the AP-Tools
    HMPPS-Auth container as Nomis users with the POM role.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/SeedNomisUsersTest.kt
@@ -0,0 +1,88 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
+
+import io.github.bluegroundltd.kfactory.Factory
+import io.github.bluegroundltd.kfactory.Yielded
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SeedFileType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.seed.CsvBuilder
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringMultiCaseWithNumbers
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class SeedNomisUsersTest : SeedTestBase() {
+  @Test
+  fun `Attempting to seed a fake currently unknown user succeeds`() {
+    withCsv(
+      "unknown-nomis-user",
+      nomisUserSeedCsvRowsToCsv(
+        listOf(
+          NomisUsersSeedCsvRowFactory()
+            .withNomisUsername("ROGER_SMITH_FAKE")
+            .withName("Roger Smith")
+            .withEmail("roger.smith@example.com")
+            .produce(),
+        ),
+      ),
+    )
+
+    seedService.seedData(SeedFileType.nomisUsers, "unknown-nomis-user")
+
+    val persistedUser = nomisUserRepository.findByNomisUsername("ROGER_SMITH_FAKE")
+
+    assertThat(persistedUser).isNotNull
+    assertThat(persistedUser!!.name).isEqualTo("Roger Smith")
+    assertThat(persistedUser!!.email).isEqualTo("roger.smith@example.com")
+  }
+
+  private fun nomisUserSeedCsvRowsToCsv(rows: List<NomisUsersSeedUntypedEnumsCsvRow>):
+    String {
+    val builder = CsvBuilder()
+      .withUnquotedFields(
+        "nomisUsername",
+        "name",
+        "email",
+      )
+      .newRow()
+
+    rows.forEach {
+      builder
+        .withQuotedField(it.nomisUsername)
+        .withQuotedField(it.name)
+        .withQuotedField(it.email)
+        .newRow()
+    }
+
+    return builder.build()
+  }
+}
+
+data class NomisUsersSeedUntypedEnumsCsvRow(
+  val nomisUsername: String,
+  val name: String,
+  val email: String,
+)
+
+class NomisUsersSeedCsvRowFactory : Factory<NomisUsersSeedUntypedEnumsCsvRow> {
+  private var nomisUsername: Yielded<String> = { randomStringMultiCaseWithNumbers(10) }
+  private var name: Yielded<String> = { randomStringMultiCaseWithNumbers(10) }
+  private var email: Yielded<String> = { randomStringMultiCaseWithNumbers(10) }
+
+  fun withNomisUsername(nomisUsername: String) = apply {
+    this.nomisUsername = { nomisUsername }
+  }
+
+  fun withName(name: String) = apply {
+    this.name = { name }
+  }
+
+  fun withEmail(email: String) = apply {
+    this.email = { email }
+  }
+
+  override fun produce() = NomisUsersSeedUntypedEnumsCsvRow(
+    nomisUsername = this.nomisUsername(),
+    name = this.name(),
+    email = this.email(),
+  )
+}
