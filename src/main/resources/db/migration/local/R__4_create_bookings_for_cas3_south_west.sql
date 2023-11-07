3be158a645a8e36a9f1be5cccf01d5c87291433a
Tom Hipkin <tomhipkin@gmail.com>
2023-11-07 16:33:50 +0000

    Seed 11 local provisional bookings
    This will help us test pagination.

--- src/main/resources/db/migration/local/R__4_create_bookings_for_cas3_south_west.sql
+++ src/main/resources/db/migration/local/R__4_create_bookings_for_cas3_south_west.sql
@@ -50,6 +50,486 @@ VALUES
   )
 ON CONFLICT(id) DO NOTHING;
 
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    'c628b111-d1c4-49b5-9b7a-2f4aad9c2755',
+    CURRENT_DATE + (4 * 2) + 1,
+    CURRENT_DATE + (84 * 2) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 2) + 1,
+    CURRENT_DATE + (84 * 2) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '322ed054-9ddc-4430-8f47-3f0e83158b99',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    'fc201bb9-5027-4abe-957b-4f8166df9d82',
+    CURRENT_DATE + (4 * 3) + 1,
+    CURRENT_DATE + (84 * 3) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 3) + 1,
+    CURRENT_DATE + (84 * 3) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    'a2133216-09cf-4381-86d9-c9aa72b77658',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '6ca783e0-7567-4ed9-a88c-ee9cc8735054',
+    CURRENT_DATE + (4 * 4) + 1,
+    CURRENT_DATE + (84 * 4) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 4) + 1,
+    CURRENT_DATE + (84 * 4) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '227c1bff-5e57-4959-bb28-62e129bbdb29',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '06968e0f-1392-452d-a4db-ed54cea93ea5',
+    CURRENT_DATE + (4 * 5) + 1,
+    CURRENT_DATE + (84 * 5) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 5) + 1,
+    CURRENT_DATE + (84 * 5) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '7f0488d1-3fab-4535-89f8-e294a6876e9e',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '7b006f69-8246-4671-8268-3f67743feb23',
+    CURRENT_DATE + (4 * 6) + 1,
+    CURRENT_DATE + (84 * 6) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 6) + 1,
+    CURRENT_DATE + (84 * 6) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    'aa2af6dc-2063-4ac5-93a9-6d3c101a7ad8',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '762c79be-557e-4930-8374-0a5690d93099',
+    CURRENT_DATE + (4 * 7) + 1,
+    CURRENT_DATE + (84 * 7) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 7) + 1,
+    CURRENT_DATE + (84 * 7) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '56451b07-c853-4db1-b440-a282bfe96f06',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    'daa9a8c8-fbb3-4c64-9290-7a1a09c8d035',
+    CURRENT_DATE + (4 * 8) + 1,
+    CURRENT_DATE + (84 * 8) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 8) + 1,
+    CURRENT_DATE + (84 * 8) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '0ac60a27-6a84-4649-8fef-119a03e7a6cd',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    'd18c13c4-4a8c-48a5-9d47-63245a6834b1',
+    CURRENT_DATE + (4 * 9) + 1,
+    CURRENT_DATE + (84 * 9) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 9) + 1,
+    CURRENT_DATE + (84 * 9) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    '141ced0e-a248-4f72-8682-23263dec4891',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '3adc31b5-0af8-4e2c-9fcb-5cc6bd69b8c8',
+    CURRENT_DATE + (4 * 10) + 1,
+    CURRENT_DATE + (84 * 10) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 10) + 1,
+    CURRENT_DATE + (84 * 10) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    'e56aa565-9579-47d5-957a-7887efbb845e',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
+--- Add a Booking arriving soon ---
+
+INSERT INTO
+  bookings (
+    "id",
+    "arrival_date",
+    "departure_date",
+    "crn",
+    "original_arrival_date",
+    "original_departure_date",
+    "premises_id",
+    "bed_id",
+    "service",
+    "created_at",
+    "noms_number"
+  )
+VALUES
+  (
+    '5f2d0864-2790-477f-910b-bbcfa89a352d',
+    CURRENT_DATE + (4 * 11) + 1,
+    CURRENT_DATE + (84 * 11) + 1,
+    'X371199',
+    CURRENT_DATE + (4 * 11) + 1,
+    CURRENT_DATE + (84 * 11) + 1,
+    'b9ae343d-38b4-46db-9ca6-46572a347791',
+    'f45a897a-5831-49d8-81bd-050368fd3bf2',
+    'temporary-accommodation',
+    CURRENT_DATE,
+    NULL
+  )
+ON CONFLICT(id) DO NOTHING;
+
+INSERT INTO
+  turnarounds (
+    "id",
+    "booking_id",
+    "working_day_count",
+    "created_at"
+  )
+VALUES
+  (
+    'd875f408-ba3f-48d7-9a21-20b02f5530de',
+    '1d0f6bcb-b742-4a53-8988-aeb192350824',
+    2,
+    CURRENT_DATE
+  )
+ON CONFLICT(id) DO NOTHING;
+
 --- Add a confirmed booking ---
 
 INSERT INTO
