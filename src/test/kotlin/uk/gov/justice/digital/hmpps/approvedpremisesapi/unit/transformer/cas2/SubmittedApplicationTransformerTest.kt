44a4a2e4c48154e78633fd327a4293b39d247ef7
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-11-09 19:38:44 +0000

    Backfill missing test for summary transformation of Submitted Application

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas2/SubmittedApplicationTransformerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas2/SubmittedApplicationTransformerTest.kt
@@ -10,14 +10,20 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApplicationStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NomisUser
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Person
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.Cas2ApplicationEntityFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.NomisUserEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.NomisUserTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.PersonTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2.SubmittedApplicationTransformer
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringMultiCaseWithNumbers
+import java.sql.Timestamp
+import java.time.Instant
 import java.time.OffsetDateTime
+import java.util.UUID
 
 class SubmittedApplicationTransformerTest {
   private val mockPersonTransformer = mockk<PersonTransformer>()
@@ -73,5 +79,23 @@ class SubmittedApplicationTransformerTest {
       )
     }
   }
-  // transformJpaSummaryToCas2SubmittedSummary
+
+  @Nested
+  inner class TransformJpaSummaryToCas2SubmittedSummary {
+    @Test
+    fun `transforms submitted summary application to API summary representation `() {
+      val applicationSummary = object : Cas2ApplicationSummary {
+        override fun getId() = UUID.fromString("2f838a8c-dffc-48a3-9536-f0e95985e809")
+        override fun getCrn() = randomStringMultiCaseWithNumbers(6)
+        override fun getCreatedByUserId() = UUID.fromString("836a9460-b177-433a-a0d9-262509092c9f")
+        override fun getCreatedAt() = Timestamp(Instant.parse("2023-04-19T13:25:00+01:00").toEpochMilli())
+        override fun getSubmittedAt() = Timestamp(Instant.parse("2023-04-19T13:25:30+01:00").toEpochMilli())
+      }
+
+      val transformation = applicationTransformer.transformJpaSummaryToCas2SubmittedSummary(applicationSummary, mockk())
+
+      assertThat(transformation.id).isEqualTo(applicationSummary.getId())
+      assertThat(transformation.status).isEqualTo(ApplicationStatus.submitted)
+    }
+  }
 }
