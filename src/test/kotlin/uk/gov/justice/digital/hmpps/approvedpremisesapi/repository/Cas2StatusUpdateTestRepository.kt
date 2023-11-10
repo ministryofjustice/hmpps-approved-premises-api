c9c01ca69451e92f8993286a6e66bf648da08b1b
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-11-10 07:05:18 +0000

    Add StatusUpdate factory wiring for integration testing

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/repository/Cas2StatusUpdateTestRepository.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2StatusUpdateEntity
+import java.util.UUID
+
+@Repository
+interface Cas2StatusUpdateTestRepository : JpaRepository<Cas2StatusUpdateEntity, UUID>
