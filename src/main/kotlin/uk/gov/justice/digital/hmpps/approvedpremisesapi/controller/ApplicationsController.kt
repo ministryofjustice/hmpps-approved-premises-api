6061cc2e29b69aedab688292f861dcb601fa808b
Gareth <gareth.hallberg@digital.justice.gov.uk>
2023-11-14 13:46:37 +0000

    Sort field enum amended endpoint amended sql extra tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ApplicationsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ApplicationsController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.ApplicationsApiDelegate
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Application
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApplicationSortField
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApplicationSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApplicationTimelineNote
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Assessment
@@ -83,13 +84,14 @@ class ApplicationsController(
     page: Int?,
     crn: String?,
     sortDirection: SortDirection?,
+    applicationSortField: ApplicationSortField?,
   ): ResponseEntity<List<ApplicationSummary>> {
     if (xServiceName != ServiceName.approvedPremises) {
       throw ForbiddenProblem()
     }
     val user = userService.getUserForRequest()
     val (applications, metadata) =
-      applicationService.getAllApprovedPremisesApplications(page, crn, sortDirection)
+      applicationService.getAllApprovedPremisesApplications(page, crn, sortDirection, applicationSortField)
 
     return ResponseEntity.ok().headers(
       metadata?.toHeaders(),
