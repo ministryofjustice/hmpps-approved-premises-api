a7108a030517e84d6bd29d4317d170105f2f4f74
Harriet H-W <harriethw@gmail.com>
2023-12-01 11:42:02 +0000

    get person details from application in person transformer
    Person details are now stored directly on the application entity

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2PersonSearchTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2PersonSearchTest.kt
@@ -135,7 +135,6 @@ class Cas2PersonSearchTest : IntegrationTestBase() {
                 nomsNumber = "NOMS321",
                 pncNumber = "PNC123",
                 nationality = "English",
-                isRestricted = false,
                 prisonName = "HMP Bristol",
               ),
             ),
