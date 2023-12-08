629a5b45f2d5e811716f6d8ef1bdce4162130e23
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-12-08 16:56:22 +0000

    Refactor ApplicationSubmitted domain event integration test
    There's no change to the tests, we just use the `@Nested`
    annotation to group the 'application.submitted' tests,
    before adding those of the `application.status-updated`
    domain event. This will aid maintainability and navigation.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2DomainEventTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2DomainEventTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas2.model.Cas2ApplicationSubmittedEvent
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas2.model.EventType
@@ -11,75 +12,79 @@ import java.time.Instant
 import java.util.UUID
 
 class Cas2DomainEventTest : IntegrationTestBase() {
-  @Test
-  fun `Get CAS2 Application Submitted Event without JWT returns 401`() {
-    webTestClient.get()
-      .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
-      .exchange()
-      .expectStatus()
-      .isUnauthorized
-  }
 
-  @Test
-  fun `Get CAS2 Application Submitted Event without ROLE_CAS2_EVENTS returns 403`() {
-    val jwt = jwtAuthHelper.createClientCredentialsJwt(
-      username = "username",
-    )
-
-    webTestClient.get()
-      .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isForbidden
-  }
-
-  @Test
-  fun `Get CAS2 Application Submitted Event with only ROLE_APPROVED_PREMISES_EVENTS returns 403`() {
-    val jwt = jwtAuthHelper.createClientCredentialsJwt(
-      username = "username",
-      roles = listOf("ROLE_APPROVED_PREMISES_EVENTS"),
-    )
-
-    webTestClient.get()
-      .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isForbidden
-  }
-
-  @Test
-  fun `Get CAS2 Application Submitted Event returns 200 with correct body`() {
-    val jwt = jwtAuthHelper.createClientCredentialsJwt(
-      username = "username",
-      roles = listOf("ROLE_CAS2_EVENTS"),
-    )
-
-    val eventId = UUID.randomUUID()
-
-    val eventToSave = Cas2ApplicationSubmittedEvent(
-      id = eventId,
-      timestamp = Instant.now(),
-      eventType = EventType.applicationSubmitted,
-      eventDetails = Cas2ApplicationSubmittedEventDetailsFactory().produce(),
-    )
-
-    val event = domainEventFactory.produceAndPersist {
-      withId(eventId)
-      withType(DomainEventType.CAS2_APPLICATION_SUBMITTED)
-      withData(objectMapper.writeValueAsString(eventToSave))
+  @Nested
+  inner class ApplicationSubmitted {
+    @Test
+    fun `without JWT returns 401`() {
+      webTestClient.get()
+        .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
     }
 
-    val response = webTestClient.get()
-      .uri("/events/cas2/application-submitted/${event.id}")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isOk
-      .expectBody(Cas2ApplicationSubmittedEvent::class.java)
-      .returnResult()
+    @Test
+    fun `without ROLE_CAS2_EVENTS returns 403`() {
+      val jwt = jwtAuthHelper.createClientCredentialsJwt(
+        username = "username",
+      )
 
-    assertThat(response.responseBody).isEqualTo(eventToSave)
+      webTestClient.get()
+        .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `with only ROLE_APPROVED_PREMISES_EVENTS returns 403`() {
+      val jwt = jwtAuthHelper.createClientCredentialsJwt(
+        username = "username",
+        roles = listOf("ROLE_APPROVED_PREMISES_EVENTS"),
+      )
+
+      webTestClient.get()
+        .uri("/events/cas2/application-submitted/e4b004f8-bdb2-4bf6-9958-db602be71ed3")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `returns 200 with correct body`() {
+      val jwt = jwtAuthHelper.createClientCredentialsJwt(
+        username = "username",
+        roles = listOf("ROLE_CAS2_EVENTS"),
+      )
+
+      val eventId = UUID.randomUUID()
+
+      val eventToSave = Cas2ApplicationSubmittedEvent(
+        id = eventId,
+        timestamp = Instant.now(),
+        eventType = EventType.applicationSubmitted,
+        eventDetails = Cas2ApplicationSubmittedEventDetailsFactory().produce(),
+      )
+
+      val event = domainEventFactory.produceAndPersist {
+        withId(eventId)
+        withType(DomainEventType.CAS2_APPLICATION_SUBMITTED)
+        withData(objectMapper.writeValueAsString(eventToSave))
+      }
+
+      val response = webTestClient.get()
+        .uri("/events/cas2/application-submitted/${event.id}")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody(Cas2ApplicationSubmittedEvent::class.java)
+        .returnResult()
+
+      assertThat(response.responseBody).isEqualTo(eventToSave)
+    }
   }
 }
