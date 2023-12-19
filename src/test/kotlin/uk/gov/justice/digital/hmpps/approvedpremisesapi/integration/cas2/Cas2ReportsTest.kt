8dfd8287c1e0e7db188adba03983d2d4f4ef0b1d
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-12-19 15:31:04 +0000

    Describe downloaded report

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2ReportsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2ReportsTest.kt
@@ -1,8 +1,16 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
 
+import org.assertj.core.api.Assertions
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.ExcessiveColumns
+import org.jetbrains.kotlinx.dataframe.api.convertTo
+import org.jetbrains.kotlinx.dataframe.api.sortBy
+import org.jetbrains.kotlinx.dataframe.api.toDataFrame
+import org.jetbrains.kotlinx.dataframe.io.readExcel
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.model.Cas2ExampleMetricsRow
 
 class Cas2ReportsTest : IntegrationTestBase() {
 
@@ -86,5 +94,32 @@ class Cas2ReportsTest : IntegrationTestBase() {
         .expectStatus()
         .isOk
     }
+  }
+
+  @Test
+  fun `downloaded report is streamed as a spreadsheet`() {
+    val jwt = jwtAuthHelper.createClientCredentialsJwt(
+      username = "username",
+      authSource = "nomis",
+      roles = listOf("ROLE_PRISON", "ROLE_CAS2_MI"),
+    )
+
+    val expectedDataFrame = listOf(Cas2ExampleMetricsRow(id = "123", data = "example"))
+      .toDataFrame()
+
+    webTestClient.get()
+      .uri("/cas2/reports/example-report")
+      .header("Authorization", "Bearer $jwt")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .consumeWith {
+        val actual = DataFrame
+          .readExcel(it.responseBody!!.inputStream())
+          .convertTo<Cas2ExampleMetricsRow>(ExcessiveColumns.Remove)
+          .sortBy(Cas2ExampleMetricsRow::id)
+        Assertions.assertThat(actual).isEqualTo(expectedDataFrame)
+      }
   }
 }
