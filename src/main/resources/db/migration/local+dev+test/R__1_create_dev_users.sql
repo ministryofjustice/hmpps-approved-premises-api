ead797427379f23dc1802757dad963cd415e6e2d
Ed Davey <ed.davey@digital.justice.gov.uk>
2024-01-23 17:15:55 +0000

    Remove defunct users in local/dev/test environments
    These users don't exist in Delius in our local or dev/test
    environments.

--- src/main/resources/db/migration/local+dev+test/R__1_create_dev_users.sql
+++ src/main/resources/db/migration/local+dev+test/R__1_create_dev_users.sql
@@ -8,9 +8,7 @@ INSERT INTO "users" (id, name, delius_username, delius_staff_identifier, probati
     ('9807de9d-05b3-49e2-84b8-529790a299cc', 'C. Load-Tester', 'CAS-LOAD-TESTER', 2500041004, 'db82d408-d440-4eb5-960b-119cb33427cd', 'STAFFCODE'), -- Kent, Surrey & Sussex
     ('0621c5b0-0028-40b7-87fb-53ec65704314', 'T. Assessor', 'TEMPORARY-ACCOMMODATION-E2E-TESTER', 2500041003, 'db82d408-d440-4eb5-960b-119cb33427cd', 'STAFFCODE'), -- Kent, Surrey & Sussex
     ('7e36a89e-c69e-48b1-a5cf-a7c8949f432a', 'T. Referrer', 'TEMPORARY-ACCOMMODATION-E2E-REFERRER', 2500510725, 'db82d408-d440-4eb5-960b-119cb33427cd', 'STAFFCODE'), -- Kent, Surrey & Sussex
-    ('b5825da0-1553-4398-90ac-6a8e0c8a4cae', 'Tester Testy', 'TESTER.TESTY', 2500043547, 'c5acff6c-d0d2-4b89-9f4d-89a15cfa3891', 'STAFFCODE'), -- North East
-    ('695ba399-c407-4b66-aafc-e8835d72b8a7', 'Bernard Beaks', 'BERNARD.BEAKS', 2500057096, 'ca979718-b15d-4318-9944-69aaff281cad', 'STAFFCODE'), -- East of England
-    ('045b71d3-9845-49b3-a79b-c7799a6bc7bc', 'Panesar Jaspal', 'PANESAR.JASPAL', 2500054544, 'afee0696-8df3-4d9f-9d0c-268f17772e2c', 'STAFFCODE') -- Wales
+    ('695ba399-c407-4b66-aafc-e8835d72b8a7', 'Bernard Beaks', 'BERNARD.BEAKS', 2500057096, 'ca979718-b15d-4318-9944-69aaff281cad', 'STAFFCODE') -- East of England
 ON CONFLICT (id) DO NOTHING;
 
 UPDATE
@@ -103,21 +101,6 @@ ON CONFLICT (id)
 DO
   NOTHING;
 
--- Copy all roles from JIMSNOWLDAP to TESTER.TESTY
-INSERT INTO
-  "user_role_assignments" ("id", "role", "user_id")
-SELECT
-  gen_random_uuid() AS id,
-  role AS role,
-  (SELECT id FROM users where delius_username='TESTER.TESTY') AS user_id
-FROM
-  "user_role_assignments"
-WHERE
-  "user_id" = (SELECT id FROM users where delius_username='JIMSNOWLDAP')
-ON CONFLICT (id)
-DO
-  NOTHING;
-
 -- Copy all roles from JIMSNOWLDAP to BERNARD.BEAKS
 INSERT INTO
   "user_role_assignments" ("id", "role", "user_id")
@@ -133,20 +116,6 @@ ON CONFLICT (id)
 DO
   NOTHING;
 
--- Copy all roles from JIMSNOWLDAP to PANESAR.JASPAL
-INSERT INTO
-  "user_role_assignments" ("id", "role", "user_id")
-SELECT
-  gen_random_uuid() AS id,
-  role AS role,
-  (SELECT id FROM users where delius_username='PANESAR.JASPAL') AS user_id
-FROM
-  "user_role_assignments"
-WHERE
-  "user_id" = (SELECT id FROM users where delius_username='JIMSNOWLDAP')
-ON CONFLICT (id)
-DO
-  NOTHING;
 
 -- Copy all roles from JIMSNOWLDAP to CAS-LOAD-TESTER
 INSERT INTO
