f658332977f25c1e798abc4ee58b3f2fc8610ff4
Ed Davey <ed.davey@digital.justice.gov.uk>
2024-01-25 17:08:27 +0000

    Verify that active status only are exposed through reference-data endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2ReferenceDataTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2ReferenceDataTest.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2ApplicationStatusSeeding
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2PersistedApplicationStatusFinder
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2.ApplicationStatusTransformer
 
 class Cas2ReferenceDataTest : IntegrationTestBase() {
@@ -11,10 +11,13 @@ class Cas2ReferenceDataTest : IntegrationTestBase() {
   @Autowired
   lateinit var statusTransformer: ApplicationStatusTransformer
 
+  @Autowired
+  lateinit var statusFinder: Cas2PersistedApplicationStatusFinder
+
   @Test
   fun `All available application status options are returned`() {
     val expectedStatusOptions = objectMapper.writeValueAsString(
-      Cas2ApplicationStatusSeeding.statusList().map { status -> statusTransformer.transformModelToApi(status) },
+      statusFinder.active().map { status -> statusTransformer.transformModelToApi(status) },
     )
 
     val jwt = jwtAuthHelper.createValidExternalAuthorisationCodeJwt()
