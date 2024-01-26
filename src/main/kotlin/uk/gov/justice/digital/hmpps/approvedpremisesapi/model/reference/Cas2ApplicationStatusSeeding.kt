779353825175d1556efcb89e0dbd65131c75bf00
Harriet H-W <harriethw@gmail.com>
2024-01-26 11:00:20 +0000

    Re-order CAS2 Status updates
    These should be in a certain order for our assessors.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2ApplicationStatusSeeding.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2ApplicationStatusSeeding.kt
@@ -19,16 +19,22 @@ object Cas2ApplicationStatusSeeding {
         description = "The CAS-2 team has the information they need and will make a decision.",
       ),
       Cas2PersistedApplicationStatus(
+        id = UUID.fromString("a919097d-b324-471c-9834-756f255e87ea"),
+        name = "onWaitingList",
+        label = "On waiting list",
+        description = "The applicant has been added to the waiting list for Short-Term Accommodation (CAS-2).",
+      ),
+      Cas2PersistedApplicationStatus(
         id = UUID.fromString("176bbda0-0766-4d77-8d56-18ed8f9a4ef2"),
         name = "placeOffered",
         label = "Place offered",
         description = "The applicant has been offered a place for Short-Term Accommodation (CAS-2).",
       ),
       Cas2PersistedApplicationStatus(
-        id = UUID.fromString("a919097d-b324-471c-9834-756f255e87ea"),
-        name = "onWaitingList",
-        label = "On waiting list",
-        description = "The applicant has been added to the waiting list for Short-Term Accommodation (CAS-2).",
+        id = UUID.fromString("fe254d88-ce1d-4cd8-8bd6-88de88f39019"),
+        name = "offerAccepted",
+        label = "Offer accepted",
+        description = "The accommodation offered has been accepted.",
       ),
       Cas2PersistedApplicationStatus(
         id = UUID.fromString("758eee61-2a6d-46b9-8bdd-869536d77f1b"),
@@ -45,22 +51,16 @@ object Cas2ApplicationStatusSeeding {
         isActive = false,
       ),
       Cas2PersistedApplicationStatus(
-        id = UUID.fromString("004e2419-9614-4c1e-a207-a8418009f23d"),
-        name = "withdrawn",
-        label = "Referral withdrawn",
-        description = "The prison offender manager (POM) withdrew the application.",
-      ),
-      Cas2PersistedApplicationStatus(
-        id = UUID.fromString("fe254d88-ce1d-4cd8-8bd6-88de88f39019"),
-        name = "offerAccepted",
-        label = "Offer accepted",
-        description = "The accommodation offered has been accepted.",
-      ),
-      Cas2PersistedApplicationStatus(
         id = UUID.fromString("9a381bc6-22d3-41d6-804d-4e49f428c1de"),
         name = "offerDeclined",
         label = "Offer declined or withdrawn",
         description = "The accommodation offered has been declined or withdrawn.",
+      ),
+      Cas2PersistedApplicationStatus(
+        id = UUID.fromString("004e2419-9614-4c1e-a207-a8418009f23d"),
+        name = "withdrawn",
+        label = "Referral withdrawn",
+        description = "The prison offender manager (POM) withdrew the application.",
       ),
       Cas2PersistedApplicationStatus(
         id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
