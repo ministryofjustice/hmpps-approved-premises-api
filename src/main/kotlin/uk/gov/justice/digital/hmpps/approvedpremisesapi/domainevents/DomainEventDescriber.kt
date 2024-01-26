9e65f426d0ee06006be0708cff3d40ad18f653eb
Dan Livings <dan.livings@dxw.com>
2024-01-26 18:05:44 +0000

    Implement description generator for domain events
    This will be used by the `ApplicationTimelineTransformer` to retrieve
    the descriptions of domain events.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/domainevents/DomainEventDescriber.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.domainevents
+
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.DomainEvent
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.DomainEventSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.DomainEventService
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.util.UUID
+
+@Component
+class DomainEventDescriber(
+  private val domainEventService: DomainEventService,
+) {
+  fun getDescription(domainEventSummary: DomainEventSummary): String? {
+    return when (domainEventSummary.type) {
+      DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED -> "The application was submitted"
+      DomainEventType.APPROVED_PREMISES_APPLICATION_ASSESSED -> buildApplicationAssessedDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_BOOKING_MADE -> buildBookingMadeDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_PERSON_ARRIVED -> buildPersonArrivedDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_PERSON_NOT_ARRIVED -> buildPersonNotArrivedDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_PERSON_DEPARTED -> buildPersonDepartedDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_BOOKING_NOT_MADE -> buildBookingNotMadeDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_BOOKING_CANCELLED -> buildBookingCancelledDescription(domainEventSummary)
+      DomainEventType.APPROVED_PREMISES_BOOKING_CHANGED -> "The booking had its arrival or departure date changed"
+      DomainEventType.APPROVED_PREMISES_APPLICATION_WITHDRAWN -> "The application was withdrawn"
+      else -> throw IllegalArgumentException("Only CAS1 is currently supported")
+    }
+  }
+
+  private fun buildApplicationAssessedDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getApplicationAssessedDomainEvent(domainEventSummary.id())
+    return event.describe { "The application was assessed and ${it.eventDetails.decision.lowercase()}" }
+  }
+
+  private fun buildBookingMadeDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getBookingMadeEvent(domainEventSummary.id())
+    return event.describe { "A booking was made for between ${it.eventDetails.arrivalOn} and ${it.eventDetails.departureOn}" }
+  }
+
+  private fun buildPersonArrivedDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getPersonArrivedEvent(domainEventSummary.id())
+    return event.describe { "The person moved into the premises on ${LocalDate.ofInstant(it.eventDetails.arrivedAt, ZoneOffset.UTC)}" }
+  }
+
+  private fun buildPersonNotArrivedDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getPersonNotArrivedEvent(domainEventSummary.id())
+    return event.describe { "The person was due to move into the premises on ${it.eventDetails.expectedArrivalOn} but did not arrive" }
+  }
+
+  private fun buildPersonDepartedDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getPersonDepartedEvent(domainEventSummary.id())
+    return event.describe { "The person moved out of the premises on ${LocalDate.ofInstant(it.eventDetails.departedAt, ZoneOffset.UTC)}" }
+  }
+
+  private fun buildBookingNotMadeDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getBookingNotMadeEvent(domainEventSummary.id())
+    return event.describe { "A booking was not made for the placement request. The reason was: ${it.eventDetails.failureDescription}" }
+  }
+
+  private fun buildBookingCancelledDescription(domainEventSummary: DomainEventSummary): String? {
+    val event = domainEventService.getBookingCancelledEvent(domainEventSummary.id())
+    return event.describe { "The booking was cancelled. The reason was: ${it.eventDetails.cancellationReason}" }
+  }
+
+  private fun DomainEventSummary.id(): UUID = UUID.fromString(this.id)
+  private fun <T> DomainEvent<T>?.describe(describe: (T) -> String?): String? = this?.let { describe(it.data) }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/domainevents/DomainEventDescriberTest.kt
@@ -0,0 +1,248 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.domainevents
+
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.ApplicationAssessedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.BookingCancelledEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.BookingMadeEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.BookingNotMadeEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.PersonArrivedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.PersonDepartedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.PersonNotArrivedEnvelope
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.domainevents.DomainEventDescriber
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.ApplicationAssessedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.BookingCancelledFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.BookingMadeFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.BookingNotMadeFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.PersonArrivedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.PersonDepartedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.PersonNotArrivedFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.DomainEvent
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.DomainEventSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.DomainEventService
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.util.UUID
+
+class DomainEventDescriberTest {
+  private val mockDomainEventService = mockk<DomainEventService>()
+
+  private val domainEventDescriber = DomainEventDescriber(mockDomainEventService)
+
+  @Test
+  fun `Returns expected description for application submitted event`() {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_APPLICATION_SUBMITTED)
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The application was submitted")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["accepted", "rejected"])
+  fun `Returns expected description for application assessed event`(decision: String) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_APPLICATION_ASSESSED)
+
+    every { mockDomainEventService.getApplicationAssessedDomainEvent(any()) } returns buildDomainEvent {
+      ApplicationAssessedEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.application.assessed",
+        eventDetails = ApplicationAssessedFactory()
+          .withDecision(decision)
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The application was assessed and $decision")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["2024-01-01,2024-04-01", "2024-01-02,2024-04-02"])
+  fun `Returns expected description for booking made event`(arrivalDate: LocalDate, departureDate: LocalDate) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_BOOKING_MADE)
+
+    every { mockDomainEventService.getBookingMadeEvent(any()) } returns buildDomainEvent {
+      BookingMadeEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.booking.made",
+        eventDetails = BookingMadeFactory()
+          .withArrivalOn(arrivalDate)
+          .withDepartureOn(departureDate)
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("A booking was made for between $arrivalDate and $departureDate")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["2024-01-01", "2024-01-02"])
+  fun `Returns expected description for person arrived event`(arrivalDate: LocalDate) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_PERSON_ARRIVED)
+
+    every { mockDomainEventService.getPersonArrivedEvent(any()) } returns buildDomainEvent {
+      PersonArrivedEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.person.arrived",
+        eventDetails = PersonArrivedFactory()
+          .withArrivedAt(arrivalDate.atTime(12, 34, 56).toInstant(ZoneOffset.UTC))
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The person moved into the premises on $arrivalDate")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["2024-01-01", "2024-01-02"])
+  fun `Returns expected description for person not arrived event`(expectedArrivalDate: LocalDate) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_PERSON_NOT_ARRIVED)
+
+    every { mockDomainEventService.getPersonNotArrivedEvent(any()) } returns buildDomainEvent {
+      PersonNotArrivedEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.person.not-arrived",
+        eventDetails = PersonNotArrivedFactory()
+          .withExpectedArrivalOn(expectedArrivalDate)
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The person was due to move into the premises on $expectedArrivalDate but did not arrive")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["2024-04-01", "2024-04-02"])
+  fun `Returns expected description for person departed event`(departureDate: LocalDate) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_PERSON_DEPARTED)
+
+    every { mockDomainEventService.getPersonDepartedEvent(any()) } returns buildDomainEvent {
+      PersonDepartedEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.person.departed",
+        eventDetails = PersonDepartedFactory()
+          .withDepartedAt(departureDate.atTime(12, 34, 56).toInstant(ZoneOffset.UTC))
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The person moved out of the premises on $departureDate")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["Reason A", "Reason B"])
+  fun `Returns expected description for booking not made event`(reason: String) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_BOOKING_NOT_MADE)
+
+    every { mockDomainEventService.getBookingNotMadeEvent(any()) } returns buildDomainEvent {
+      BookingNotMadeEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.booking.not-made",
+        eventDetails = BookingNotMadeFactory()
+          .withFailureDescription(reason)
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("A booking was not made for the placement request. The reason was: $reason")
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = ["Reason A", "Reason B"])
+  fun `Returns expected description for booking cancelled event`(reason: String) {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_BOOKING_CANCELLED)
+
+    every { mockDomainEventService.getBookingCancelledEvent(any()) } returns buildDomainEvent {
+      BookingCancelledEnvelope(
+        id = it,
+        timestamp = Instant.now(),
+        eventType = "approved-premises.booking.cancelled",
+        eventDetails = BookingCancelledFactory()
+          .withCancellationReason(reason)
+          .produce(),
+      )
+    }
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The booking was cancelled. The reason was: $reason")
+  }
+
+  @Test
+  fun `Returns expected description for booking changed event`() {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_BOOKING_CHANGED)
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The booking had its arrival or departure date changed")
+  }
+
+  @Test
+  fun `Returns expected description for application withdrawn event`() {
+    val domainEventSummary = DomainEventSummaryImpl.ofType(DomainEventType.APPROVED_PREMISES_APPLICATION_WITHDRAWN)
+
+    val result = domainEventDescriber.getDescription(domainEventSummary)
+
+    assertThat(result).isEqualTo("The application was withdrawn")
+  }
+
+  private fun <T> buildDomainEvent(builder: (UUID) -> T): DomainEvent<T> {
+    val id = UUID.randomUUID()
+    val applicationId = UUID.randomUUID()
+
+    return DomainEvent(
+      id = id,
+      applicationId = applicationId,
+      crn = "SOME-CRN",
+      occurredAt = Instant.now(),
+      data = builder(id),
+    )
+  }
+}
+
+data class DomainEventSummaryImpl(
+  override val id: String,
+  override val type: DomainEventType,
+  override val occurredAt: Timestamp,
+  override val applicationId: UUID?,
+  override val assessmentId: UUID?,
+  override val bookingId: UUID?,
+  override val premisesId: UUID?,
+) : DomainEventSummary {
+  companion object {
+    fun ofType(type: DomainEventType) = DomainEventSummaryImpl(
+      id = UUID.randomUUID().toString(),
+      type = type,
+      occurredAt = Timestamp.from(Instant.now()),
+      applicationId = null,
+      assessmentId = null,
+      bookingId = null,
+      premisesId = null,
+    )
+  }
+}
