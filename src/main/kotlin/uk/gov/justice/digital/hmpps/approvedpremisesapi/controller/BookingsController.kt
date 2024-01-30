c6c0664643d494bbac2023a907f11c5a8167b866
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-01-30 11:55:04 +0000

    Add endpoint to get booking without premise id
    As part of this change BookingSearchController has now been renamed to BookingsController as it’s no longer used exclusively for search requests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/BookingsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/BookingsController.kt
@@ -3,19 +3,45 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.BookingsApiDelegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Booking
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingSearchResults
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingSearchSortField
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SortOrder
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.ForbiddenProblem
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.NotFoundProblem
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.BookingSearchService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.BookingService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.BookingSearchResultTransformer
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.BookingTransformer
+import java.util.UUID
 
 @Service
-class BookingSearchController(
+class BookingsController(
   private val bookingSearchService: BookingSearchService,
   private val bookingSearchResultTransformer: BookingSearchResultTransformer,
+  private val bookingService: BookingService,
+  private val bookingTransformer: BookingTransformer,
 ) : BookingsApiDelegate {
+
+  override fun bookingsBookingIdGet(bookingId: UUID): ResponseEntity<Booking> {
+    val bookingAndPersons = when (val result = bookingService.getBooking(bookingId)) {
+      is AuthorisableActionResult.Unauthorised -> throw ForbiddenProblem()
+      is AuthorisableActionResult.NotFound -> throw NotFoundProblem(bookingId, result.entityType!!)
+      is AuthorisableActionResult.Success -> result.entity
+    }
+
+    val apiBooking = bookingTransformer.transformJpaToApi(
+      bookingAndPersons.booking,
+      bookingAndPersons.personInfo,
+      bookingAndPersons.staffMember
+    )
+
+    return ResponseEntity.ok(apiBooking)
+  }
+
   override fun bookingsSearchGet(
     xServiceName: ServiceName,
     status: BookingStatus?,
