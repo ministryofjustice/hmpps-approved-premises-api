0b655aa6e16c84570ca1657f25875e7a6fd560cf
pezholio <pezholio@gmail.com>
2024-02-01 15:43:56 +0000

    Add a new endpoint to withdraw all withdrawables
    This goes through all the withdrawables that an application has and
    withdraws/cancels them. I’ve had to hardcode a UUID for the
    “Withdrawn by PP” booking cancellation reason, which isn’t ideal,
    but there’s prior art in the bookings cancellation service method
    where we check an appeal has been made, and this seems to work OK.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/WithdrawableService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/WithdrawableService.kt
@@ -3,7 +3,12 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesApplicationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PlacementApplicationWithdrawalReason
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PlacementRequestWithdrawalReason
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.Withdrawables
+import java.time.LocalDate
+import java.util.UUID
 
 @Service
 class WithdrawableService(
@@ -12,6 +17,9 @@ class WithdrawableService(
   @Lazy private val bookingService: BookingService,
   @Lazy private val placementApplicationService: PlacementApplicationService,
 ) {
+  val approvedPremisesWithdrawnByPPBookingWithdrawnReasonId: UUID =
+    UUID.fromString("d39572ea-9e42-460c-ae88-b6b30fca0b09")
+
   fun allWithdrawables(application: ApprovedPremisesApplicationEntity): Withdrawables {
     val placementRequests = placementRequestService.getWithdrawablePlacementRequests(application)
     val bookings = bookingService.getCancelleableBookings(application)
@@ -22,5 +30,21 @@ class WithdrawableService(
       bookings = bookings,
       placementApplications = placementApplications,
     )
+  }
+
+  fun withdrawAllForApplication(application: ApprovedPremisesApplicationEntity, user: UserEntity) {
+    val withdrawables = allWithdrawables(application)
+
+    withdrawables.placementApplications.forEach {
+      placementApplicationService.withdrawPlacementApplication(it.id, PlacementApplicationWithdrawalReason.WITHDRAWN_BY_PP)
+    }
+
+    withdrawables.placementRequests.forEach {
+      placementRequestService.withdrawPlacementRequest(it.id, user, PlacementRequestWithdrawalReason.WITHDRAWN_BY_PP)
+    }
+
+    withdrawables.bookings.forEach {
+      bookingService.createCancellation(user, it, LocalDate.now(), approvedPremisesWithdrawnByPPBookingWithdrawnReasonId, "Automatically withdrawn as application was withdrawn")
+    }
   }
 }
