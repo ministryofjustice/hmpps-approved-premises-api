058a560b28ec3cbe9a4f9c6db0e1443da0a90aa6
pezholio <pezholio@gmail.com>
2024-02-02 15:23:14 +0000

    Update Emergency Assessments rule
    Despite what SOMEONE said (i.e. me), we should be allocating Emergency
    applications by config, not by region, as some users may be allocated
    to a region they are not actually in.
    
    This commit basically cribs from a previous commit made by Dan, with
    some slight changes now applications are associated with AP Area, not
    Probation Region.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/allocations/rules/EmergencyAssessmentRule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/allocations/rules/EmergencyAssessmentRule.kt
@@ -2,21 +2,27 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.allocations.rules
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.allocations.UserAllocatorRule
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.allocations.UserAllocatorRuleOutcome
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApAreaEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserQualification
 
 @Component
 @ConditionalOnProperty(name = ["user-allocations.rules.emergency-assessments.enabled"])
 class EmergencyAssessmentRule(
   @Value("\${user-allocations.rules.emergency-assessments.priority:0}")
   override val priority: Int,
+  private val config: EmergencyAssessmentRuleConfig,
 ) : UserAllocatorRule {
   override val name: String
     get() = "Emergency assessments"
+
+  private val allocateToUsersNormalised: Map<String, String> by lazy {
+    config.allocateToUsers.mapKeys { it.key.normalise() }
+  }
 
   override fun evaluateAssessment(assessmentEntity: AssessmentEntity): UserAllocatorRuleOutcome {
     val application = assessmentEntity.application
@@ -25,8 +31,29 @@ class EmergencyAssessmentRule(
     if (application.submittedAt == null) return UserAllocatorRuleOutcome.Skip
 
     return when (application.isEmergencyApplication) {
-      true -> UserAllocatorRuleOutcome.AllocateByQualification(UserQualification.EMERGENCY)
+      true -> allocateByRegion(application.apArea)
       else -> UserAllocatorRuleOutcome.Skip
     }
   }
+
+  private fun allocateByRegion(apArea: ApAreaEntity?): UserAllocatorRuleOutcome {
+    if (apArea == null) return UserAllocatorRuleOutcome.Skip
+
+    val regionName = apArea.name.normalise()
+
+    return when (val userName = allocateToUsersNormalised[regionName]) {
+      null -> UserAllocatorRuleOutcome.Skip
+      else -> UserAllocatorRuleOutcome.AllocateToUser(userName)
+    }
+  }
+
+  private fun String.normalise(): String = this.lowercase()
+    .replace(regex = "[^a-zA-Z -]".toRegex(), "")
+    .replace("[\\s-]+".toRegex(), "-")
 }
+
+@Component
+@ConfigurationProperties(prefix = "user-allocations.rules.emergency-assessments")
+data class EmergencyAssessmentRuleConfig(
+  val allocateToUsers: Map<String, String>,
+)
