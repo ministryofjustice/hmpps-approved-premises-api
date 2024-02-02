f990a4b388e3dc15f01fff540493b43236e02a09
Dan Livings <dan.livings@dxw.com>
2024-02-02 12:53:58 +0000

    Add integration tests for 'get appeal' endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/AppealsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/AppealsTest.kt
@@ -7,12 +7,118 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.AppealDecision
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NewAppeal
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given a User`
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given an Application`
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRole
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.util.withinSeconds
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.roundNanosToMillisToAccountForLossOfPrecisionInPostgres
 import java.time.LocalDate
+import java.time.OffsetDateTime
 import java.util.UUID
 
 class AppealsTest : IntegrationTestBase() {
+  @Test
+  fun `Get appeal without JWT returns 401`() {
+    webTestClient.get()
+      .uri("/applications/${UUID.randomUUID()}/appeals/${UUID.randomUUID()}")
+      .exchange()
+      .expectStatus()
+      .isUnauthorized
+  }
+
+  @Test
+  fun `Get appeal returns 404 when application could not be found`() {
+    `Given a User`(roles = listOf(UserRole.CAS1_APPEALS_MANAGER)) { _, jwt ->
+      webTestClient.get()
+        .uri("/applications/${UUID.randomUUID()}/appeals/${UUID.randomUUID()}")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isNotFound
+    }
+  }
+
+  @Test
+  fun `Get appeal returns 403 when application is not accessible to user`() {
+    `Given a User` { createdByUser, _ ->
+      `Given an Application`(createdByUser) { application ->
+        `Given a User`(roles = listOf(UserRole.CAS1_APPEALS_MANAGER)) { _, jwt ->
+          webTestClient.get()
+            .uri("/applications/${application.id}/appeals/${UUID.randomUUID()}")
+            .header("Authorization", "Bearer $jwt")
+            .exchange()
+            .expectStatus()
+            .isForbidden
+        }
+      }
+    }
+  }
+
+  @Test
+  fun `Get appeal returns 403 when user does not have CAS1_APPEALS_MANAGER role`() {
+    `Given a User` { userEntity, jwt ->
+      `Given an Application`(userEntity) { application ->
+        val appeal = appealEntityFactory.produceAndPersist {
+          withApplication(application as ApprovedPremisesApplicationEntity)
+          withCreatedBy(userEntity)
+        }
+
+        webTestClient.get()
+          .uri("/applications/${application.id}/appeals/${appeal.id}")
+          .header("Authorization", "Bearer $jwt")
+          .exchange()
+          .expectStatus()
+          .isForbidden
+      }
+    }
+  }
+
+  @Test
+  fun `Get appeal returns 404 when appeal could not be found`() {
+    `Given a User`(roles = listOf(UserRole.CAS1_APPEALS_MANAGER)) { userEntity, jwt ->
+      `Given an Application`(userEntity) { application ->
+        webTestClient.get()
+          .uri("/applications/${application.id}/appeals/${UUID.randomUUID()}")
+          .header("Authorization", "Bearer $jwt")
+          .exchange()
+          .expectStatus()
+          .isNotFound
+      }
+    }
+  }
+
+  @Test
+  fun `Get appeal returns 200 with correct body`() {
+    `Given a User`(roles = listOf(UserRole.CAS1_APPEALS_MANAGER)) { userEntity, jwt ->
+      `Given an Application`(userEntity) { application ->
+        val appeal = appealEntityFactory.produceAndPersist {
+          withApplication(application as ApprovedPremisesApplicationEntity)
+          withCreatedBy(userEntity)
+          withCreatedAt(OffsetDateTime.now().roundNanosToMillisToAccountForLossOfPrecisionInPostgres())
+        }
+
+        val result = webTestClient.get()
+          .uri("/applications/${application.id}/appeals/${appeal.id}")
+          .header("Authorization", "Bearer $jwt")
+          .exchange()
+          .expectStatus()
+          .isOk
+          .returnResult(Appeal::class.java)
+
+        assertThat(result.responseBody.blockFirst()).matches {
+          it.appealDate == appeal.appealDate &&
+            it.appealDetail == appeal.appealDetail &&
+            it.reviewer == appeal.reviewer &&
+            it.createdAt == appeal.createdAt.toInstant() &&
+            it.applicationId == application.id &&
+            it.createdByUserId == userEntity.id &&
+            it.decision.value == appeal.decision &&
+            it.decisionDetail == appeal.decisionDetail &&
+            it.assessmentId == null
+        }
+      }
+    }
+  }
+
   @Test
   fun `Create new appeal without JWT returns 401`() {
     webTestClient.post()
