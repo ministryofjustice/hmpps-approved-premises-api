44995f003ce1acee603a175232f264762c232837
Harriet H-W <harriethw@gmail.com>
2024-02-02 10:00:27 +0000

    return `statusDetails` on CAS2 Application Statuses reference data
    We previously introduced a set of CAS2 specific
    Application statuses, that are applied by
    Assessors. We now need to introduce a sub-level of
    category on those statuses, which we are calling
    'status details'.
    
    This returns the status details under their
    corresponding statuses on the reference data
    controller via the `ApplicationStatusTransformer`,
    so that the frontend can request them and display
    as options in a form, and so we have a source for
    which Detail belongs to which Status.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2PersistedApplicationStatusDetail.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2PersistedApplicationStatusDetail.kt
@@ -2,11 +2,9 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference
 
 import java.util.UUID
 
-data class Cas2PersistedApplicationStatus(
+data class Cas2PersistedApplicationStatusDetail(
   val id: UUID,
   val name: String,
   val label: String,
-  val description: String,
-  val statusDetails: List<Cas2PersistedApplicationStatusDetail>? = emptyList(),
   val isActive: Boolean = true,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas2/ApplicationStatusTransformerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas2/ApplicationStatusTransformerTest.kt
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2ApplicationStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2ApplicationStatusDetail
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2PersistedApplicationStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2PersistedApplicationStatusDetail
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2.ApplicationStatusTransformer
 import java.util.UUID
 
@@ -15,26 +17,69 @@ class ApplicationStatusTransformerTest {
   @Nested
   inner class TransformModelToApi {
 
-    @Test
-    fun `returns the expected properties from the internal _model_`() {
-      val internalModel = Cas2PersistedApplicationStatus(
-        id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
-        name = "cancelled",
-        label = "Referral cancelled",
-        description = "The application has been cancelled.",
-        isActive = true,
-      )
-
-      val apiRepresentation = transformer.transformModelToApi(internalModel)
-
-      Assertions.assertThat(apiRepresentation).isEqualTo(
-        Cas2ApplicationStatus(
+    @Nested
+    inner class WhenThereAreStatusDetails {
+      @Test
+      fun `returns the expected properties from the internal _model_`() {
+        val internalModel = Cas2PersistedApplicationStatus(
           id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
           name = "cancelled",
           label = "Referral cancelled",
           description = "The application has been cancelled.",
-        ),
-      )
+          statusDetails = listOf(
+            Cas2PersistedApplicationStatusDetail(
+              id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+              name = "changeOfCircumstances",
+              label = "Change of circumstances",
+            ),
+          ),
+          isActive = true,
+        )
+
+        val apiRepresentation = transformer.transformModelToApi(internalModel)
+
+        Assertions.assertThat(apiRepresentation).isEqualTo(
+          Cas2ApplicationStatus(
+            id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+            name = "cancelled",
+            label = "Referral cancelled",
+            statusDetails = listOf(
+              Cas2ApplicationStatusDetail(
+                id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+                name = "changeOfCircumstances",
+                label = "Change of circumstances",
+              ),
+            ),
+            description = "The application has been cancelled.",
+          ),
+        )
+      }
+    }
+
+    @Nested
+    inner class WhenThereAreNotStatusDetails {
+      @Test
+      fun `returns the expected properties from the internal _model_`() {
+        val internalModel = Cas2PersistedApplicationStatus(
+          id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+          name = "cancelled",
+          label = "Referral cancelled",
+          description = "The application has been cancelled.",
+          isActive = true,
+        )
+
+        val apiRepresentation = transformer.transformModelToApi(internalModel)
+
+        Assertions.assertThat(apiRepresentation).isEqualTo(
+          Cas2ApplicationStatus(
+            id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+            name = "cancelled",
+            label = "Referral cancelled",
+            description = "The application has been cancelled.",
+            statusDetails = emptyList(),
+          ),
+        )
+      }
     }
   }
 }
