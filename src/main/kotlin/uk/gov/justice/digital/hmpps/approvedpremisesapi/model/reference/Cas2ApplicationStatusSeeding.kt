4cbea20aee60d21447ec3dad1f05ffcb4c93aab7
Harriet H-W <harriethw@gmail.com>
2024-02-02 10:00:27 +0000

    return `statusDetails` on CAS2 Application Statuses reference data
    We previously introduced a set of CAS2 specific
    Application statuses, that are applied by
    Assessors. We now need to introduce a sub-level of
    category on those statuses, which we are calling
    'status details'.
    
    This returns the status details under their
    corresponding statuses on the reference data
    controller via the `ApplicationStatusTransformer`,
    so that the frontend can request them and display
    as options in a form, and so we have a source for
    which Detail belongs to which Status.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2ApplicationStatusSeeding.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/reference/Cas2ApplicationStatusSeeding.kt
@@ -55,18 +55,99 @@ object Cas2ApplicationStatusSeeding {
         name = "offerDeclined",
         label = "Offer declined or withdrawn",
         description = "The accommodation offered has been declined or withdrawn.",
+        statusDetails = listOf(
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("62645779-242d-4601-a8f8-d2cbf1d41dfa"),
+            name = "areaUnsuitable",
+            label = "Area unsuitable",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("fc38f750-e9d2-4270-b542-d38286b9855c"),
+            name = "changeOfCircumstances",
+            label = "Change of circumstances",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("31122b89-e087-4b5f-b59a-f7ffa0dd3e0c"),
+            name = "noResponse",
+            label = "No response",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("32e62af3-6ea5-4496-a82c-b7bad67080a5"),
+            name = "offerWithdrawnByNacro",
+            label = "Offer withdrawn by Nacro",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("b5bfbff4-aaa6-4fb0-ba36-5bca58927dc5"),
+            name = "propertyUnsuitable",
+            label = "Property unsuitable for needs",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("f58267b8-f91b-4a4f-9aa2-80089ba111e4"),
+            name = "withdrawnByReferrer",
+            label = "Withdrawn by referrer",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("ed5d529c-d7d1-4f29-a0c0-89fd104cc320"),
+            name = "rehousedByAnotherLandlord",
+            label = "Rehoused by another landlord",
+          ),
+        ),
       ),
       Cas2PersistedApplicationStatus(
         id = UUID.fromString("004e2419-9614-4c1e-a207-a8418009f23d"),
         name = "withdrawn",
         label = "Referral withdrawn",
         description = "The prison offender manager (POM) withdrew the application.",
+        statusDetails = listOf(
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("e4a2391e-e847-427a-a913-51e0b0ad9f52"),
+            name = "hdcNoLongerEligible",
+            label = "HDC - No longer eligible",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("c5dce0d2-fc05-4a07-8157-25b8821cdb06"),
+            name = "governorDecidedUnsuitable",
+            label = "Governor - Decided unsuitable",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("3fb37f85-be88-4eee-812d-af122e268eef"),
+            name = "governorChosenAlternative",
+            label = "Governor - Chosen alternative accommodation",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("8a857a7d-94f9-43ec-963c-2a2528e88a6e"),
+            name = "governorOther",
+            label = "Governor - Other",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("d3c789b8-947d-4e24-9cef-335545d85abe"),
+            name = "withdrewOrDeclinedOffer",
+            label = "Withdrew or declined offer",
+          ),
+        ),
       ),
       Cas2PersistedApplicationStatus(
         id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
         name = "cancelled",
         label = "Referral cancelled",
         description = "The application has been cancelled.",
+        statusDetails = listOf(
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+            name = "assessedAsHighRisk",
+            label = "Assessed as High Risk",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+            name = "notEligible",
+            label = "Not Eligible",
+          ),
+          Cas2PersistedApplicationStatusDetail(
+            id = UUID.fromString("f13bbdd6-44f1-4362-b9d3-e6f1298b1bf9"),
+            name = "noRecourseToPublicFunds",
+            label = "No Recourse to Public Funds",
+          ),
+        ),
       ),
       Cas2PersistedApplicationStatus(
         id = UUID.fromString("89458555-3219-44a2-9584-c4f715d6b565"),
