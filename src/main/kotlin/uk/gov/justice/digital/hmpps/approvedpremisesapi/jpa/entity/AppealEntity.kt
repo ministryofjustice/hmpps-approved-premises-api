919016697dd79e0ff33883b50df3887f915fca4c
Dan Livings <dan.livings@dxw.com>
2024-02-26 16:12:48 +0000

    Add `AppealRepository.findAllByAssessmentId` method
    This will allow the application and placement reports to retrieve the
    appropriate assessment for performing assertions, as when an appeal is
    accepted the latest assessment is the one that gets automatically
    created, rather than the one that rejected the application (which is the
    one needed to assert on).

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/AppealEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/AppealEntity.kt
@@ -14,6 +14,7 @@ import javax.persistence.Table
 @Repository
 interface AppealRepository : JpaRepository<AppealEntity, UUID> {
   fun findByApplication(application: ApplicationEntity): List<AppealEntity>
+  fun findAllByAssessmentId(assessmentId: UUID): List<AppealEntity>
 }
 
 @Entity
