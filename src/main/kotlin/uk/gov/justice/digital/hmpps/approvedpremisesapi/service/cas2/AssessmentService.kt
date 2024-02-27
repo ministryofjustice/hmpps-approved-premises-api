723f983a2a1c5b269275f8dac64e5d5af16213aa
Harriet H-W <harriethw@gmail.com>
2024-02-27 13:10:11 +0000

    service method to update an Assessment

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/AssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/AssessmentService.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.UpdateCas2Assessment
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2AssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2AssessmentRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.ValidatableActionResult
 import java.time.OffsetDateTime
 import java.util.UUID
 import javax.transaction.Transactional
@@ -22,4 +26,20 @@ class AssessmentService(
         application = cas2ApplicationEntity,
       ),
     )
+
+  fun updateAssessment(assessmentId: UUID, newAssessment: UpdateCas2Assessment): AuthorisableActionResult<ValidatableActionResult<Cas2AssessmentEntity>> {
+    val assessmentEntity = assessmentRepository.findByIdOrNull(assessmentId)
+      ?: return AuthorisableActionResult.NotFound()
+
+    assessmentEntity.apply {
+      this.nacroReferralId = newAssessment.nacroReferralId
+      this.assessorName = newAssessment.assessorName
+    }
+
+    val savedAssessment = assessmentRepository.save(assessmentEntity)
+
+    return AuthorisableActionResult.Success(
+      ValidatableActionResult.Success(savedAssessment),
+    )
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas2/AssessmentServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas2/AssessmentServiceTest.kt
@@ -7,10 +7,14 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.times
+import org.springframework.data.repository.findByIdOrNull
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.UpdateCas2Assessment
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.Cas2ApplicationEntityFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.NomisUserEntityFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2AssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2AssessmentRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.ValidatableActionResult
 import java.time.OffsetDateTime
 import java.util.UUID
 
@@ -52,6 +56,81 @@ class AssessmentServiceTest {
           match { it.application == application },
         )
       }
+    }
+  }
+
+  @Nested
+  inner class UpdateAssessment {
+
+    @Test
+    fun `saves and returns entity from db`() {
+      val assessmentId = UUID.randomUUID()
+      val application = Cas2ApplicationEntityFactory()
+        .withCreatedByUser(
+          NomisUserEntityFactory()
+            .produce(),
+        ).produce()
+      val assessEntity = Cas2AssessmentEntity(
+        id = assessmentId,
+        application = application,
+        createdAt = OffsetDateTime.now(),
+      )
+
+      val newAssessmentData = UpdateCas2Assessment(
+        nacroReferralId = "1234OH",
+        assessorName = "Anne Assessor",
+      )
+
+      every { mockAssessmentRepository.save(any()) } answers
+        {
+          assessEntity
+        }
+
+      every { mockAssessmentRepository.findByIdOrNull(assessmentId) } answers
+        {
+          assessEntity
+        }
+
+      val result = assessmentService.updateAssessment(
+        assessmentId = assessmentId,
+        newAssessment = newAssessmentData,
+      )
+      Assertions.assertThat(result).isEqualTo(
+        AuthorisableActionResult.Success(
+          ValidatableActionResult.Success(assessEntity),
+        ),
+      )
+
+      verify(exactly = 1) {
+        mockAssessmentRepository.save(
+          match {
+            it.id == assessEntity.id &&
+              it.nacroReferralId == newAssessmentData.nacroReferralId &&
+              it.assessorName == newAssessmentData.assessorName
+          },
+        )
+      }
+    }
+
+    @Test
+    fun `returns NotFound if entity is not found`() {
+      val assessmentId = UUID.randomUUID()
+      val newAssessmentData = UpdateCas2Assessment(
+        nacroReferralId = "1234OH",
+        assessorName = "Anne Assessor",
+      )
+
+      every { mockAssessmentRepository.findByIdOrNull(assessmentId) } answers
+        {
+          null
+        }
+
+      val result = assessmentService.updateAssessment(
+        assessmentId = assessmentId,
+        newAssessment = newAssessmentData,
+      )
+
+      Assertions.assertThat(result is AuthorisableActionResult.NotFound).isTrue
     }
   }
 }
