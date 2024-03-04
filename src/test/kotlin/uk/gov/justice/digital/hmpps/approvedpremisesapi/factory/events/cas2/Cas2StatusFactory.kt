c4c4bee60d58165a7343af1a4fd0ea02854571cc
Danie Humphreys <daniel.humphreys@digital.justice.gov.uk>
2024-03-04 12:26:33 +0000

    Added status details to persisted domain event factory object so that application status update GET test ensures they are returned.  Addresses part [2] of the ticket (see description).

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/events/cas2/Cas2StatusFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/events/cas2/Cas2StatusFactory.kt
@@ -3,11 +3,17 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.events.cas2
 import io.github.bluegroundltd.kfactory.Factory
 import io.github.bluegroundltd.kfactory.Yielded
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas2.model.Cas2Status
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas2.model.StatusDetailItem
 
 class Cas2StatusFactory : Factory<Cas2Status> {
   private var name: Yielded<String> = { "moreInfoRequested" }
   private var label: Yielded<String> = { "More information requested" }
   private var description: Yielded<String> = { "More information about the application has been requested" }
+  private var statusDetails = listOf(
+    StatusDetailItem("exclusionZonesAndAreas", "Exclusion zones and preferred areas"),
+    StatusDetailItem("riskOfSeriousHarm", "Risk of serious harm"),
+    StatusDetailItem("hdcAndCpp", "HDC licence and CPP details"),
+  )
 
   fun withName(name: String) = apply {
     this.name = { name }
@@ -17,5 +23,6 @@ class Cas2StatusFactory : Factory<Cas2Status> {
     name = this.name(),
     label = this.label(),
     description = this.description(),
+    statusDetails = this.statusDetails
   )
 }
