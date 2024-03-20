d49b816dccd24f4ba0394bad4c97c08c38c225a4
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-03-20 12:56:38 +0000

    Add function to make constant names readable
    This will be required when showing withdrawal reasons for placement requests and placement applicaitons

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/util/StringHelpers.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/util/StringHelpers.kt
@@ -6,3 +6,8 @@ fun String.kebabCaseToPascalCase(): String {
   val pattern = "-[a-z]".toRegex()
   return replace(pattern) { it.value.last().uppercase() }.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 }
+
+fun String.javaConstantNameToSentence():
+  String = replace("_", " ")
+  .lowercase()
+  .replaceFirstChar { it.uppercase() }
