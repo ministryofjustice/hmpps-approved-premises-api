ba637ae2d7ccbcb1170cd6263ad350bf7d4a08b3
Sudhakar Savariappan <sudhakarsavariappan@yahoo.com>
2024-03-26 12:38:49 +0000

    [CAS-234]-Refactor to accept start and endate to support both old and new report API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/properties/BedUsageReportProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/properties/BedUsageReportProperties.kt
@@ -1,11 +1,12 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.properties
 
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
+import java.time.LocalDate
 import java.util.UUID
 
 data class BedUsageReportProperties(
   val serviceName: ServiceName,
   val probationRegionId: UUID?,
-  val year: Int,
-  val month: Int,
+  val startDate: LocalDate,
+  val endDate: LocalDate,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/properties/TransitionalAccommodationReferralReportProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/properties/TransitionalAccommodationReferralReportProperties.kt
@@ -1,11 +1,10 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.reporting.properties
 
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
+import java.time.LocalDate
 import java.util.UUID
 
 data class TransitionalAccommodationReferralReportProperties(
-  val serviceName: ServiceName,
   val probationRegionId: UUID?,
-  val year: Int,
-  val month: Int,
+  val startDate: LocalDate,
+  val endDate: LocalDate,
 )
