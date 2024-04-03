5b161e18d9e6397c06a027d1d05da7f050852107
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-04-03 14:23:01 +0100

    In dev/test update user ap_area_id using migration
    Without this change local users need to completely destroy their databases for the ap area ids to be correctly populated, which is not ideal

---
+++ src/main/resources/db/migration/local+dev+test/20240403141500__add_ap_area_id_to_users.sql
@@ -0,0 +1,16 @@
+-- JIMSNOWLDAP, South West
+UPDATE users set ap_area_id = '2cc6bc0f-58cf-4d82-99dd-dfc67b7f5c33' where id = 'aa30f20a-84e3-4baa-bef0-3c9bd51879ad';
+-- APPROVEDPREMISESTESTUSER, East of England
+UPDATE users set ap_area_id = '858a01cf-9fd1-482c-a983-1a74713f9227' where id = 'f29b6402-b5f0-4f39-a66f-a2bb13eb5d6d';
+-- CAS-LOAD-TESTER, East of England
+UPDATE users set ap_area_id = '858a01cf-9fd1-482c-a983-1a74713f9227' where id = '9807de9d-05b3-49e2-84b8-529790a299cc';
+-- TEMPORARY-ACCOMMODATION-E2E-TESTER, East of England
+UPDATE users set ap_area_id = '858a01cf-9fd1-482c-a983-1a74713f9227' where id = '0621c5b0-0028-40b7-87fb-53ec65704314';
+-- TEMPORARY-ACCOMMODATION-E2E-REFERRER, East of England
+UPDATE users set ap_area_id = '858a01cf-9fd1-482c-a983-1a74713f9227' where id = '7e36a89e-c69e-48b1-a5cf-a7c8949f432a';
+-- TESTER.TESTY, North East
+UPDATE users set ap_area_id = '6025b8c6-883f-4300-bc26-2b01fbd6e272' where id = 'b5825da0-1553-4398-90ac-6a8e0c8a4cae';
+-- BERNARD.BEAKS, East of England
+UPDATE users set ap_area_id = '858a01cf-9fd1-482c-a983-1a74713f9227' where id = '695ba399-c407-4b66-aafc-e8835d72b8a7';
+-- PANESAR.JASPAL, Wales
+UPDATE users set ap_area_id = '56d78348-64d4-4af2-a13c-e5c0d4d30dd0' where id = '045b71d3-9845-49b3-a79b-c7799a6bc7bc';
