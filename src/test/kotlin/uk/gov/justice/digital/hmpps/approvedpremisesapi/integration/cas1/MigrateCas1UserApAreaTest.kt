176b2a361d80ef1091c79043a1fcecb9753ee156
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-04-03 15:16:07 +0100

    Update ApArea backfill to always populate ap area
    Previously the ap area was not populated if the staff details could not be found. Because the user.ap_area_id is required to show the users on the users page, we really need this to be populated with something, even for inactive users.
    
    This change updates the ap area backfill to fall back to probationRegion.apArea where the staff user details cannot be retrieved

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas1/MigrateCas1UserApAreaTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas1/MigrateCas1UserApAreaTest.kt
@@ -40,4 +40,36 @@ class MigrateCas1UserApAreaTest : MigrationJobTestBase() {
       assertThat(updatedUser.teamCodes).isEqualTo(listOf("abc"))
     }
   }
+
+  @Test
+  fun `If staff details not found for user (404), fall back to probation region ap area and set team codes to an empty list`() {
+    val apArea = apAreaEntityFactory.produceAndPersist()
+
+    val probationRegion = probationRegionEntityFactory.produceAndPersist {
+      withDefaults()
+      withApArea(apArea)
+    }
+
+    `Given a User`(
+      probationRegion = probationRegion,
+      staffUserDetailsConfigBlock = {
+        this.withTeams(
+          listOf(StaffUserTeamMembershipFactory().withCode("abc").produce()),
+        )
+      },
+      mockStaffUserDetailsCall = false,
+    ) { userEntity, _ ->
+
+      userEntity.apArea = null
+      userRepository.save(userEntity)
+      assertThat(userEntity.apArea).isNull()
+      assertThat(userEntity.teamCodes).isNull()
+
+      migrationJobService.runMigrationJob(MigrationJobType.cas1BackfillUserApArea)
+
+      val updatedUser = userRepository.findByIdOrNull(userEntity.id)!!
+      assertThat(updatedUser.apArea!!.id).isEqualTo(apArea.id)
+      assertThat(updatedUser.teamCodes).isEmpty()
+    }
+  }
 }
