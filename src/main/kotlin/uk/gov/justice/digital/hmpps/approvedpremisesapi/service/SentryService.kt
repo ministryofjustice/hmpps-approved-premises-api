f25e74fc55b2aa4df3b221be059a339bb7cd17d0
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-04-09 16:14:02 +0100

    Raise error if allocator rule returns invalid user
    Whilst such issues are logged at warn level, these are only useful once we know there is an issue (i.e. the user has informed us). This commit also raises a message in sentry so we can immediately investigate and rectify such issues.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/SentryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/SentryService.kt
@@ -1,11 +1,16 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 
 import io.sentry.Sentry
+import io.sentry.SentryLevel
 import org.springframework.stereotype.Service
 
 @Service
 class SentryService {
   fun captureException(throwable: Throwable) {
     Sentry.captureException(throwable)
+  }
+
+  fun captureErrorMessage(message: String) {
+    Sentry.captureMessage(message, SentryLevel.ERROR)
   }
 }
