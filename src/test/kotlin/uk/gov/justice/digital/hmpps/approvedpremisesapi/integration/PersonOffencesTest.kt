8377abe40b14a9c8ced3d5c67054c9475956c4c7
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-04-19 16:02:23 +0100

    Nest PersonOffenceTest
    In preparation for adding tests for a v2 endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/PersonOffencesTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/PersonOffencesTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ConvictionFactory
@@ -14,119 +15,126 @@ class PersonOffencesTest : InitialiseDatabasePerClassTestBase() {
   @Autowired
   lateinit var convictionTransformer: ConvictionTransformer
 
-  @Test
-  fun `Getting offences by CRN without a JWT returns 401`() {
-    webTestClient.get()
-      .uri("/people/CRN/offences")
-      .exchange()
-      .expectStatus()
-      .isUnauthorized
-  }
+  @Nested
+  inner class GetOffence {
 
-  @Test
-  fun `Getting offences for a CRN with a non-Delius JWT returns 403`() {
-    val jwt = jwtAuthHelper.createClientCredentialsJwt(
-      username = "username",
-      authSource = "nomis",
-    )
+    @Test
+    fun `Getting offences by CRN without a JWT returns 401`() {
+      webTestClient.get()
+        .uri("/people/CRN/offences")
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
 
-    webTestClient.get()
-      .uri("/people/CRN/offences")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isForbidden
-  }
-
-  @Test
-  fun `Getting offences for a CRN without ROLE_PROBATION returns 403`() {
-    val jwt = jwtAuthHelper.createAuthorizationCodeJwt(
-      subject = "username",
-      authSource = "delius",
-    )
-
-    webTestClient.get()
-      .uri("/people/CRN/offences")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isForbidden
-  }
-
-  @Test
-  fun `Getting offences for a CRN that does not exist returns 404`() {
-    `Given a User` { userEntity, jwt ->
-      val crn = "CRN123"
-
-      CommunityAPI_mockNotFoundOffenderDetailsCall(crn)
-      loadPreemptiveCacheForOffenderDetails(crn)
+    @Test
+    fun `Getting offences for a CRN with a non-Delius JWT returns 403`() {
+      val jwt = jwtAuthHelper.createClientCredentialsJwt(
+        username = "username",
+        authSource = "nomis",
+      )
 
       webTestClient.get()
-        .uri("/people/$crn/offences")
+        .uri("/people/CRN/offences")
         .header("Authorization", "Bearer $jwt")
         .exchange()
         .expectStatus()
-        .isNotFound
+        .isForbidden
     }
-  }
 
-  @Test
-  fun `Getting offences for a CRN returns OK with correct body`() {
-    `Given a User` { userEntity, jwt ->
-      `Given an Offender` { offenderDetails, inmateDetails ->
+    @Test
+    fun `Getting offences for a CRN without ROLE_PROBATION returns 403`() {
+      val jwt = jwtAuthHelper.createAuthorizationCodeJwt(
+        subject = "username",
+        authSource = "delius",
+      )
 
-        val activeConviction = ConvictionFactory()
-          .withConvictionId(12345)
-          .withIndex("5")
-          .withActive(true)
-          .withOffences(
-            listOf(
-              OffenceFactory()
-                .withOffenceId("1")
-                .withMainCategoryDescription("Main Category 1")
-                .withSubCategoryDescription("Sub Category 1")
-                .produce(),
-              OffenceFactory()
-                .withOffenceId("2")
-                .withMainCategoryDescription("Main Category 2")
-                .withSubCategoryDescription("Sub Category 2")
-                .produce(),
-            ),
-          )
-          .produce()
+      webTestClient.get()
+        .uri("/people/CRN/offences")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
 
-        val inactiveConviction = ConvictionFactory()
-          .withConvictionId(6789)
-          .withIndex("2")
-          .withActive(false)
-          .withOffences(
-            listOf(
-              OffenceFactory()
-                .withOffenceId("3")
-                .withMainCategoryDescription("Main Category 1")
-                .withSubCategoryDescription("Sub Category 1")
-                .produce(),
-              OffenceFactory()
-                .withOffenceId("4")
-                .withMainCategoryDescription("Main Category 2")
-                .withSubCategoryDescription("Sub Category 2")
-                .produce(),
-            ),
-          )
-          .produce()
+    @Test
+    fun `Getting offences for a CRN that does not exist returns 404`() {
+      `Given a User` { userEntity, jwt ->
+        val crn = "CRN123"
 
-        CommunityAPI_mockSuccessfulConvictionsCall(offenderDetails.otherIds.crn, listOf(activeConviction, inactiveConviction))
+        CommunityAPI_mockNotFoundOffenderDetailsCall(crn)
+        loadPreemptiveCacheForOffenderDetails(crn)
 
         webTestClient.get()
-          .uri("/people/${offenderDetails.otherIds.crn}/offences")
+          .uri("/people/$crn/offences")
           .header("Authorization", "Bearer $jwt")
           .exchange()
           .expectStatus()
-          .isOk
-          .expectBody()
-          .json(
-            objectMapper.writeValueAsString(convictionTransformer.transformToApi(activeConviction)),
+          .isNotFound
+      }
+    }
+
+    @Test
+    fun `Getting offences for a CRN returns OK with correct body`() {
+      `Given a User` { userEntity, jwt ->
+        `Given an Offender` { offenderDetails, inmateDetails ->
+
+          val activeConviction = ConvictionFactory()
+            .withConvictionId(12345)
+            .withIndex("5")
+            .withActive(true)
+            .withOffences(
+              listOf(
+                OffenceFactory()
+                  .withOffenceId("1")
+                  .withMainCategoryDescription("Main Category 1")
+                  .withSubCategoryDescription("Sub Category 1")
+                  .produce(),
+                OffenceFactory()
+                  .withOffenceId("2")
+                  .withMainCategoryDescription("Main Category 2")
+                  .withSubCategoryDescription("Sub Category 2")
+                  .produce(),
+              ),
+            )
+            .produce()
+
+          val inactiveConviction = ConvictionFactory()
+            .withConvictionId(6789)
+            .withIndex("2")
+            .withActive(false)
+            .withOffences(
+              listOf(
+                OffenceFactory()
+                  .withOffenceId("3")
+                  .withMainCategoryDescription("Main Category 1")
+                  .withSubCategoryDescription("Sub Category 1")
+                  .produce(),
+                OffenceFactory()
+                  .withOffenceId("4")
+                  .withMainCategoryDescription("Main Category 2")
+                  .withSubCategoryDescription("Sub Category 2")
+                  .produce(),
+              ),
+            )
+            .produce()
+
+          CommunityAPI_mockSuccessfulConvictionsCall(
+            offenderDetails.otherIds.crn,
+            listOf(activeConviction, inactiveConviction),
           )
+
+          webTestClient.get()
+            .uri("/people/${offenderDetails.otherIds.crn}/offences")
+            .header("Authorization", "Bearer $jwt")
+            .exchange()
+            .expectStatus()
+            .isOk
+            .expectBody()
+            .json(
+              objectMapper.writeValueAsString(convictionTransformer.transformToApi(activeConviction)),
+            )
+        }
       }
     }
   }
