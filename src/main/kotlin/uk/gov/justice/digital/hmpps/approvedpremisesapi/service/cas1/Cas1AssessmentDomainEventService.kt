f1f335d6a729ebddb0fa7893e9ec76828a4258c1
pezholio <pezholio@gmail.com>
2024-04-24 09:55:00 +0100

    Add a `furtherInformationRequested` function
    This has an `emit` argument, which default to `true`. This will allow
    us to backfill domain events in the next commit

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/Cas1AssessmentDomainEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/Cas1AssessmentDomainEventService.kt
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.AssessmentAllocated
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.AssessmentAllocatedEnvelope
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.EventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.FurtherInformationRequested
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.FurtherInformationRequestedEnvelope
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.model.PersonReference
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ClientResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.CommunityApiClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentClarificationNoteEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.DomainEvent
@@ -69,5 +72,51 @@ class Cas1AssessmentDomainEventService(
         ),
       ),
     )
+  }
+
+  fun furtherInformationRequested(assessment: AssessmentEntity, clarificationNoteEntity: AssessmentClarificationNoteEntity, emit: Boolean = true) {
+    val requesterStaffDetails = when (val result = communityApiClient.getStaffUserDetails(clarificationNoteEntity.createdByUser.deliusUsername)) {
+      is ClientResult.Success -> result.body
+      is ClientResult.Failure -> result.throwException()
+    }
+
+    val recipientStaffDetails = when (val result = communityApiClient.getStaffUserDetails(assessment.application.createdByUser.deliusUsername)) {
+      is ClientResult.Success -> result.body
+      is ClientResult.Failure -> result.throwException()
+    }
+
+    val id = UUID.randomUUID()
+    val occurredAt = Instant.now()
+
+    val data = FurtherInformationRequestedEnvelope(
+      id = id,
+      timestamp = occurredAt,
+      eventType = EventType.informationRequestMade,
+      eventDetails = FurtherInformationRequested(
+        assessmentId = assessment.id,
+        assessmentUrl = assessmentUrlTemplate.resolve("id", assessment.id.toString()),
+        applicationId = assessment.application.id,
+        applicationUrl = applicationUrlTemplate.resolve("id", assessment.application.id.toString()),
+        personReference = PersonReference(
+          crn = assessment.application.crn,
+          noms = assessment.application.nomsNumber ?: "Unknown NOMS Number",
+        ),
+        requestedAt = Instant.now(),
+        requester = requesterStaffDetails.toStaffMember(),
+        recipient = recipientStaffDetails.toStaffMember(),
+        requestId = clarificationNoteEntity.id,
+      ),
+    )
+
+    val domainEvent = DomainEvent(
+      id = id,
+      applicationId = assessment.application.id,
+      assessmentId = assessment.id,
+      crn = assessment.application.crn,
+      occurredAt = occurredAt,
+      data = data,
+    )
+
+    domainEventService.saveFurtherInformationRequestedEvent(domainEvent, emit)
   }
 }
