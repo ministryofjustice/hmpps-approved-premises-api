2ee7051fee497e5f4f27bb3ebffdd176d9936329
Muhammad El Abdulla <>
2024-05-01 13:40:12 +0100

    Add order by to referral rejection reasons

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/ReferralRejectionReasonEntityFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/ReferralRejectionReasonEntityFactory.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.factory
 import io.github.bluegroundltd.kfactory.Factory
 import io.github.bluegroundltd.kfactory.Yielded
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ReferralRejectionReasonEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomInt
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringMultiCaseWithNumbers
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringUpperCase
 import java.util.UUID
@@ -12,6 +13,7 @@ class ReferralRejectionReasonEntityFactory : Factory<ReferralRejectionReasonEnti
   private var name: Yielded<String> = { randomStringMultiCaseWithNumbers(6) }
   private var isActive: Yielded<Boolean> = { true }
   private var serviceScope: Yielded<String> = { randomStringUpperCase(4) }
+  private var sortOrder: Yielded<Int> = { randomInt(0, 1000) }
 
   fun withId(id: UUID) = apply {
     this.id = { id }
@@ -29,10 +31,15 @@ class ReferralRejectionReasonEntityFactory : Factory<ReferralRejectionReasonEnti
     this.serviceScope = { serviceScope }
   }
 
+  fun withSortOrder(sortOrder: Int) = apply {
+    this.sortOrder = { sortOrder }
+  }
+
   override fun produce(): ReferralRejectionReasonEntity = ReferralRejectionReasonEntity(
     id = this.id(),
     name = this.name(),
     isActive = this.isActive(),
     serviceScope = this.serviceScope(),
+    sortOrder = this.sortOrder(),
   )
 }
