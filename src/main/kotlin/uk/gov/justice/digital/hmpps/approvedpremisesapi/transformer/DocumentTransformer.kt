db3d45aecc789ae25911fef4fbd73dc31d329d43
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-05-08 08:45:12 +0100

    Filter out documents without names
    We have observed rare cases where the community API has returned a document without a name specified.
    
    This commit filters out such documents as we cannot automatically generate a filename without knowing the file extension/type, which is not provided.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/DocumentTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/DocumentTransformer.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Document
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.DocumentLevel
@@ -9,12 +10,15 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.Document
 
 @Component
 class DocumentTransformer {
+
+  private val log = LoggerFactory.getLogger(this::class.java)
+
   fun transformToApi(groupedDocuments: GroupedDocuments, convictionId: Long): List<Document> {
-    val offenderDocuments = documentsWithIds(groupedDocuments.documents).map {
+    val offenderDocuments = documentsWithIdsAndNames(groupedDocuments.documents).map {
       Document(
         id = it.id!!,
         level = DocumentLevel.offender,
-        fileName = it.documentName,
+        fileName = it.documentName!!,
         createdAt = it.createdAt.toInstant(ZoneOffset.UTC),
         typeCode = it.type.code,
         typeDescription = it.type.description,
@@ -26,11 +30,11 @@ class DocumentTransformer {
       .firstOrNull { it.convictionId == convictionId.toString() }
 
     val convictionDocuments = if (documentsForConvictionId != null) {
-      documentsWithIds(documentsForConvictionId.documents).map {
+      documentsWithIdsAndNames(documentsForConvictionId.documents).map {
         Document(
           id = it.id!!,
           level = DocumentLevel.conviction,
-          fileName = it.documentName,
+          fileName = it.documentName!!,
           createdAt = it.createdAt.toInstant(ZoneOffset.UTC),
           typeCode = it.type.code,
           typeDescription = it.type.description,
@@ -44,7 +48,19 @@ class DocumentTransformer {
     return offenderDocuments + convictionDocuments
   }
 
-  // Filter out any documents without IDs - at the moment, we don't have a way of fetching documents without IDs
-  // This fixes the nullability problem we have previously had with documents with no ID
-  fun documentsWithIds(documents: List<CommunityApiDocument>) = documents.filter { it.id != null }
+  /**
+   * If a document doesn't have an ID we have no way to retrieve it for download, so it's filtered out
+   *
+   * If a document doesn't have a name, we have no way to determine what the file should be called on download,
+   * or what file extension (e.g. pdf) should be used. This rarely occurs.
+   */
+  private fun documentsWithIdsAndNames(documents: List<CommunityApiDocument>): List<CommunityApiDocument> {
+    documents
+      .filter { it.documentName == null }
+      .forEach {
+        log.warn("Filtering out document with id ${it.id} because it doesn't have a name")
+      }
+
+    return documents.filter { it.id != null && it.documentName != null }
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/DocumentFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/factory/DocumentFactory.kt
@@ -11,7 +11,7 @@ import java.util.UUID
 
 class DocumentFactory : Factory<Document> {
   private var id: Yielded<String?> = { UUID.randomUUID().toString() }
-  private var documentName: Yielded<String> = { "${randomStringMultiCaseWithNumbers(5)}.pdf" }
+  private var documentName: Yielded<String?> = { "${randomStringMultiCaseWithNumbers(5)}.pdf" }
   private var author: Yielded<String?> = { randomStringMultiCaseWithNumbers(4) }
   private var typeCode: Yielded<String> = { randomStringMultiCaseWithNumbers(4) }
   private var typeDescription: Yielded<String> = { randomStringMultiCaseWithNumbers(4) }
@@ -24,7 +24,7 @@ class DocumentFactory : Factory<Document> {
     this.id = { id }
   }
 
-  fun withDocumentName(documentName: String) = apply {
+  fun withDocumentName(documentName: String?) = apply {
     this.documentName = { documentName }
   }
 
