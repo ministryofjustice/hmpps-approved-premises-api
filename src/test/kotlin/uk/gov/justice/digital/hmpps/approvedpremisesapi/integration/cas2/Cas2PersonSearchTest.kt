1810fe8bfbe3d4aa76ee370814a39503fb183ee1
Harriet H-W <harriethw@gmail.com>
2024-05-13 14:34:02 +0100

    remove duplicate search test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2PersonSearchTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2PersonSearchTest.kt
@@ -1,27 +1,13 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
 
-import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.FullPerson
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PersonStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PersonType
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.InmateDetailFactory
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ProbationOffenderDetailFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.`Given a CAS2 POM User`
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.PrisonAPI_mockSuccessfulInmateDetailsCall
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.ProbationOffenderSearchAPI_mockForbiddenOffenderSearchCall
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.ProbationOffenderSearchAPI_mockNotFoundSearchCall
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.ProbationOffenderSearchAPI_mockServerErrorSearchCall
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.ProbationOffenderSearchAPI_mockSuccessfulOffenderSearchCall
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.AssignedLivingUnit
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.probationoffendersearchapi.IDs
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.probationoffendersearchapi.OffenderProfile
-import java.time.LocalDate
 
 class Cas2PersonSearchTest : IntegrationTestBase() {
   @Nested
@@ -66,85 +52,6 @@ class Cas2PersonSearchTest : IntegrationTestBase() {
     }
 
     @Test
-    fun `Searching for a NOMIS ID that does not exist returns 404`() {
-      mockClientCredentialsJwtRequest()
-
-      `Given a CAS2 POM User` { userEntity, jwt ->
-        wiremockServer.stubFor(
-          get(WireMock.urlEqualTo("/search?nomsNumber=nomsNumber"))
-            .willReturn(
-              aResponse()
-                .withHeader("Content-Type", "application/json")
-                .withStatus(404),
-            ),
-        )
-
-        webTestClient.get()
-          .uri("/cas2/people/search?nomsNumber=nomsNumber")
-          .header("Authorization", "Bearer $jwt")
-          .exchange().expectStatus()
-          .isNotFound
-      }
-    }
-
-    @Test
-    fun `Searching for a NOMIS ID returns OK with correct body`() {
-      `Given a CAS2 POM User` { userEntity, jwt ->
-        val offender = ProbationOffenderDetailFactory()
-          .withOtherIds(IDs(crn = "CRN", nomsNumber = "NOMS321", pncNumber = "PNC123"))
-          .withFirstName("James")
-          .withSurname("Someone")
-          .withDateOfBirth(
-            LocalDate
-              .parse("1985-05-05"),
-          )
-          .withGender("Male")
-          .withOffenderProfile(OffenderProfile(nationality = "English"))
-          .produce()
-
-        val inmateDetail = InmateDetailFactory().withOffenderNo("NOMS321")
-          .withCustodyStatus(InmateStatus.IN)
-          .withAssignedLivingUnit(
-            AssignedLivingUnit(
-              agencyId = "BRI",
-              locationId = 5,
-              description = "B-2F-004",
-              agencyName = "HMP Bristol",
-            ),
-          )
-          .produce()
-
-        ProbationOffenderSearchAPI_mockSuccessfulOffenderSearchCall("NOMS321", listOf(offender))
-        PrisonAPI_mockSuccessfulInmateDetailsCall(inmateDetail = inmateDetail)
-
-        webTestClient.get()
-          .uri("/cas2/people/search?nomsNumber=NOMS321")
-          .header("Authorization", "Bearer $jwt")
-          .exchange()
-          .expectStatus()
-          .isOk
-          .expectBody()
-          .json(
-            objectMapper.writeValueAsString(
-              FullPerson(
-                type = PersonType.fullPerson,
-                crn = "CRN",
-                name = "James Someone",
-                dateOfBirth = LocalDate.parse("1985-05-05"),
-                sex = "Male",
-                status = PersonStatus.inCustody,
-                nomsNumber = "NOMS321",
-                pncNumber = "PNC123",
-                nationality = "English",
-                isRestricted = false,
-                prisonName = "HMP Bristol",
-              ),
-            ),
-          )
-      }
-    }
-
-    @Test
     fun `Searching for a NOMIS ID returns Unauthorised error when it is unauthorized`() {
       `Given a CAS2 POM User` { userEntity, jwt ->
         ProbationOffenderSearchAPI_mockForbiddenOffenderSearchCall()
@@ -159,7 +66,7 @@ class Cas2PersonSearchTest : IntegrationTestBase() {
     }
 
     @Test
-    fun `Searching for a NOMIS ID returns Unauthorised error when it is not found`() {
+    fun `Searching for a NOMIS ID returns 404 error when it is not found`() {
       `Given a CAS2 POM User` { userEntity, jwt ->
         ProbationOffenderSearchAPI_mockNotFoundSearchCall()
 
