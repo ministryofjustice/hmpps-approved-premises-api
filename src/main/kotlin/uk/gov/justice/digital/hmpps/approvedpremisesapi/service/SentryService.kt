e8f24d683574eaaa4b0024d405b20eb6097c91ee
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-05-23 12:10:09 +0100

    Add ability to log API Request/Response
    This is only enabled for local deployments.
    
    If enabled a sentry error will be logged, meaning we’d spot this quickly if the configuration was enabled in any other environment.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/SentryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/SentryService.kt
@@ -2,15 +2,22 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 
 import io.sentry.Sentry
 import io.sentry.SentryLevel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class SentryService {
+
+  var log: Logger = LoggerFactory.getLogger(this::class.java)
+
   fun captureException(throwable: Throwable) {
+    log.debug("Will capture exception in sentry", throwable)
     Sentry.captureException(throwable)
   }
 
   fun captureErrorMessage(message: String) {
+    log.debug("Will capture error message in sentry: '$message'")
     Sentry.captureMessage(message, SentryLevel.ERROR)
   }
 }
