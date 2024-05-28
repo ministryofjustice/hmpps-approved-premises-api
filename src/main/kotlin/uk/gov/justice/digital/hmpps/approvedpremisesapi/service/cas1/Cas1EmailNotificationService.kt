e6665e99e4193485b2c3b8bf7bfa68545e91123d
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-05-28 14:19:22 +0100

    Remove cas1-email-use-cru-for-reply-to featureflag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/Cas1EmailNotificationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/Cas1EmailNotificationService.kt
@@ -3,12 +3,10 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas1
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.EmailNotificationService
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.FeatureFlagService
 
 @Service
 class Cas1EmailNotificationService(
   private val emailNotificationService: EmailNotificationService,
-  private val featureFlagService: FeatureFlagService,
 ) : Cas1EmailNotifier {
   override fun sendEmail(
     recipientEmailAddress: String,
@@ -38,13 +36,7 @@ class Cas1EmailNotificationService(
     )
   }
 
-  private fun getReplyToEmailId(application: ApprovedPremisesApplicationEntity): String? {
-    return if (featureFlagService.getBooleanFlag("cas1-email-use-cru-for-reply-to", default = false)) {
-      application.apArea?.notifyReplyToEmailId
-    } else {
-      null
-    }
-  }
+  private fun getReplyToEmailId(application: ApprovedPremisesApplicationEntity) = application.apArea?.notifyReplyToEmailId
 }
 
 interface Cas1EmailNotifier {
