95c945f5a5ab3989a12cfe606bca23ff5f8bde24
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-05-31 09:04:58 +0100

    Support content-disposition when streaming files
    Add a new generateStreamingResponse function that allows the caller to define the filename which is then written to the content-disposition header. The function also allows the content type to be defined, in preparation for adding CSV support.
    
    This function has been added as we’re going to move setting report filenames out of the CAS1 UI and into the API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ControllerUtils.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ControllerUtils.kt
@@ -7,10 +7,28 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 import java.io.OutputStream
 
 fun generateXlsxStreamingResponse(outputProvider: (OutputStream) -> Unit) =
+  generateStreamingResponse(
+    contentType = ContentType.XLSX,
+    fileName = null,
+    outputProvider = outputProvider,
+  )
+
+fun generateStreamingResponse(
+  contentType: ContentType,
+  fileName: String? = null,
+  outputProvider: (OutputStream) -> Unit,
+) =
   ResponseEntity(
     StreamingResponseBody { outputStream -> outputProvider.invoke(outputStream) },
     HttpHeaders().apply {
-      put(HttpHeaders.CONTENT_TYPE, listOf("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+      put(HttpHeaders.CONTENT_TYPE, listOf(contentType.mimeType))
+      fileName?.let {
+        put(HttpHeaders.CONTENT_DISPOSITION, listOf("attachment; filename=\"$it\""))
+      }
     },
     HttpStatus.OK,
   )
+
+enum class ContentType(val mimeType: String, val extension: String) {
+  XLSX("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "xlsx"),
+}
