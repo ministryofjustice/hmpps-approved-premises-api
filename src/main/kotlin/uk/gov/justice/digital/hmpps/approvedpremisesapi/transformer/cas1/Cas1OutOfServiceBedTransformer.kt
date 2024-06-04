93dcab95cc4256b11961803c1506a7d59ca52be9
Dan Livings <dan.livings@dxw.com>
2024-06-04 13:34:53 +0100

    Update `Cas1OutOfServiceBed` schema in API spec
    The data model has been updated to better reflect what is presented in
    the ticket (https://dsdmoj.atlassian.net/browse/APS-845):
    
    - `startDate` and `endDate` have been renamed to `outOfServiceFrom` and
      `outOfServiceTo` respectively
    - `daysLostCount` and `temporality` have been added, and are derived
      from the start and end dates of the out-of-service bed
    - `bedId`, `bedName`, and `roomName` have been replaced with `bed`,
      `room`, `premises`, and `apArea`, which are all `NamedId`s

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/cas1/Cas1OutOfServiceBedTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/cas1/Cas1OutOfServiceBedTransformer.kt
@@ -3,7 +3,11 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas1
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1OutOfServiceBed
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1OutOfServiceBedStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NamedId
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Temporality
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas1OutOfServiceBedEntity
+import java.time.Duration
+import java.time.LocalDate
 
 @Component
 class Cas1OutOfServiceBedTransformer(
@@ -13,12 +17,15 @@ class Cas1OutOfServiceBedTransformer(
   fun transformJpaToApi(jpa: Cas1OutOfServiceBedEntity) = Cas1OutOfServiceBed(
     id = jpa.id,
     createdAt = jpa.createdAt.toInstant(),
-    startDate = jpa.startDate,
-    endDate = jpa.endDate,
-    bedId = jpa.bed.id,
-    bedName = jpa.bed.name,
-    roomName = jpa.bed.room.name,
+    outOfServiceFrom = jpa.startDate,
+    outOfServiceTo = jpa.endDate,
+    bed = NamedId(jpa.bed.id, jpa.bed.name),
+    room = NamedId(jpa.bed.room.id, jpa.bed.room.name),
+    premises = NamedId(jpa.premises.id, jpa.premises.name),
+    apArea = NamedId(jpa.premises.probationRegion.apArea.id, jpa.premises.probationRegion.apArea.name),
     reason = cas1OutOfServiceBedReasonTransformer.transformJpaToApi(jpa.reason),
+    daysLostCount = jpa.deriveDaysLost(),
+    temporality = jpa.deriveTemporality(),
     status = jpa.deriveStatus(),
     referenceNumber = jpa.referenceNumber,
     notes = jpa.notes,
@@ -28,5 +35,20 @@ class Cas1OutOfServiceBedTransformer(
   private fun Cas1OutOfServiceBedEntity.deriveStatus() = when (this.cancellation) {
     null -> Cas1OutOfServiceBedStatus.active
     else -> Cas1OutOfServiceBedStatus.cancelled
+  }
+
+  private fun Cas1OutOfServiceBedEntity.deriveDaysLost() = Duration
+    .between(this.startDate.atStartOfDay(), this.endDate.plusDays(1).atStartOfDay())
+    .toDays()
+    .toInt()
+
+  private fun Cas1OutOfServiceBedEntity.deriveTemporality(): Temporality {
+    val now = LocalDate.now()
+
+    return when {
+      now > this.endDate -> Temporality.past
+      now < this.startDate -> Temporality.future
+      else -> Temporality.current
+    }
   }
 }
