bfadf45428eff646f613df87d9ad7efce390f67c
Muhammad El Abdulla <>
2024-07-02 09:29:22 +0100

    Fix issue with existing person departure updated domain event

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/migration/Cas3UpdateDomainEventTypeForPersonDepartureUpdatedJob.kt
@@ -0,0 +1,39 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.migration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.CAS3PersonDepartedEvent
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model.EventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+
+class Cas3UpdateDomainEventTypeForPersonDepartureUpdatedJob(
+  private val domainEventRepository: DomainEventRepository,
+  private val objectMapper: ObjectMapper,
+  private val migrationLogger: MigrationLogger,
+) : MigrationJob() {
+  override val shouldRunInTransaction = true
+
+  @SuppressWarnings("MagicNumber", "TooGenericExceptionCaught", "NestedBlockDepth")
+  override fun process() {
+    val domainEvents = domainEventRepository.findByType(DomainEventType.CAS3_PERSON_DEPARTURE_UPDATED)
+    domainEvents.forEach {
+      migrationLogger.info("Updating person departure updated domain event. Event id ${it.id}")
+      try {
+        val domainEvent = domainEventRepository.findById(it.id)
+        val domainEventData = domainEvent.get().data
+        if (!domainEventData.contains("accommodation.cas3.person.departed.updated")) {
+          val departureEvent = objectMapper.readValue<CAS3PersonDepartedEvent>(domainEventData)
+          if (departureEvent.eventType == EventType.personDeparted) {
+            val updatedDepartureEvent = departureEvent.copy(
+              eventType = EventType.personDepartureUpdated,
+            )
+            domainEventRepository.updateData(it.id, objectMapper.writeValueAsString(updatedDepartureEvent))
+          }
+        }
+      } catch (exception: Exception) {
+        migrationLogger.error("Unable to update person departure updated domain event ${it.id}", exception)
+      }
+    }
+  }
+}
