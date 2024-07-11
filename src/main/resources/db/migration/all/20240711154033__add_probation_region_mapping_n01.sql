f717dfaedcc2353fa2abc215b6427c2d48b40ea7
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-07-11 15:55:03 +0100

    Add region mapping for N01
    This adds a missing mapping for probation area delius code N01 to region Wales

---
+++ src/main/resources/db/migration/all/20240711154033__add_probation_region_mapping_n01.sql
@@ -0,0 +1,3 @@
+INSERT INTO probation_area_probation_region_mappings (id, probation_area_delius_code, probation_region_id)
+VALUES
+('cadfb193-56fd-40cc-8e8c-38bf3b0d4625', 'N01', 'afee0696-8df3-4d9f-9d0c-268f17772e2c')
