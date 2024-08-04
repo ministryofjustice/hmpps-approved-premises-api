561280424e18b415dbdba2a0eeaa5852a58840f0
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-08-04 18:59:26 +0100

    Add logging to ProfileController
    This helps verify behaviour

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ProfileController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/ProfileController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -15,6 +16,8 @@ class ProfileController(
   private val userService: UserService,
   private val userTransformer: UserTransformer,
 ) : ProfileApiDelegate {
+  private val log = LoggerFactory.getLogger(this::class.java)
+
   override fun profileGet(xServiceName: ServiceName): ResponseEntity<User> {
     val userEntity = userService.getUserForRequest()
 
@@ -25,7 +28,10 @@ class ProfileController(
     val username = userService.getDeliusUserNameForRequest()
     var getUserResponse = userService.getUserForProfile(username)
     if (getUserResponse is UserService.GetUserResponse.Success && !getUserResponse.createdOnGet) {
+      log.info("On call to /profile/v2 user record for $username already exists, so will update")
       getUserResponse = userService.updateUserFromCommunityApi(getUserResponse.user, xServiceName)
+    } else {
+      log.info("On call to /profile/v2 user record for $username was created")
     }
     return ResponseEntity(userTransformer.transformProfileResponseToApi(username, getUserResponse, xServiceName), HttpStatus.OK)
   }
