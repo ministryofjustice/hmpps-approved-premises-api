29cd7315f7738065d2cc2b2a826f81fb79504d34
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-09-10 12:18:11 +0100

    Add exception handler for NoResourceFoundException
    We currently receive Sentry Alerts for NoResourceFoundException errors because our custom exception handler doesn’t know how to deal with this exception (note, this exception has only started appearing since upgrading to spring boot 3).
    
    This PR adds exception handling for NoResourceFoundException, ensuring we no longer receive sentry alerts for these issues. Note that a WARN is still logged.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/problem/NotFoundResourceProblem.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.problem
+
+import org.zalando.problem.AbstractThrowableProblem
+import org.zalando.problem.Exceptional
+import org.zalando.problem.Status
+
+class NotFoundResourceProblem : AbstractThrowableProblem(null, "Not Found", Status.NOT_FOUND, "Resource not found") {
+  override fun getCause(): Exceptional? {
+    return null
+  }
+}
