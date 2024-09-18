6277ae29bf47a1e6cddc2d47a12e30a07f6ff098
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-09-18 17:44:52 +0100

    Nest tests in Cas1ReferenceDataTest

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas1/Cas1ReferenceDataTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas1/Cas1ReferenceDataTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas1
 
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
@@ -9,38 +10,42 @@ class Cas1ReferenceDataTest : IntegrationTestBase() {
   @Autowired
   lateinit var reasonTransformer: Cas1OutOfServiceBedReasonTransformer
 
-  @Test
-  fun `All available out-of-service bed reasons are returned`() {
-    cas1OutOfServiceBedReasonTestRepository.deleteAll()
+  @Nested
+  inner class GetOutOfServiceBedReasons {
 
-    val activeReason1 = cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
-      withIsActive(true)
-      withName("Active reason 1")
+    @Test
+    fun success() {
+      cas1OutOfServiceBedReasonTestRepository.deleteAll()
+
+      val activeReason1 = cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
+        withIsActive(true)
+        withName("Active reason 1")
+      }
+
+      val activeReason2 = cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
+        withIsActive(true)
+        withName("Active reason 2")
+      }
+
+      cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
+        withIsActive(false)
+        withName("Inactive reason")
+      }
+
+      val expectedReasons = objectMapper.writeValueAsString(
+        listOf(activeReason1, activeReason2).map { reason -> reasonTransformer.transformJpaToApi(reason) },
+      )
+
+      val jwt = jwtAuthHelper.createValidAuthorizationCodeJwt()
+
+      webTestClient.get()
+        .uri("/cas1/reference-data/out-of-service-bed-reasons")
+        .header("Authorization", "Bearer $jwt")
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectBody()
+        .json(expectedReasons)
     }
-
-    val activeReason2 = cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
-      withIsActive(true)
-      withName("Active reason 2")
-    }
-
-    cas1OutOfServiceBedReasonEntityFactory.produceAndPersist {
-      withIsActive(false)
-      withName("Inactive reason")
-    }
-
-    val expectedReasons = objectMapper.writeValueAsString(
-      listOf(activeReason1, activeReason2).map { reason -> reasonTransformer.transformJpaToApi(reason) },
-    )
-
-    val jwt = jwtAuthHelper.createValidAuthorizationCodeJwt()
-
-    webTestClient.get()
-      .uri("/cas1/reference-data/out-of-service-bed-reasons")
-      .header("Authorization", "Bearer $jwt")
-      .exchange()
-      .expectStatus()
-      .isOk
-      .expectBody()
-      .json(expectedReasons)
   }
 }
