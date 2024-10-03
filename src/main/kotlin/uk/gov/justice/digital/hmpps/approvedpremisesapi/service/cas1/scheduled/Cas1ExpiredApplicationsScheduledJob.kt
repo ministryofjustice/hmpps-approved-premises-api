329ba396a80733252dd29a1d8f9c8c6417ded624
Danie Humphreys <daniel.humphreys@digital.justice.gov.uk>
2024-10-03 11:25:43 +0100

    Add feature flag.
    Prod: enabled = false
    Preprod: enabled = true

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/scheduled/Cas1ExpiredApplicationsScheduledJob.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/scheduled/Cas1ExpiredApplicationsScheduledJob.kt
@@ -4,15 +4,20 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.FeatureFlagService
 
 @Service
-class Cas1ExpiredApplicationsScheduledJob {
+class Cas1ExpiredApplicationsScheduledJob(
+  private val featureFlagService: FeatureFlagService,
+) {
 
   private val log = LoggerFactory.getLogger(this::class.java)
 
   @Scheduled(cron = "0 0/30 * * * ?")
   @SchedulerLock(name = "cas1_expire_applications", lockAtMostFor = "5m", lockAtLeastFor = "1m")
   fun expireApplications() {
-    log.info("Checking for expired applications (Dry Run)")
+    if (featureFlagService.getBooleanFlag("cas1-expired-applications-enabled")) {
+      log.info("Checking for expired applications (Dry Run)")
+    }
   }
 }
