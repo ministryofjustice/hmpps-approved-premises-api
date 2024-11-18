2008798108cfa9a0b014d1617f53a81035cc390c
Danie Humphreys <daniel.humphreys@digital.justice.gov.uk>
2024-11-18 12:01:55 +0000

    Upgrade detekt Kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ configurations {
 configurations.matching { it.name == "detekt" }.all {
   resolutionStrategy.eachDependency {
     if (requested.group == "org.jetbrains.kotlin") {
-      useVersion("1.9.21")
+      useVersion(io.gitlab.arturbosch.detekt.getSupportedKotlinVersion())
     }
   }
 }
