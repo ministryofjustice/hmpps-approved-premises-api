b5a78278bc828fee7b42a2320058baa5ccf2131d
venkatesh bala <venkatesh.bala1@digital.justice.gov.uk>
2024-11-25 08:44:14 +0000

    Added soft delete end point for cas3 applications.

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas3/Cas3ApplicationTest.kt
@@ -0,0 +1,64 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas3
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.InitialiseDatabasePerClassTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAUser
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAnOffender
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRole
+import java.time.LocalDate
+import java.util.UUID
+
+class Cas3ApplicationTest : InitialiseDatabasePerClassTestBase() {
+
+  @Nested
+  inner class SoftDelete {
+    @Test
+    fun `soft delete application without JWT returns 401`() {
+      webTestClient.delete()
+        .uri("/cas3/applications/${UUID.randomUUID()}")
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @Test
+    fun `soft delete inProgress application successfully returns 200`() {
+      givenAUser(roles = listOf(UserRole.CAS3_REFERRER)) { userEntity, jwt ->
+        givenAnOffender { offenderDetails, _ ->
+
+          val application =
+            persistApplication(crn = offenderDetails.otherIds.crn, user = userEntity)
+
+          webTestClient.delete()
+            .uri("/cas3/applications/${application.id}")
+            .header("Authorization", "Bearer $jwt")
+            .exchange()
+            .expectStatus()
+            .isOk
+
+          Assertions.assertThat(temporaryAccommodationApplicationRepository.findById(application.id).get().deletedAt)
+            .isNotNull()
+        }
+      }
+    }
+  }
+
+  private fun persistApplication(crn: String, user: UserEntity) =
+    temporaryAccommodationApplicationEntityFactory.produceAndPersist {
+      withCrn(crn)
+      withCreatedByUser(user)
+      withApplicationSchema(persistApplicationSchema())
+      withProbationRegion(user.probationRegion)
+      withArrivalDate(LocalDate.now().plusDays(30))
+      withSubmittedAt(null)
+    }
+
+  private fun persistApplicationSchema() = temporaryAccommodationAssessmentJsonSchemaEntityFactory.produceAndPersist {
+    temporaryAccommodationApplicationJsonSchemaEntityFactory.produceAndPersist {
+      withPermissiveSchema()
+    }
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas3/Cas3ApplicationServiceTest.kt
@@ -0,0 +1,97 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.service.cas3
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.TemporaryAccommodationApplicationEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.UserEntityFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.CasResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserAccessService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3.Cas3ApplicationService
+import java.time.OffsetDateTime
+import java.util.UUID
+
+class Cas3ApplicationServiceTest {
+  private val mockApplicationRepository = mockk<ApplicationRepository>()
+
+  private val mockUserAccessService = mockk<UserAccessService>()
+
+  private val mockUserService = mockk<UserService>()
+
+  private val cas3ApplicationService = Cas3ApplicationService(mockApplicationRepository, mockUserService, mockUserAccessService)
+
+  val user = UserEntityFactory()
+    .withUnitTestControlProbationRegion()
+    .produce()
+
+  val submittedApplication = TemporaryAccommodationApplicationEntityFactory()
+    .withCreatedByUser(user)
+    .withProbationRegion(user.probationRegion)
+    .withSubmittedAt(OffsetDateTime.now())
+    .produce()
+
+  val inProgressApplication = TemporaryAccommodationApplicationEntityFactory()
+    .withCreatedByUser(user)
+    .withProbationRegion(user.probationRegion)
+    .withSubmittedAt(null)
+    .produce()
+
+  @Nested
+  inner class SoftDeleteCas3Application {
+
+    @Test
+    fun `softDeleteCas3Application returns NotFound if Application does not exist`() {
+      val applicationId = UUID.fromString("6504a40f-e52e-4f6b-b340-f87b480bf41d")
+
+      every { mockApplicationRepository.findByIdOrNull(applicationId) } returns null
+
+      every { mockUserService.getUserForRequest() } returns user
+      every { mockUserAccessService.userCanAccessTemporaryAccommodationApplication(any(), any()) } returns true
+
+      val result = cas3ApplicationService.markApplicationAsDeleted(applicationId)
+
+      assertThat(result is CasResult.NotFound).isTrue
+    }
+
+    @Test
+    fun `softDeleteCas3Application returns GeneralValidationError if Application is already submitted`() {
+      every { mockApplicationRepository.findByIdOrNull(submittedApplication.id) } returns submittedApplication
+      every { mockUserService.getUserForRequest() } returns user
+      every { mockUserAccessService.userCanAccessTemporaryAccommodationApplication(user, submittedApplication) } returns true
+
+      val result = cas3ApplicationService.markApplicationAsDeleted(submittedApplication.id)
+
+      assertThat(result is CasResult.GeneralValidationError).isTrue
+      val generalValidationError = (result as CasResult.GeneralValidationError).message
+
+      assertThat(generalValidationError).isEqualTo("Cannot mark as deleted: temporary accommodation application already submitted.")
+    }
+
+    @Test
+    fun `softDelete inProgress cas3 application returns success`() {
+      every { mockApplicationRepository.findByIdOrNull(inProgressApplication.id) } returns inProgressApplication
+      every { mockUserService.getUserForRequest() } returns user
+      every { mockUserAccessService.userCanAccessTemporaryAccommodationApplication(user, inProgressApplication) } returns true
+      every { mockApplicationRepository.saveAndFlush(any()) } answers { it.invocation.args[0] as ApplicationEntity }
+
+      val result = cas3ApplicationService.markApplicationAsDeleted(inProgressApplication.id)
+
+      assertThat(result is CasResult.Success).isTrue
+
+      verify {
+        mockApplicationRepository.saveAndFlush(
+          match {
+            it.id == inProgressApplication.id
+          },
+        )
+      }
+    }
+  }
+}
