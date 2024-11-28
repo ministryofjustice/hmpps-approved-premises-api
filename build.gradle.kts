c8de685302c198eb2fed62589ecf7472e9508a7d
Toby Batch <toby.batch@digital.justice.co.uk>
2024-11-28 10:36:16 +0000

    feat: added open api scaffold for cas2 to cas2bail routes and delegates

--- build.gradle.kts
+++ build.gradle.kts
@@ -159,7 +159,14 @@ tasks.bootRun {
 }
 
 tasks.withType<Test> {
-  jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED", "--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED", "--add-opens", "java.base/java.time=ALL-UNNAMED")
+  jvmArgs(
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang.reflect=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.time=ALL-UNNAMED"
+  )
 
   afterEvaluate {
     if (environment["CI"] != null) {
@@ -249,6 +256,14 @@ registerAdditionalOpenApiGenerateTask(
 )
 
 registerAdditionalOpenApiGenerateTask(
+  name = "openApiGenerateCas2bailNamespace",
+  ymlPath = "$rootDir/src/main/resources/static/codegen/built-cas2bail-api-spec.yml",
+  apiPackageName = "uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2bail",
+  modelPackageName = "uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model",
+  apiSuffix = "Cas2bail",
+)
+
+registerAdditionalOpenApiGenerateTask(
   name = "openApiGenerateCas2Namespace",
   ymlPath = "$rootDir/src/main/resources/static/codegen/built-cas2-api-spec.yml",
   apiPackageName = "uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2",
@@ -327,6 +342,7 @@ tasks.register("openApiPreCompilation") {
       .readFileToString(file, "UTF-8")
       .replace("_shared.yml#/components", "#/components")
       .replace("cas1-schemas.yml#/components", "#/components")
+      .replace("cas2bail-schemas.yml#/components", "#/components")
     FileUtils.writeStringToFile(file, updatedContents, "UTF-8")
   }
 
@@ -380,6 +396,11 @@ tasks.register("openApiPreCompilation") {
     inputSpec = "cas2-api.yml",
   )
   buildSpecWithSharedComponentsAppended(
+    outputFileName = "built-cas2bail-api-spec.yml",
+    inputSpec = "cas2bail-api.yml",
+    inputSchemas = "cas2bail-schemas.yml",
+  )
+  buildSpecWithSharedComponentsAppended(
     outputFileName = "built-cas3-api-spec.yml",
     inputSpec = "cas3-api.yml",
   )
@@ -392,6 +413,7 @@ tasks.get("openApiGenerate").dependsOn(
   "openApiPreCompilation",
   "openApiGenerateCas1Namespace",
   "openApiGenerateCas2Namespace",
+  "openApiGenerateCas2bailNamespace",
   "openApiGenerateCas3Namespace",
 )
 
