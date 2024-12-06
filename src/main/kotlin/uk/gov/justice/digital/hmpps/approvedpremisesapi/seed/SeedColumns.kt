18bcb158c60f9035129f0d71bb3067a8553347b9
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-12-06 12:32:36 +0000

    Add `get` suffix to `SeedColumn` functions
    This is to clarify they provide a value

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/SeedColumns.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/SeedColumns.kt
@@ -8,35 +8,35 @@ import java.util.UUID
 data class SeedColumns(
   val columns: Map<String, String>,
 ) {
-  fun stringOrNull(label: String) = columns[label]?.trim()?.ifBlank { null }
+  fun getStringOrNull(label: String) = columns[label]?.trim()?.ifBlank { null }
 
-  fun uuidOrNull(label: String) = columns[label]?.let { UUID.fromString(it.trim()) }
+  fun getUuidOrNull(label: String) = columns[label]?.let { UUID.fromString(it.trim()) }
 
-  fun intOrNull(label: String) = columns[label]?.toIntOrNull()
+  fun getIntOrNull(label: String) = columns[label]?.toIntOrNull()
 
-  fun longOrNull(label: String) = columns[label]?.toLongOrNull()
+  fun getLongOrNull(label: String) = columns[label]?.toLongOrNull()
 
-  fun lastDateTimeFromList(label: String, formatter: DateTimeFormatter): LocalDateTime? {
-    val rawValue = stringOrNull(label) ?: return null
+  fun getLastDateTimeFromListOrNull(label: String, formatter: DateTimeFormatter): LocalDateTime? {
+    val rawValue = getStringOrNull(label) ?: return null
 
     val lastDateTime = rawValue.split(",").last()
     return LocalDateTime.parse(lastDateTime, formatter)
   }
 
-  fun dateFromUtcDateTime(label: String): LocalDate? {
-    val rawValue = stringOrNull(label) ?: return null
+  fun getDateFromUtcDateTimeOrNull(label: String): LocalDate? {
+    val rawValue = getStringOrNull(label) ?: return null
 
     return LocalDate.parse(rawValue.substring(startIndex = 0, endIndex = 10))
   }
 
-  fun stringsFromList(label: String): List<String> {
-    val rawValue = stringOrNull(label) ?: return emptyList()
+  fun getStringsFromListOrNull(label: String): List<String> {
+    val rawValue = getStringOrNull(label) ?: return emptyList()
 
     return rawValue.split(",")
   }
 
-  fun yesNoBoolean(label: String): Boolean? {
-    val rawValue = stringOrNull(label)?.uppercase() ?: return null
+  fun getYesNoBooleanOrNull(label: String): Boolean? {
+    val rawValue = getStringOrNull(label)?.uppercase() ?: return null
 
     return when (rawValue) {
       "YES" -> {
