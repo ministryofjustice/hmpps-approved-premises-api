9fe5dbb725fce066fb52a2cc35008a06942fa9d6
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2024-12-23 10:54:57 +0000

    Simplify Cas1SpaceSearchResultsTransformerTest
    Assert on literals instead of doing it in an opaque dynamic manner

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas1/Cas1SpaceSearchResultsTransformerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas1/Cas1SpaceSearchResultsTransformerTest.kt
@@ -9,11 +9,10 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1SpaceSearc
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Gender
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.cas1.CandidatePremises
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.ApprovedPremisesType
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.asApiType
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas1.Cas1SpaceSearchResultsTransformer
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1SpaceSearchResult as ApiSpaceSearchResult
 
 class Cas1SpaceSearchResultsTransformerTest {
   private val transformer = Cas1SpaceSearchResultsTransformer()
@@ -32,77 +31,78 @@ class Cas1SpaceSearchResultsTransformerTest {
       ),
     )
 
-    val candidatePremises1 = CandidatePremises(
-      UUID.randomUUID(),
-      1.0f,
-      ApprovedPremisesType.NORMAL,
-      "Some AP",
-      "1 The Street",
-      null,
-      "Townsbury",
-      "TB1 2AB",
-      UUID.randomUUID(),
-      "Some AP Area",
-      characteristics = emptyList(),
-    )
+    val candidatePremise1Id = UUID.randomUUID()
+    val apAreaId1 = UUID.randomUUID()
 
-    val candidatePremises2 = CandidatePremises(
-      UUID.randomUUID(),
-      2.0f,
-      ApprovedPremisesType.NORMAL,
-      "Some Other AP",
-      "2 The Street",
-      null,
-      "Townsbury",
-      "TB1 2AB",
-      UUID.randomUUID(),
-      "Some AP Area",
-      characteristics = emptyList(),
-    )
-
-    val candidatePremises3 = CandidatePremises(
-      UUID.randomUUID(),
-      3.0f,
-      ApprovedPremisesType.NORMAL,
-      "Some AP",
-      "3 The Street",
-      null,
-      "Townsbury",
-      "TB1 2AB",
-      UUID.randomUUID(),
-      "Some AP Area",
-      characteristics = emptyList(),
-    )
+    val candidatePremise2Id = UUID.randomUUID()
+    val apAreaId2 = UUID.randomUUID()
 
     val searchResults = listOf(
-      candidatePremises1,
-      candidatePremises2,
-      candidatePremises3,
+      CandidatePremises(
+        premisesId = candidatePremise1Id,
+        distanceInMiles = 1.0f,
+        apType = ApprovedPremisesType.NORMAL,
+        name = "Some AP 1",
+        addressLine1 = "1 The Street",
+        addressLine2 = null,
+        town = "Townsbury",
+        postcode = "TB1 2AB",
+        apAreaId = apAreaId1,
+        apAreaName = "Some AP Area 1",
+        characteristics = emptyList(),
+      ),
+      CandidatePremises(
+        premisesId = candidatePremise2Id,
+        distanceInMiles = 2.0f,
+        apType = ApprovedPremisesType.ESAP,
+        name = "Some Other AP",
+        addressLine1 = "2 The Street",
+        addressLine2 = "Additional Bit",
+        town = "Townton",
+        postcode = "TB1 2AC",
+        apAreaId = apAreaId2,
+        apAreaName = "Some AP Area 2",
+        characteristics = emptyList(),
+      ),
     )
 
     val actual = transformer.transformDomainToApi(searchParameters, searchResults)
     assertThat(actual.searchCriteria).isNotNull
     assertThat(actual.searchCriteria).isEqualTo(searchParameters)
-    assertThat(actual.resultsCount).isEqualTo(3)
-    assertThatTransformedResultMatches(actual.results[0], candidatePremises1)
-    assertThatTransformedResultMatches(actual.results[1], candidatePremises2)
-    assertThatTransformedResultMatches(actual.results[2], candidatePremises3)
-  }
+    assertThat(actual.resultsCount).isEqualTo(2)
 
-  private fun assertThatTransformedResultMatches(actual: ApiSpaceSearchResult, expected: CandidatePremises) {
-    assertThat(actual.premises).isNotNull
-    assertThat(actual.premises!!.id).isEqualTo(expected.premisesId)
-    assertThat(actual.premises!!.apType).isEqualTo(expected.apType.asApiType())
-    assertThat(actual.premises!!.name).isEqualTo(expected.name)
-    assertThat(actual.premises!!.addressLine1).isEqualTo(expected.addressLine1)
-    assertThat(actual.premises!!.addressLine2).isEqualTo(expected.addressLine2)
-    assertThat(actual.premises!!.town).isEqualTo(expected.town)
-    assertThat(actual.premises!!.postcode).isEqualTo(expected.postcode)
-    assertThat(actual.premises!!.apArea).isNotNull
-    assertThat(actual.premises!!.apArea!!.id).isEqualTo(expected.apAreaId)
-    assertThat(actual.premises!!.apArea!!.name).isEqualTo(expected.apAreaName)
-    assertThat(actual.premises!!.premisesCharacteristics).isEmpty()
-    assertThat(actual.distanceInMiles).isEqualTo(expected.distanceInMiles.toBigDecimal())
-    assertThat(actual.spacesAvailable).isEmpty()
+    val premises1 = actual.results[0].premises
+    assertThat(premises1).isNotNull
+    premises1!!
+    assertThat(premises1.id).isEqualTo(candidatePremise1Id)
+    assertThat(premises1.apType).isEqualTo(ApType.normal)
+    assertThat(premises1.name).isEqualTo("Some AP 1")
+    assertThat(premises1.addressLine1).isEqualTo("1 The Street")
+    assertThat(premises1.addressLine2).isNull()
+    assertThat(premises1.town).isEqualTo("Townsbury")
+    assertThat(premises1.postcode).isEqualTo("TB1 2AB")
+    assertThat(premises1.apArea.id).isEqualTo(apAreaId1)
+    assertThat(premises1.apArea.name).isEqualTo("Some AP Area 1")
+    assertThat(premises1.premisesCharacteristics).isEmpty()
+    assertThat(premises1.characteristics).isEmpty()
+    assertThat(actual.results[0].distanceInMiles).isEqualTo(BigDecimal.valueOf(1.0))
+    assertThat(actual.results[0].spacesAvailable).isEmpty()
+
+    val premises2 = actual.results[1].premises
+    assertThat(premises2).isNotNull
+    premises2!!
+    assertThat(premises2.id).isEqualTo(candidatePremise2Id)
+    assertThat(premises2.apType).isEqualTo(ApType.esap)
+    assertThat(premises2.name).isEqualTo("Some Other AP")
+    assertThat(premises2.addressLine1).isEqualTo("2 The Street")
+    assertThat(premises2.addressLine2).isEqualTo("Additional Bit")
+    assertThat(premises2.town).isEqualTo("Townton")
+    assertThat(premises2.postcode).isEqualTo("TB1 2AC")
+    assertThat(premises2.apArea.id).isEqualTo(apAreaId2)
+    assertThat(premises2.apArea.name).isEqualTo("Some AP Area 2")
+    assertThat(premises2.premisesCharacteristics).isEmpty()
+    assertThat(premises2.characteristics).isEmpty()
+    assertThat(actual.results[1].distanceInMiles).isEqualTo(BigDecimal.valueOf(2.0))
+    assertThat(actual.results[1].spacesAvailable).isEmpty()
   }
 }
