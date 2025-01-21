bdf701014857e4e429e1003d45128cb06066b617
Toby Batch <toby.batch@digital.justice.co.uk>
2025-01-21 16:23:46 +0000

    feat: adds cas2v2 reference controller

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas2v2/Cas2v2ReferenceDataController.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller.cas2v2
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2v2.ReferenceDataCas2v2Delegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2ApplicationStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2PersistedApplicationStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.reference.Cas2PersistedApplicationStatusFinder
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2.ApplicationStatusTransformer
+
+@Service
+class Cas2v2ReferenceDataController(
+  private val statusTransformer: ApplicationStatusTransformer,
+  private val statusFinder: Cas2PersistedApplicationStatusFinder,
+) : ReferenceDataCas2v2Delegate {
+  override fun referenceDataApplicationStatusGet(): ResponseEntity<List<Cas2ApplicationStatus>> {
+    return ResponseEntity.ok(transformToApi(statusFinder.active()))
+  }
+
+  private fun transformToApi(statusList: List<Cas2PersistedApplicationStatus>): List<Cas2ApplicationStatus> {
+    return statusList.map { status -> statusTransformer.transformModelToApi(status) }
+  }
+}
