0f602738aae58a9ab67b7ffdc7fbb56477f13de8
Toby Batch <toby.batch@digital.justice.co.uk>
2025-01-22 15:48:37 +0000

    feat: adds a search by CRN

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas2v2/Cas2v2PeopleController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas2v2/Cas2v2PeopleController.kt
@@ -10,42 +10,70 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.OASysRiskOfSer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.OASysRiskToSelf
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Person
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PersonRisks
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserQualification
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.ProbationOffenderSearchResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.BadRequestProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.ForbiddenProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.NotFoundProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.NomisUserService
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.OffenderService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.OASysSectionsTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.PersonTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.RisksTransformer
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.OffenderService as DeliusOffenderService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.OffenderService as OASysOffenderService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.OffenderService as NomsOffenderService
 
 @Service("Cas2v2PeopleController")
 class Cas2v2PeopleController(
-  private val offenderService: OffenderService,
+  private val nomsOffenderService: NomsOffenderService,
+  private val deliusOffenderService: DeliusOffenderService,
   private val oaSysOffenderService: OASysOffenderService,
   private val oaSysSectionsTransformer: OASysSectionsTransformer,
   private val personTransformer: PersonTransformer,
   private val risksTransformer: RisksTransformer,
   private val nomisUserService: NomisUserService,
+  private val deliusUserService: UserService,
 ) : PeopleCas2v2Delegate {
 
-  override fun peopleSearchGet(nomsNumber: String): ResponseEntity<Person> {
-    val currentUser = nomisUserService.getUserForRequest()
+  override fun peopleSearchGet(nomsNumber: String?, crn: String?): ResponseEntity<Person> {
+    return when {
+      nomsNumber != null -> searchByNoms(nomsNumber)
+      crn != null -> searchByCrn(crn)
+      else -> throw BadRequestProblem(errorDetail = "Either nomsNumber or crn must be provided")
+    }
+  }
 
-    val probationOffenderResult = offenderService.getPersonByNomsNumber(nomsNumber, currentUser)
+  @SuppressWarnings("ThrowsCount")
+  private fun searchByNoms(nomsNumber: String): ResponseEntity<Person> {
+    val currentUser = nomisUserService.getUserForRequest()
+    val probationOffenderResult = nomsOffenderService.getPersonByNomsNumber(nomsNumber, currentUser)
 
     when (probationOffenderResult) {
       is ProbationOffenderSearchResult.NotFound -> throw NotFoundProblem(nomsNumber, "Offender")
       is ProbationOffenderSearchResult.Forbidden -> throw ForbiddenProblem()
       is ProbationOffenderSearchResult.Unknown ->
         throw probationOffenderResult.throwable
-          ?: RuntimeException("Could not retrieve person info for Prison Number: $nomsNumber")
+          ?: BadRequestProblem(errorDetail = "Could not retrieve person info for Prison Number: $nomsNumber")
 
       is ProbationOffenderSearchResult.Success.Full -> return ResponseEntity.ok(
         personTransformer.transformProbationOffenderToPersonApi(probationOffenderResult, nomsNumber),
+      )
+    }
+  }
+
+  private fun searchByCrn(crn: String): ResponseEntity<Person> {
+    val deliusUser = deliusUserService.getUserForRequest()
+    val personInfo = deliusOffenderService.getPersonInfoResult(crn, deliusUser.deliusUsername, deliusUser.hasQualification(UserQualification.LAO))
+
+    when (personInfo) {
+      is PersonInfoResult.NotFound -> throw NotFoundProblem(crn, "Offender")
+      is PersonInfoResult.Unknown -> throw personInfo.throwable ?: BadRequestProblem(errorDetail = "Could not retrieve person info for CRN: $crn")
+      is PersonInfoResult.Success -> return ResponseEntity.ok(
+        personTransformer.transformModelToPersonApi(personInfo),
       )
     }
   }
@@ -93,7 +121,7 @@ class Cas2v2PeopleController(
   }
 
   override fun peopleCrnRisksGet(crn: String): ResponseEntity<PersonRisks> {
-    val risks = when (val risksResult = offenderService.getRiskByCrn(crn)) {
+    val risks = when (val risksResult = nomsOffenderService.getRiskByCrn(crn)) {
       is AuthorisableActionResult.Unauthorised -> throw ForbiddenProblem()
       is AuthorisableActionResult.NotFound -> throw NotFoundProblem(crn, "Person")
       is AuthorisableActionResult.Success -> risksResult.entity
@@ -103,7 +131,7 @@ class Cas2v2PeopleController(
   }
 
   private fun getOffenderDetails(crn: String): OffenderDetailSummary {
-    val offenderDetails = when (val offenderDetailsResult = offenderService.getOffenderByCrn(crn)) {
+    val offenderDetails = when (val offenderDetailsResult = nomsOffenderService.getOffenderByCrn(crn)) {
       is AuthorisableActionResult.NotFound -> throw NotFoundProblem(crn, "Person")
       is AuthorisableActionResult.Unauthorised -> throw ForbiddenProblem()
       is AuthorisableActionResult.Success -> offenderDetailsResult.entity
