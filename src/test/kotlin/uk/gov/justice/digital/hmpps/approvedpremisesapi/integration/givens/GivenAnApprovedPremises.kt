395091a3ded7b0460bda6ed1021ed7d67c08506c
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-01-22 11:53:15 +0000

    Tidy up day premise summary tests
    This is in preparation for changes.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/givens/GivenAnApprovedPremises.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/givens/GivenAnApprovedPremises.kt
@@ -2,16 +2,18 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens
 
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ProbationRegionEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomStringMultiCaseWithNumbers
 
 fun IntegrationTestBase.givenAnApprovedPremises(
   name: String = randomStringMultiCaseWithNumbers(8),
   supportsSpaceBookings: Boolean = false,
+  region: ProbationRegionEntity? = null,
 ): ApprovedPremisesEntity {
   return approvedPremisesEntityFactory
     .produceAndPersist {
       withName(name)
-      withProbationRegion(probationRegionEntityFactory.produceAndPersist())
+      withProbationRegion(region ?: probationRegionEntityFactory.produceAndPersist())
       withLocalAuthorityArea(localAuthorityEntityFactory.produceAndPersist())
       withSupportsSpaceBookings(supportsSpaceBookings)
     }
