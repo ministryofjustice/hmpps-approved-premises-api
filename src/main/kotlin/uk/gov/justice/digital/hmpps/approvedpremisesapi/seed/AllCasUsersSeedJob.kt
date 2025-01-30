a54052d5118da207e56798435cdf57818615390a
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-01-30 12:43:53 +0000

    Tidy up User Seed Jobs
    This commit renames the `ApStaffUsersSeedJob` to `UsersBasicSeedJob` to better reflect it’s purpose, and improves documentation on the users seed jobs.
    
    It also improves the logs to indicate if a user already existed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/AllCasUsersSeedJob.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.approvedpremisesapi.seed
-
-import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
-
-@Component
-class AllCasUsersSeedJob(userService: UserService) : AbstractUsersSeedJob(ServiceName.entries, userService)
