e349d96878ea8d1dec9a99d15b1a0d85be68c486
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-03 14:27:54 +0000

    Add PlacementRequestBookingSummary.characteristics
    To support this we’ve had to move the Cas1SpaceCharacteristics type back into _shared.yml. In time we’ll move placement request code into a /cas1 endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementRequestBookingSummaryTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementRequestBookingSummaryTransformer.kt
@@ -1,12 +1,20 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1SpaceCharacteristic
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementRequestBookingSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas1SpaceBookingEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.CharacteristicEntity
 
 @Component
 class PlacementRequestBookingSummaryTransformer {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   fun transformJpaToApi(jpa: BookingEntity) = PlacementRequestBookingSummary(
     id = jpa.id,
     premisesId = jpa.premises.id,
@@ -15,8 +23,10 @@ class PlacementRequestBookingSummaryTransformer {
     departureDate = jpa.departureDate,
     createdAt = jpa.createdAt.toInstant(),
     type = PlacementRequestBookingSummary.Type.legacy,
+    characteristics = null,
   )
 
+  @SuppressWarnings("SwallowedException")
   fun transformJpaToApi(jpa: Cas1SpaceBookingEntity) = PlacementRequestBookingSummary(
     id = jpa.id,
     premisesId = jpa.premises.id,
@@ -25,5 +35,12 @@ class PlacementRequestBookingSummaryTransformer {
     departureDate = jpa.canonicalDepartureDate,
     createdAt = jpa.createdAt.toInstant(),
     type = PlacementRequestBookingSummary.Type.space,
+    characteristics = jpa.criteria.mapNotNull { it.toCas1SpaceCharacteristicOrNull() },
   )
+
+  private fun CharacteristicEntity.toCas1SpaceCharacteristicOrNull() =
+    Cas1SpaceCharacteristic.entries.find { it.name == propertyName } ?: run {
+      log.warn("Couldn't find a Cas1SpaceCharacteristic enum entry for propertyName $propertyName")
+      null
+    }
 }
