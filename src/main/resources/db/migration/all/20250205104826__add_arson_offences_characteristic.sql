4487d9cdef130f3657bc7db442668b11e752a96e
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-05 11:05:24 +0000

    Add arson offence characteristic
    This characteristic will be associated with any placement request that was created from an assessment where ‘arson offences’ was selected. This is currently represented by the isArsonSuitable characteristic, but that mapping is incorrect and will be repurposed in subsequent changes.
    
    This characteristic is disabled by default because it will not be added to any placement requests once the aforementioned changes have been made

---
+++ src/main/resources/db/migration/all/20250205104826__add_arson_offences_characteristic.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."characteristics" (id,"name",service_scope,model_scope,property_name,is_active)
+VALUES ('8655cb07-3419-4f24-a579-f57d1d5495b4','N/A','approved-premises','premises','arsonOffences',false)
