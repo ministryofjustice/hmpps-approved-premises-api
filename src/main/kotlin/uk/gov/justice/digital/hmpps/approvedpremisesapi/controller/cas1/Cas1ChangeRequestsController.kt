ea2848b776bd4678851fac8d24b8f9b0ce160f6c
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-07 09:16:17 +0000

    Add change request API definitions

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas1/Cas1ChangeRequestsController.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller.cas1
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas1.ChangeRequestsCas1Delegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1ChangeRequestSortField
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1ChangeRequestStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1ChangeRequestSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1NewChangeRequest
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SortDirection
+
+@Service
+class Cas1ChangeRequestsController : ChangeRequestsCas1Delegate {
+
+  override fun create(cas1NewChangeRequest: Cas1NewChangeRequest): ResponseEntity<Unit> {
+    return super.create(cas1NewChangeRequest)
+  }
+
+  override fun find(
+    status: Cas1ChangeRequestStatus?,
+    page: Int?,
+    sortBy: Cas1ChangeRequestSortField?,
+    sortDirection: SortDirection?,
+  ): ResponseEntity<List<Cas1ChangeRequestSummary>> {
+    return super.find(status, page, sortBy, sortDirection)
+  }
+}
