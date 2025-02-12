a6c5a773a95195d36d7be5e4a5b355b03aa57330
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-12 08:20:31 +0000

    Remove OSIV from /cas1 endpoints
    Open Entity (Session) In View (OSIV) is enabled by default in Spring, and whilst simplifying development wrt. lazy loading JPA relationships, it results in a connection being held open from it’s first usage to completion of the API request. This can lead to empty connection pools under load, especially when calls are being made to upstream services in the same thread that may take some time to respond.
    
    Because removing OSIV can cause subtle issues (especially are object equality), we’re only applying it to the /cas1 endpoints in this commit, most of which are not yet in use in production.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/config/CasOpenEntityManagerInViewInterceptor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/config/CasOpenEntityManagerInViewInterceptor.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.config
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.ServletWebRequest
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -11,11 +13,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Component
 class CasOpenEntityManagerInViewInterceptor : OpenEntityManagerInViewInterceptor() {
   override fun preHandle(request: WebRequest) {
+    if (!shouldApply(request)) {
+      return
+    }
     super.preHandle(request)
   }
 
   override fun afterCompletion(request: WebRequest, ex: Exception?) {
+    if (!shouldApply(request)) {
+      return
+    }
     super.afterCompletion(request, ex)
+  }
+
+  private fun shouldApply(webRequest: WebRequest): Boolean {
+    return if (webRequest is ServletWebRequest) {
+      return !webRequest.request.requestURI.startsWith("/cas1")
+    } else {
+      true
+    }
   }
 }
 
