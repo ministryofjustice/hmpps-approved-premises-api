63e8ff200cb4aef0c5eba3d8070550a3f0479a35
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-13 11:15:24 +0000

    add limit to sapce bookings result

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/Cas1SpaceBookingEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/Cas1SpaceBookingEntity.kt
@@ -195,6 +195,7 @@ interface Cas1SpaceBookingRepository : JpaRepository<Cas1SpaceBookingEntity, UUI
       b.crn = :crn AND
       b.cancellation_occurred_at IS NULL 
       ORDER by b.canonical_arrival_date
+      LIMIT 100
     """,
     nativeQuery = true,
   )
