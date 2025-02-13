68a45cafd5bc31684be5630dddd4d44a104abc0b
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-02-13 13:47:14 +0000

    added awaits to the tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2DomainEventListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas2/Cas2DomainEventListenerTest.kt
@@ -2,10 +2,13 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.cas2
 
 import com.ninjasquad.springmockk.SpykBean
 import io.mockk.verify
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.untilNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import software.amazon.awssdk.services.sns.model.PublishRequest
+import software.amazon.awssdk.services.sns.model.PublishResponse
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.DomainEventListener
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
@@ -26,7 +29,7 @@ class Cas2DomainEventListenerTest : IntegrationTestBase() {
 
   private val domainEventsClient by lazy { domainEventsTopic.snsClient }
 
-  private fun publishMessageToTopic(eventType: String, json: String = "{}") {
+  private fun publishMessageToTopic(eventType: String, json: String = "{}"): PublishResponse {
     val sendMessageRequest = PublishRequest.builder()
       .topicArn(domainEventsTopic.arn)
       .message(json)
@@ -36,33 +39,33 @@ class Cas2DomainEventListenerTest : IntegrationTestBase() {
         ),
       )
       .build()
-    domainEventsClient.publish(sendMessageRequest).get()
+    return domainEventsClient.publish(sendMessageRequest).get()
   }
 
   @Test
   fun `Start to process Allocation Changed Message on Domain Events Topic`() {
     val eventType = "offender-management.allocation.changed"
+    val response = publishMessageToTopic(eventType)
 
-    publishMessageToTopic(eventType)
-    TimeUnit.MILLISECONDS.sleep(3000)
+    await untilNotNull { response }
     verify(exactly = 1) { mockDomainEventListener.processMessage(any()) }
   }
 
   @Test
   fun `Start to process Location Changed Message on Domain Events Topic`() {
     val eventType = "prisoner-offender-search.prisoner.updated"
+    val response = publishMessageToTopic(eventType)
 
-    publishMessageToTopic(eventType)
-    TimeUnit.MILLISECONDS.sleep(3000)
+    await untilNotNull { response }
     verify(exactly = 1) { mockDomainEventListener.processMessage(any()) }
   }
 
   @Test
   fun `Do not process Message that is not a required event type`() {
     val eventType = "unwanted"
+    val response = publishMessageToTopic(eventType)
 
-    publishMessageToTopic(eventType)
-    TimeUnit.MILLISECONDS.sleep(3000)
+    await untilNotNull { response }
     verify(exactly = 0) { mockDomainEventListener.processMessage(any()) }
   }
 }
