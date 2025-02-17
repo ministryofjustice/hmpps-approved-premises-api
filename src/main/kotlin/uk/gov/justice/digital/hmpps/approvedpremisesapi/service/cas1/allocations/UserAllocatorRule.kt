db299f4fc51f67da8be3b97482a9d700bd840cd5
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-17 16:51:07 +0000

    un-deprecate placement app allocation logic
    Whilst this logic always returns null, it’s conceivable that it will be required in the future so have un-deprecated it for now

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/allocations/UserAllocatorRule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas1/allocations/UserAllocatorRule.kt
@@ -24,13 +24,5 @@ interface UserAllocatorRule {
   )
   fun evaluatePlacementRequest(placementRequestEntity: PlacementRequestEntity): UserAllocatorRuleOutcome = UserAllocatorRuleOutcome.Skip
 
-  @Deprecated(
-    """
-    This function was added to support the switch over from the Legacy behaviour to the
-    new allocation behaviour. The new allocation behaviour will never auto allocate a 
-    placement application, so this function will always return UserAllocatorRuleOutcome.Skip
-  """,
-    replaceWith = ReplaceWith("remove any call to this function as it will always return UserAllocatorRuleOutcome.Skip"),
-  )
   fun evaluatePlacementApplication(placementApplicationEntity: PlacementApplicationEntity): UserAllocatorRuleOutcome = UserAllocatorRuleOutcome.Skip
 }
