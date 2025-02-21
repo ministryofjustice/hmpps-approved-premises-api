5b7ea5755598ab08bcb212297833440923683f85
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-02-21 10:51:41 +0000

    Populate `PlacementRequestDetail.spaceBookings`
    Populates the `spaceBookings` and `legacySpaceBooking` fields of `PlacementRequestDetail`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementRequestDetailTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/PlacementRequestDetailTransformer.kt
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementRequestBookingSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementRequestDetail
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.CancellationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PlacementRequestEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas1.Cas1SpaceBookingTransformer
 
 @Component
 class PlacementRequestDetailTransformer(
@@ -12,6 +14,8 @@ class PlacementRequestDetailTransformer(
   private val cancellationTransformer: CancellationTransformer,
   bookingSummaryTransformer: PlacementRequestBookingSummaryTransformer,
   private val applicationTransformer: ApplicationsTransformer,
+  private val personTransformer: PersonTransformer,
+  private val cas1SpaceBookingTransformer: Cas1SpaceBookingTransformer,
 ) {
   val placementRequestBookingSummaryTransformer = PlacementRequestBookingSummariesTransformer(
     bookingSummaryTransformer,
@@ -19,6 +23,8 @@ class PlacementRequestDetailTransformer(
 
   fun transformJpaToApi(jpa: PlacementRequestEntity, personInfo: PersonInfoResult, cancellations: List<CancellationEntity>): PlacementRequestDetail {
     val placementRequest = placementRequestTransformer.transformJpaToApi(jpa, personInfo)
+    val personSummaryInfo = personTransformer.personInfoResultToPersonSummaryInfoResult(personInfo)
+    val placementRequestBookingSummary = placementRequestBookingSummaryTransformer.getBookingSummary(jpa)
 
     return PlacementRequestDetail(
       id = placementRequest.id,
@@ -42,12 +48,16 @@ class PlacementRequestDetailTransformer(
       assessor = placementRequest.assessor,
       notes = placementRequest.notes,
       cancellations = cancellations.mapNotNull { cancellationTransformer.transformJpaToApi(it) },
-      booking = placementRequestBookingSummaryTransformer.getBookingSummary(jpa),
+      booking = placementRequestBookingSummary,
+      spaceBookings = jpa.spaceBookings.filter { it.isActive() }.map { cas1SpaceBookingTransformer.transformToSummary(it, personSummaryInfo) },
+      legacyBooking = if (placementRequestBookingSummary?.type == PlacementRequestBookingSummary.Type.legacy) {
+        placementRequestBookingSummary
+      } else {
+        null
+      },
       isWithdrawn = jpa.isWithdrawn,
       isParole = jpa.isParole,
       application = applicationTransformer.transformJpaToApi(jpa.application, personInfo),
-      spaceBookings = emptyList(),
-      legacyBooking = null,
     )
   }
 }
