1b64cf87a48092c94477b7ddf91893cb32ae7405
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-02-24 11:00:15 +0000

    implemented prisoner location service for location transfer - no tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/PrisonerLocationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/PrisonerLocationService.kt
@@ -2,10 +2,22 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.Prisoner
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PrisonerSearchClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent.HmppsDomainEvent
+import java.net.URI
+import java.util.*
 
 @Service
-class PrisonerLocationService {
+class PrisonerLocationService(
+  private val prisonerSearchClient: PrisonerSearchClient,
+  private val applicationRepository: Cas2ApplicationRepository,
+  private val prisonerLocationRepository: Cas2PrisonerLocationRepository,
+) {
 
   private val log = LoggerFactory.getLogger(this::class.java)
 
@@ -14,6 +26,39 @@ class PrisonerLocationService {
   }
 
   fun handleLocationChangedEvent(event: HmppsDomainEvent) {
-    log.info("Handle location changed event at ${event.occurredAt}")
+    val nomsNumber = event.personReference.findNomsNumber() ?: throw Exception("No nomsNumber found")
+    val detailUrl = event.detailUrl ?: throw Exception("No detail URL found")
+
+    val applications = applicationRepository.findAllSubmittedApplicationByNomsNumber(nomsNumber)
+
+    if (applications.isEmpty()) {
+      log.info("No submitted applications found for NomsNumber $nomsNumber")
+      return
+    }
+
+    val prisoner = prisonerSearchClient.getPrisoner(URI.create(detailUrl))
+      ?: throw Exception("No prisoner found for detailUrl $detailUrl")
+
+    applicationRepository.findAllSubmittedApplicationByNomsNumber(nomsNumber).forEach {
+      updatePrisonerLocation(it, prisoner, event)
+    }
+  }
+
+  fun updatePrisonerLocation(application: Cas2ApplicationEntity, prisoner: Prisoner, event: HmppsDomainEvent) {
+    val oldPrisonerLocation = prisonerLocationRepository.findPrisonerLocation(application.id)
+    if (oldPrisonerLocation != null) {
+      val oldPrisonerLocationUpdated = oldPrisonerLocation.copy(occurredAt = event.occurredAt.toOffsetDateTime())
+      prisonerLocationRepository.save(oldPrisonerLocationUpdated)
+    }
+    prisonerLocationRepository.save(
+      Cas2PrisonerLocationEntity(
+        id = UUID.randomUUID(),
+        application = application,
+        prisonCode = prisoner.prisonId,
+        staffId = null,
+        occurredAt = event.occurredAt.toOffsetDateTime(),
+        endDate = null,
+      ),
+    )
   }
 }
