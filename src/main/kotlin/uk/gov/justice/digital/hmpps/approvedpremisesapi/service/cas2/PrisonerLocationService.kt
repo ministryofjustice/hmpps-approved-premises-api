893af3caf8c0568d652210b4828c1f1931e28ee3
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-03-10 09:59:39 +0000

    handled allocation changed event with unit tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/PrisonerLocationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2/PrisonerLocationService.kt
@@ -1,16 +1,16 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.Prisoner
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ManagePomCasesClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PrisonerSearchClient
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.NomisUserRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.IgnorableMessageException
 import java.net.URI
+import java.time.ZonedDateTime
 import java.util.UUID
 
 @Service
@@ -18,12 +18,40 @@ class PrisonerLocationService(
   private val prisonerSearchClient: PrisonerSearchClient,
   private val applicationRepository: Cas2ApplicationRepository,
   private val prisonerLocationRepository: Cas2PrisonerLocationRepository,
+  private val nomisUserRepository: NomisUserRepository,
+  private val managePomCasesClient: ManagePomCasesClient,
 ) {
 
-  private val log = LoggerFactory.getLogger(this::class.java)
-
+  @SuppressWarnings("ThrowsCount")
   fun handleAllocationChangedEvent(event: HmppsDomainEvent) {
-    log.info("Handle allocation changed event ${event.occurredAt}")
+    val nomsNumber = event.personReference.findNomsNumber() ?: throw IgnorableMessageException("No nomsNumber found")
+    val detailUrl = event.detailUrl ?: throw IgnorableMessageException("No detail URL found")
+    val staffCode = event.staffCode ?: throw IgnorableMessageException("No staff code found")
+
+    val applications = applicationRepository.findAllSubmittedApplicationByNomsNumber(nomsNumber)
+    if (applications.isEmpty()) {
+      return
+    }
+
+    val user = nomisUserRepository.findByNomisStaffId(staffCode.toLong())
+      ?: throw IgnorableMessageException("No user found for staffCode $staffCode")
+
+    val pomAllocation = managePomCasesClient.getPomAllocation(URI.create(detailUrl))
+      ?: throw IgnorableMessageException("No POM allocation found for detailUrl $detailUrl")
+
+    applications.forEach {
+      updatePrisonerLocation(
+        event.occurredAt,
+        Cas2PrisonerLocationEntity(
+          id = UUID.randomUUID(),
+          application = it,
+          prisonCode = pomAllocation.prison.code,
+          staffId = user.id,
+          occurredAt = event.occurredAt.toOffsetDateTime(),
+          endDate = null,
+        ),
+      )
+    }
   }
 
   @SuppressWarnings("ThrowsCount")
@@ -40,23 +68,24 @@ class PrisonerLocationService(
       ?: throw IgnorableMessageException("No prisoner found for detailUrl $detailUrl")
 
     applications.forEach {
-      updatePrisonerLocation(it, prisoner, event)
+      updatePrisonerLocation(
+        event.occurredAt,
+        Cas2PrisonerLocationEntity(
+          id = UUID.randomUUID(),
+          application = it,
+          prisonCode = prisoner.prisonId,
+          staffId = null,
+          occurredAt = event.occurredAt.toOffsetDateTime(),
+          endDate = null,
+        ),
+      )
     }
   }
 
-  fun updatePrisonerLocation(application: Cas2ApplicationEntity, prisoner: Prisoner, event: HmppsDomainEvent) {
-    val oldPrisonerLocation = prisonerLocationRepository.findPrisonerLocation(application.id)
-      ?: throw IgnorableMessageException("No null prisoner location found for applicationId ${application.id}")
-    prisonerLocationRepository.save(oldPrisonerLocation.copy(endDate = event.occurredAt.toOffsetDateTime()))
-    prisonerLocationRepository.save(
-      Cas2PrisonerLocationEntity(
-        id = UUID.randomUUID(),
-        application = application,
-        prisonCode = prisoner.prisonId,
-        staffId = null,
-        occurredAt = event.occurredAt.toOffsetDateTime(),
-        endDate = null,
-      ),
-    )
+  fun updatePrisonerLocation(occurredAt: ZonedDateTime, location: Cas2PrisonerLocationEntity) {
+    val oldPrisonerLocation = prisonerLocationRepository.findPrisonerLocation(location.application.id)
+      ?: throw IgnorableMessageException("No null prisoner location found for applicationId ${location.application.id}")
+    prisonerLocationRepository.save(oldPrisonerLocation.copy(endDate = occurredAt.toOffsetDateTime()))
+    prisonerLocationRepository.save(location)
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas2/PrisonerLocationServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/service/cas2/PrisonerLocationServiceTest.kt
@@ -6,6 +6,9 @@ import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ManagePomCasesClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PomAllocation
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.Prison
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.Prisoner
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PrisonerSearchClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.Cas2ApplicationEntityFactory
@@ -14,6 +17,7 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2Applicati
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2ApplicationRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas2PrisonerLocationRepository
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.NomisUserRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent.HmppsDomainEventPersonReference
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.domainevent.PersonIdentifier
@@ -28,19 +32,25 @@ class PrisonerLocationServiceTest {
   private val mockPrisonerLocationRepository = mockk<Cas2PrisonerLocationRepository>()
   private val mockApplicationRepository = mockk<Cas2ApplicationRepository>()
   private val mockPrisonerSearchClient = mockk<PrisonerSearchClient>()
+  private val mockManagePomCasesClient = mockk<ManagePomCasesClient>()
+  private val mockNomisUserRepository = mockk<NomisUserRepository>()
 
   private val prisonerLocationService: PrisonerLocationService = PrisonerLocationService(
     mockPrisonerSearchClient,
     mockApplicationRepository,
     mockPrisonerLocationRepository,
+    mockNomisUserRepository,
+    mockManagePomCasesClient,
   )
 
   private val prisoner = Prisoner(prisonId = "A1234AB")
   private val eventType = "prisoner-offender-search.prisoner.updated"
   private val nomsNumber = "NOMSABC"
   private val detailUrl = "some/url"
+  private val staffCode = "12345"
 
   private val user = NomisUserEntityFactory()
+    .withNomisStaffCode(staffCode.toLong())
     .produce()
   private val occurredAt = Instant.now().atZone(ZoneId.systemDefault())
   private val application = Cas2ApplicationEntityFactory()
@@ -57,7 +67,16 @@ class PrisonerLocationServiceTest {
     endDate = null,
   )
 
-  val event = HmppsDomainEvent(
+  private val anotherOldPrisonerLocation = Cas2PrisonerLocationEntity(
+    id = UUID.randomUUID(),
+    application = application,
+    prisonCode = "LON",
+    staffId = null,
+    occurredAt = occurredAt.toOffsetDateTime(),
+    endDate = null,
+  )
+
+  private val locationEvent = HmppsDomainEvent(
     eventType = eventType,
     version = 1,
     detailUrl = detailUrl,
@@ -65,6 +84,200 @@ class PrisonerLocationServiceTest {
     description = "anything",
     personReference = HmppsDomainEventPersonReference(listOf(PersonIdentifier("NOMS", nomsNumber))),
   )
+
+  private val allocationEvent = HmppsDomainEvent(
+    eventType = eventType,
+    version = 1,
+    detailUrl = detailUrl,
+    occurredAt = occurredAt,
+    description = "anything",
+    additionalInformation = mapOf("staffCode" to staffCode),
+    personReference = HmppsDomainEventPersonReference(listOf(PersonIdentifier("NOMS", nomsNumber))),
+  )
+
+  val pomAllocation = PomAllocation(Prison(code = "ABC"))
+
+  @Test
+  fun `handle Allocation Changed Event and save new prisonerLocation to table and update previous location endDate`() {
+    val oldPrisonerLocationWithEndDate = anotherOldPrisonerLocation.copy(endDate = occurredAt.toOffsetDateTime())
+
+    val newPrisonerLocation = Cas2PrisonerLocationEntity(
+      id = UUID.randomUUID(),
+      application = application,
+      prisonCode = pomAllocation.prison.code,
+      staffId = user.id,
+      occurredAt = occurredAt.toOffsetDateTime(),
+      endDate = null,
+    )
+
+    val applications = listOf(application)
+
+    every { mockManagePomCasesClient.getPomAllocation(any()) } returns pomAllocation
+    every { mockNomisUserRepository.findByNomisStaffId(any()) } returns user
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+    every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) } returns oldPrisonerLocation
+    every { mockPrisonerLocationRepository.save(any()) } returnsMany listOf(
+      oldPrisonerLocationWithEndDate,
+      newPrisonerLocation,
+    )
+
+    prisonerLocationService.handleAllocationChangedEvent(allocationEvent)
+
+    verify(exactly = 1) { mockManagePomCasesClient.getPomAllocation(any()) }
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+    verify(exactly = 1) { mockNomisUserRepository.findByNomisStaffId(eq(staffCode.toLong())) }
+    verify(exactly = 1) { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) }
+    verify(exactly = 2) { mockPrisonerLocationRepository.save(any()) }
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and throw error when prisoner location table is not populated with that prisoner's location`() {
+    val applications = listOf(application)
+
+    every { mockManagePomCasesClient.getPomAllocation(any()) } returns pomAllocation
+    every { mockNomisUserRepository.findByNomisStaffId(any()) } returns user
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+    every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) } returns null
+
+    val exception =
+      assertThrows<IgnorableMessageException> { prisonerLocationService.handleAllocationChangedEvent(allocationEvent) }
+    assertThat(exception.message).isEqualTo("No null prisoner location found for applicationId ${application.id}")
+
+    verify(exactly = 1) { mockManagePomCasesClient.getPomAllocation(any()) }
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+    verify(exactly = 1) { mockNomisUserRepository.findByNomisStaffId(eq(staffCode.toLong())) }
+    verify(exactly = 1) { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) }
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and throw error when pom allocation not found from event detailUrl`() {
+    val applications = listOf(application)
+
+    every { mockManagePomCasesClient.getPomAllocation(any()) } returns null
+    every { mockNomisUserRepository.findByNomisStaffId(any()) } returns user
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+
+    val exception =
+      assertThrows<IgnorableMessageException> { prisonerLocationService.handleAllocationChangedEvent(allocationEvent) }
+    assertThat(exception.message).isEqualTo("No POM allocation found for detailUrl $detailUrl")
+
+    verify(exactly = 1) { mockManagePomCasesClient.getPomAllocation(any()) }
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+    verify(exactly = 1) { mockNomisUserRepository.findByNomisStaffId(eq(staffCode.toLong())) }
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and throw error when user not found from event staffCode`() {
+    val applications = listOf(application)
+
+    every { mockNomisUserRepository.findByNomisStaffId(any()) } returns null
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+
+    val exception =
+      assertThrows<IgnorableMessageException> { prisonerLocationService.handleAllocationChangedEvent(allocationEvent) }
+    assertThat(exception.message).isEqualTo("No user found for staffCode $staffCode")
+
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+    verify(exactly = 1) { mockNomisUserRepository.findByNomisStaffId(eq(staffCode.toLong())) }
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and throw error when there is no detailUrl in the event`() {
+    val eventWithNoDetailUrl = HmppsDomainEvent(
+      eventType = eventType,
+      version = 1,
+      detailUrl = null,
+      occurredAt = occurredAt,
+      description = "anything",
+      personReference = HmppsDomainEventPersonReference(listOf(PersonIdentifier("NOMS", nomsNumber))),
+    )
+
+    val exception =
+      assertThrows<IgnorableMessageException> {
+        prisonerLocationService.handleAllocationChangedEvent(
+          eventWithNoDetailUrl,
+        )
+      }
+    assertThat(exception.message).isEqualTo("No detail URL found")
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and throw error when there is no nomsNumber in the event`() {
+    val eventWithNoNomsNumber = HmppsDomainEvent(
+      eventType = eventType,
+      version = 1,
+      detailUrl = detailUrl,
+      occurredAt = occurredAt,
+      description = "anything",
+      personReference = HmppsDomainEventPersonReference(listOf()),
+    )
+
+    val exception =
+      assertThrows<IgnorableMessageException> {
+        prisonerLocationService.handleAllocationChangedEvent(
+          eventWithNoNomsNumber,
+        )
+      }
+    assertThat(exception.message).isEqualTo("No nomsNumber found")
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and do nothing if there are no applications associated with the event`() {
+    val applications = listOf<Cas2ApplicationEntity>()
+
+    every { mockPrisonerSearchClient.getPrisoner(any()) } returns null
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+
+    prisonerLocationService.handleAllocationChangedEvent(allocationEvent)
+
+    verify(exactly = 0) { mockPrisonerSearchClient.getPrisoner(any()) }
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+  }
+
+  @Test
+  fun `handle Allocation Changed Event and save new prisonerLocation to table and update previous location endDate with multiple applications`() {
+    val application2 = Cas2ApplicationEntityFactory()
+      .withNomsNumber(nomsNumber)
+      .withCreatedByUser(user)
+      .produce()
+    val application3 = Cas2ApplicationEntityFactory()
+      .withNomsNumber(nomsNumber)
+      .withCreatedByUser(user)
+      .produce()
+
+    val oldPrisonerLocation2 = Cas2PrisonerLocationEntity(
+      id = UUID.randomUUID(),
+      application = application2,
+      prisonCode = "LON",
+      staffId = application2.createdByUser.id,
+      occurredAt = occurredAt.toOffsetDateTime(),
+      endDate = null,
+    )
+    val oldPrisonerLocation3 = Cas2PrisonerLocationEntity(
+      id = UUID.randomUUID(),
+      application = application3,
+      prisonCode = "LON",
+      staffId = application3.createdByUser.id,
+      occurredAt = occurredAt.toOffsetDateTime(),
+      endDate = null,
+    )
+
+    val applications = listOf(application, application2, application3)
+
+    every { mockPrisonerSearchClient.getPrisoner(any()) } returns prisoner
+    every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
+    every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) } returns oldPrisonerLocation
+    every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application2.id)) } returns oldPrisonerLocation2
+    every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application3.id)) } returns oldPrisonerLocation3
+    every { mockPrisonerLocationRepository.save(any()) } returns null
+
+    prisonerLocationService.handleLocationChangedEvent(allocationEvent)
+
+    verify(exactly = 1) { mockPrisonerSearchClient.getPrisoner(any()) }
+    verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
+    verify(exactly = 3) { mockPrisonerLocationRepository.findPrisonerLocation(any()) }
+    verify(exactly = 6) { mockPrisonerLocationRepository.save(any()) }
+  }
 
   @Test
   fun `handle Location Changed Event and save new prisonerLocation to table and update previous location endDate`() {
@@ -89,7 +302,7 @@ class PrisonerLocationServiceTest {
       newPrisonerLocation,
     )
 
-    prisonerLocationService.handleLocationChangedEvent(event)
+    prisonerLocationService.handleLocationChangedEvent(locationEvent)
 
     verify(exactly = 1) { mockPrisonerSearchClient.getPrisoner(any()) }
     verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
@@ -106,7 +319,7 @@ class PrisonerLocationServiceTest {
     every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application.id)) } returns null
 
     val exception =
-      assertThrows<IgnorableMessageException> { prisonerLocationService.handleLocationChangedEvent(event) }
+      assertThrows<IgnorableMessageException> { prisonerLocationService.handleLocationChangedEvent(locationEvent) }
     assertThat(exception.message).isEqualTo("No null prisoner location found for applicationId ${application.id}")
 
     verify(exactly = 1) { mockPrisonerSearchClient.getPrisoner(any()) }
@@ -122,7 +335,7 @@ class PrisonerLocationServiceTest {
     every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
 
     val exception =
-      assertThrows<IgnorableMessageException> { prisonerLocationService.handleLocationChangedEvent(event) }
+      assertThrows<IgnorableMessageException> { prisonerLocationService.handleLocationChangedEvent(locationEvent) }
     assertThat(exception.message).isEqualTo("No prisoner found for detailUrl $detailUrl")
 
     verify(exactly = 1) { mockPrisonerSearchClient.getPrisoner(any()) }
@@ -168,7 +381,7 @@ class PrisonerLocationServiceTest {
     every { mockPrisonerSearchClient.getPrisoner(any()) } returns null
     every { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) } returns applications
 
-    prisonerLocationService.handleLocationChangedEvent(event)
+    prisonerLocationService.handleLocationChangedEvent(locationEvent)
 
     verify(exactly = 0) { mockPrisonerSearchClient.getPrisoner(any()) }
     verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
@@ -211,7 +424,7 @@ class PrisonerLocationServiceTest {
     every { mockPrisonerLocationRepository.findPrisonerLocation(eq(application3.id)) } returns oldPrisonerLocation3
     every { mockPrisonerLocationRepository.save(any()) } returns null
 
-    prisonerLocationService.handleLocationChangedEvent(event)
+    prisonerLocationService.handleLocationChangedEvent(locationEvent)
 
     verify(exactly = 1) { mockPrisonerSearchClient.getPrisoner(any()) }
     verify(exactly = 1) { mockApplicationRepository.findAllSubmittedApplicationByNomsNumber(eq(nomsNumber)) }
