5f9d3a12cb0a2c70eae0af5393bd70d378b7da11
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-03-11 15:36:30 +0000

    tests: fixed nomis tests to pass if not same prison for cas2v2

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2v2/Cas2v2OffenderService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/cas2v2/Cas2v2OffenderService.kt
@@ -3,13 +3,17 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2v2
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.FullPerson
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ApDeliusContextApiClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ClientResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.PrisonsApiClient
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.ProbationOffenderSearchApiClient
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.cas2v2.Cas2v2UserEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.cas2v2.Cas2v2UserType
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.InmateDetail
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.probationoffendersearchapi.ProbationOffenderDetail
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.results.AuthorisableActionResult
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.ProbationOffenderSearchResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2v2.Cas2v2PersonTransformer
 
 @Service
 @Suppress(
@@ -18,13 +22,13 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas2.ProbationOf
 class Cas2v2OffenderService(
   private val prisonsApiClient: PrisonsApiClient,
   private val probationOffenderSearchApiClient: ProbationOffenderSearchApiClient,
+  private val apDeliusContextApiClient: ApDeliusContextApiClient,
+  private val cas2v2PersonTransformer: Cas2v2PersonTransformer,
 ) {
 
   private val log = LoggerFactory.getLogger(this::class.java)
 
-  fun getPersonByNomsNumber(
-    nomsNumber: String,
-  ): ProbationOffenderSearchResult {
+  fun getPersonByNomsNumber(nomsNumber: String, cas2v2User: Cas2v2UserEntity): Cas2v2OffenderSearchResult {
     fun logFailedResponse(probationResponse: ClientResult.Failure<List<ProbationOffenderDetail>>) = log.warn("Could not get inmate details for $nomsNumber", probationResponse.toException())
 
     val probationResponse = probationOffenderSearchApiClient.searchOffenderByNomsNumber(nomsNumber)
@@ -32,46 +36,40 @@ class Cas2v2OffenderService(
     val probationOffenderDetailList = when (probationResponse) {
       is ClientResult.Success -> probationResponse.body
       is ClientResult.Failure.StatusCode -> when (probationResponse.status) {
-        HttpStatus.NOT_FOUND -> return ProbationOffenderSearchResult.NotFound(nomsNumber)
-        HttpStatus.FORBIDDEN -> return ProbationOffenderSearchResult.Forbidden(
-          nomsNumber,
-          probationResponse.toException(),
-        )
-
+        HttpStatus.NOT_FOUND -> return Cas2v2OffenderSearchResult.NotFound(nomisIdOrCrn = nomsNumber)
+        HttpStatus.FORBIDDEN -> return Cas2v2OffenderSearchResult.Forbidden(nomisIdOrCrn = nomsNumber, probationResponse.toException())
         else -> {
           logFailedResponse(probationResponse)
-          return ProbationOffenderSearchResult.Unknown(nomsNumber, probationResponse.toException())
+          return Cas2v2OffenderSearchResult.Unknown(nomisIdOrCrn = nomsNumber, probationResponse.toException())
         }
       }
-
       is ClientResult.Failure -> {
         logFailedResponse(probationResponse)
-        return ProbationOffenderSearchResult.Unknown(nomsNumber, probationResponse.toException())
+        return Cas2v2OffenderSearchResult.Unknown(nomisIdOrCrn = nomsNumber, probationResponse.toException())
       }
     }
 
     if (probationOffenderDetailList.isEmpty()) {
-      return ProbationOffenderSearchResult.NotFound(nomsNumber)
-    } else {
-      val probationOffenderDetail = probationOffenderDetailList[0]
-
-      // check for restrictions or exclusions
-      if (hasRestrictionOrExclusion(probationOffenderDetail)) return ProbationOffenderSearchResult.Forbidden(nomsNumber)
-
-      // check inmate details from Prison API
-      val inmateDetails = getInmateDetailsForProbationOffender(probationOffenderDetail)
-        ?: return ProbationOffenderSearchResult.NotFound(nomsNumber)
-
-      return ProbationOffenderSearchResult.Success.Full(nomsNumber, probationOffenderDetail, inmateDetails)
+      return Cas2v2OffenderSearchResult.NotFound(nomisIdOrCrn = nomsNumber)
     }
-  }
 
-  private fun hasRestrictionOrExclusion(probationOffenderDetail: ProbationOffenderDetail): Boolean = probationOffenderDetail.currentExclusion == true || probationOffenderDetail.currentRestriction == true
+    val probationOffenderDetail = probationOffenderDetailList[0]
 
-  private fun getInmateDetailsForProbationOffender(probationOffenderDetail: ProbationOffenderDetail): InmateDetail? = probationOffenderDetail.otherIds.nomsNumber?.let { nomsNumber ->
-    when (val inmateDetailsResult = getInmateDetailByNomsNumber(probationOffenderDetail.otherIds.crn, nomsNumber)) {
-      is AuthorisableActionResult.Success -> inmateDetailsResult.entity
-      else -> null
+    val exclusionRestriction = hasExclusionRestriction(
+      probationOffenderDetail.currentExclusion ?: true,
+      probationOffenderDetail.currentRestriction ?: true,
+      cas2v2User,
+    )
+
+    return when (exclusionRestriction) {
+      false -> Cas2v2OffenderSearchResult.Success.Full(
+        nomisIdOrCrn = nomsNumber,
+        person = cas2v2PersonTransformer.transformProbationOffenderDetailAndInmateDetailToFullPerson(
+          probationOffenderDetail,
+        ),
+      )
+
+      else -> Cas2v2OffenderSearchResult.Forbidden(nomsNumber)
     }
   }
 
@@ -122,4 +120,64 @@ class Cas2v2OffenderService(
 
     return AuthorisableActionResult.Success(inmateDetail)
   }
+
+  fun getPersonByCrn(crn: String, cas2v2User: Cas2v2UserEntity): Cas2v2OffenderSearchResult {
+    val caseSummariesByCrn = when (val result = apDeliusContextApiClient.getSummariesForCrns(listOf(crn))) {
+      is ClientResult.Success -> result.body
+      is ClientResult.Failure -> return Cas2v2OffenderSearchResult.NotFound(crn)
+    }
+
+    if (caseSummariesByCrn.cases.isEmpty()) {
+      return Cas2v2OffenderSearchResult.NotFound(nomisIdOrCrn = crn)
+    }
+
+    val caseSummary = caseSummariesByCrn.cases[0]
+    val nomsNumber = caseSummary.nomsId
+
+    if (nomsNumber.isNullOrEmpty()) {
+      val exclusionRestriction = hasExclusionRestriction(
+        caseSummary.currentExclusion,
+        caseSummary.currentRestriction,
+        cas2v2User,
+      )
+      return when (exclusionRestriction) {
+        false -> Cas2v2OffenderSearchResult.Success.Full(
+          nomisIdOrCrn = crn,
+          person = cas2v2PersonTransformer.transformCaseSummaryToFullPerson(caseSummary),
+        )
+        else -> Cas2v2OffenderSearchResult.Forbidden(crn)
+      }
+    }
+
+    return getPersonByNomsNumber(nomsNumber, cas2v2User)
+  }
+
+  private fun hasExclusionRestriction(
+    hasExclusion: Boolean,
+    hasRestriction: Boolean,
+    cas2v2User: Cas2v2UserEntity,
+  ): Boolean = when {
+    hasRestriction -> true
+    hasExclusion &&
+      cas2v2User.userType !in setOf(
+      Cas2v2UserType.NOMIS,
+      Cas2v2UserType.DELIUS,
+    ) -> true
+    else -> false
+  }
+}
+
+sealed interface Cas2v2OffenderSearchResult {
+  val nomisIdOrCrn: String
+
+  sealed interface Success : Cas2v2OffenderSearchResult {
+    data class Full(
+      override val nomisIdOrCrn: String,
+      val person: FullPerson,
+    ) : Success
+  }
+
+  data class NotFound(override val nomisIdOrCrn: String) : Cas2v2OffenderSearchResult
+  data class Unknown(override val nomisIdOrCrn: String, val throwable: Throwable? = null) : Cas2v2OffenderSearchResult
+  data class Forbidden(override val nomisIdOrCrn: String, val throwable: Throwable? = null) : Cas2v2OffenderSearchResult
 }
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas2v2/Cas2v2PersonTransformerTest.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.unit.transformer.cas2v2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.FullPerson
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PersonStatus
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PersonType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.CaseSummaryFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.InmateDetailFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.ProbationOffenderDetailFactory
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.deliuscontext.Name
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.prisonsapi.AssignedLivingUnit
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.probationoffendersearchapi.IDs
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas2v2.Cas2v2PersonTransformer
+
+class Cas2v2PersonTransformerTest {
+
+  private val cas2v2PersonTransformer = Cas2v2PersonTransformer()
+
+  private val crn = "crn123"
+  private val nomsNumber = "noms123"
+
+  @Test
+  fun `transformProbationOffenderDetailAndInmateDetailToFullPerson transforms correctly for a full person info`() {
+    val inmateDetails = InmateDetailFactory()
+      .withOffenderNo(nomsNumber)
+      .withAssignedLivingUnit(
+        AssignedLivingUnit(
+          agencyId = "not-my-prison",
+          agencyName = "Not My Prison",
+          locationId = 5,
+          description = "",
+        ),
+      )
+      .produce()
+
+    val probationOffenderDetails = ProbationOffenderDetailFactory()
+      .withOtherIds(otherIds = IDs(crn = crn, nomsNumber = nomsNumber))
+      .withFirstName("John")
+      .withSurname("Smith")
+      .produce()
+
+    val fullPerson: FullPerson = cas2v2PersonTransformer.transformProbationOffenderDetailAndInmateDetailToFullPerson(probationOffenderDetails, inmateDetails)
+    assertThat(fullPerson.crn).isEqualTo(probationOffenderDetails.otherIds.crn)
+    assertThat(fullPerson.nomsNumber).isEqualTo(probationOffenderDetails.otherIds.nomsNumber)
+    assertThat(fullPerson.status).isEqualTo(PersonStatus.unknown)
+    assertThat(fullPerson.name).isEqualTo("John Smith")
+    assertThat(fullPerson.type).isEqualTo(PersonType.fullPerson)
+  }
+
+  @Test
+  fun `transformCaseSummaryToFullPerson transforms correctly for a full person info`() {
+    val caseSummary = CaseSummaryFactory()
+      .withName(Name(forename = "John", surname = "Smith", middleNames = emptyList()))
+      .withCrn(crn)
+      .withNomsId(nomsNumber)
+      .produce()
+
+    val fullPerson: FullPerson = cas2v2PersonTransformer.transformCaseSummaryToFullPerson(caseSummary)
+    assertThat(fullPerson.crn).isEqualTo(caseSummary.crn)
+    assertThat(fullPerson.nomsNumber).isEqualTo(caseSummary.nomsId)
+    assertThat(fullPerson.status).isEqualTo(PersonStatus.unknown)
+    assertThat(fullPerson.name).isEqualTo("John Smith")
+    assertThat(fullPerson.type).isEqualTo(PersonType.fullPerson)
+  }
+}
