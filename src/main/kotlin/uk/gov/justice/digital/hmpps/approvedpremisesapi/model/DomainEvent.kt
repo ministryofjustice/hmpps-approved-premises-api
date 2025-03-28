3a14f57b31a0fe14dadda42376d1bd3e461cadac
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-03-28 16:35:21 +0000

    Remove unused fields from `DomainEvent` type
    This commit removes fields that are no longer used on `DomainEvent`, given that the type is no longer used by CAS1

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/DomainEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/DomainEvent.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.model
 
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.MetaDataName
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TriggerSourceType
 import java.time.Instant
 import java.util.UUID
 
@@ -10,14 +9,10 @@ data class DomainEvent<T>(
   val applicationId: UUID? = null,
   val assessmentId: UUID? = null,
   val bookingId: UUID? = null,
-  val cas1SpaceBookingId: UUID? = null,
-  val cas1PlacementRequestId: UUID? = null,
   val crn: String,
   val nomsNumber: String?,
   val occurredAt: Instant,
   val data: T,
   val metadata: Map<MetaDataName, String?> = emptyMap(),
   val schemaVersion: Int? = null,
-  val triggerSource: TriggerSourceType? = null,
-  val emit: Boolean = true,
 )
