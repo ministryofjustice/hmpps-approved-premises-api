14a3ca1b581e99b8360f073bed54a92359d61b92
Muhammad El Abdulla <>
2025-04-02 15:14:32 +0100

    Split GET /applications api endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas3/Cas3ApplicationsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas3/Cas3ApplicationsController.kt
@@ -3,14 +3,57 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller.cas3
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas3.ApplicationsCas3Delegate
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas3ApplicationSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApplicationSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.ApplicationService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.LaoStrategy
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.LaoStrategy.CheckUserAccess
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.OffenderService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3.Cas3ApplicationService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas3.Cas3ApplicationTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.extractEntityFromCasResult
 import java.util.UUID
 
 @Service
 class Cas3ApplicationsController(
   private val cas3ApplicationService: Cas3ApplicationService,
+  private val applicationService: ApplicationService,
+  private val userService: UserService,
+  private val offenderService: OffenderService,
+  private val cas3ApplicationTransformer: Cas3ApplicationTransformer,
 ) : ApplicationsCas3Delegate {
+
+  override fun getApplicationsForUser(): ResponseEntity<List<Cas3ApplicationSummary>> {
+    val user = userService.getUserForRequest()
+
+    val applications = applicationService.getAllApplicationsForUsername(user, ServiceName.temporaryAccommodation)
+
+    return ResponseEntity.ok(
+      getPersonDetailAndTransformToSummary(
+        applications = applications,
+        laoStrategy = CheckUserAccess(user.deliusUsername),
+      ),
+    )
+  }
+
+  private fun getPersonDetailAndTransformToSummary(
+    applications: List<ApplicationSummary>,
+    laoStrategy: LaoStrategy,
+  ): List<Cas3ApplicationSummary> {
+    val crns = applications.map { it.getCrn() }
+    val personInfoResults = offenderService.getPersonInfoResults(crns.toSet(), laoStrategy)
+
+    return applications.map {
+      val crn = it.getCrn()
+      cas3ApplicationTransformer.transformDomainToCas3ApplicationSummary(
+        it,
+        personInfoResults.firstOrNull { it.crn == crn } ?: PersonInfoResult.Unknown(crn),
+      )
+    }
+  }
 
   override fun deleteApplication(applicationId: UUID): ResponseEntity<Unit> = ResponseEntity.ok(
     extractEntityFromCasResult(cas3ApplicationService.markApplicationAsDeleted(applicationId)),
--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas3/Cas3ApplicationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/cas3/Cas3ApplicationTest.kt
@@ -4,16 +4,147 @@ import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas3ApplicationSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TemporaryAccommodationApplicationSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.CaseAccessFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.InitialiseDatabasePerClassTestBase
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAProbationRegion
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAUser
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAnOffender
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.httpmocks.apDeliusContextAddResponseToUserAccessCall
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainEventType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ProbationRegionEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TemporaryAccommodationApplicationEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TemporaryAccommodationApplicationJsonSchemaEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRole
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.bodyAsListOfObjects
 import java.time.LocalDate
+import java.time.OffsetDateTime
 import java.util.UUID
 
 class Cas3ApplicationTest : InitialiseDatabasePerClassTestBase() {
+
+  @Nested
+  inner class GetApplications {
+    @ParameterizedTest
+    @CsvSource("/applications", "/cas3/applications")
+    fun `Get all applications without JWT returns 401`(baseUrl: String) {
+      webTestClient.get()
+        .uri(baseUrl)
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+      "CAS3_REFERRER,/applications",
+      "CAS3_ASSESSOR,/applications",
+      "CAS3_REFERRER,/cas3/applications",
+      "CAS3_ASSESSOR,/cas3/applications",
+    )
+    fun `Get all applications returns 200 for TA - returns all applications for user`(userRole: UserRole, baseUrl: String) {
+      givenAProbationRegion { probationRegion ->
+        givenAUser(roles = listOf(userRole), probationRegion = probationRegion) { otherUser, _ ->
+          givenAUser(
+            roles = listOf(UserRole.CAS3_REFERRER),
+            probationRegion = probationRegion,
+          ) { referrerUser, jwt ->
+            givenAnOffender { offenderDetails, _ ->
+              temporaryAccommodationApplicationJsonSchemaRepository.deleteAll()
+
+              val applicationSchema = temporaryAccommodationApplicationJsonSchemaEntityFactory.produceAndPersist {
+                withAddedAt(OffsetDateTime.now())
+                withId(UUID.randomUUID())
+              }
+
+              val application =
+                createApplicationEntity(applicationSchema, referrerUser, offenderDetails, probationRegion, null)
+
+              val anotherUsersApplication =
+                createApplicationEntity(applicationSchema, otherUser, offenderDetails, probationRegion, null)
+
+              apDeliusContextAddResponseToUserAccessCall(
+                listOf(
+                  CaseAccessFactory()
+                    .withCrn(offenderDetails.otherIds.crn)
+                    .produce(),
+                ),
+                referrerUser.deliusUsername,
+              )
+
+              when (baseUrl) {
+                "/applications" -> {
+                  val responseBody = webTestClient.get()
+                    .uri(baseUrl)
+                    .header("Authorization", "Bearer $jwt")
+                    .header("X-Service-Name", ServiceName.temporaryAccommodation.value)
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .bodyAsListOfObjects<TemporaryAccommodationApplicationSummary>()
+
+                  assertThat(responseBody).anyMatch {
+                    application.id == it.id &&
+                      application.crn == it.person.crn &&
+                      application.createdAt.toInstant() == it.createdAt &&
+                      application.createdByUser.id == it.createdByUserId &&
+                      application.submittedAt?.toInstant() == it.submittedAt
+                  }
+
+                  assertThat(responseBody).noneMatch {
+                    anotherUsersApplication.id == it.id
+                  }
+                }
+
+                else -> {
+                  val responseBody = webTestClient.get()
+                    .uri(baseUrl)
+                    .header("Authorization", "Bearer $jwt")
+                    .exchange()
+                    .expectStatus()
+                    .isOk
+                    .bodyAsListOfObjects<Cas3ApplicationSummary>()
+
+                  assertThat(responseBody).anyMatch {
+                    application.id == it.id &&
+                      application.crn == it.person.crn &&
+                      application.createdAt.toInstant() == it.createdAt &&
+                      application.createdByUser.id == it.createdByUserId &&
+                      application.submittedAt?.toInstant() == it.submittedAt
+                  }
+
+                  assertThat(responseBody).noneMatch {
+                    anotherUsersApplication.id == it.id
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    private fun createApplicationEntity(
+      applicationSchema: TemporaryAccommodationApplicationJsonSchemaEntity,
+      user: UserEntity,
+      offenderDetails: OffenderDetailSummary,
+      probationRegion: ProbationRegionEntity,
+      submittedAt: OffsetDateTime?,
+    ): TemporaryAccommodationApplicationEntity = temporaryAccommodationApplicationEntityFactory.produceAndPersist {
+      withApplicationSchema(applicationSchema)
+      withCreatedByUser(user)
+      withSubmittedAt(submittedAt)
+      withCrn(offenderDetails.otherIds.crn)
+      withData("{}")
+      withProbationRegion(probationRegion)
+    }
+  }
 
   @Nested
   inner class SoftDelete {
