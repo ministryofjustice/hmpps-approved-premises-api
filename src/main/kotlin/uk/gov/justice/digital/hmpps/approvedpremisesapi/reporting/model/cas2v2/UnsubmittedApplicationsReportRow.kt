7659ac635ef0674246459532787ac609416c3295
garethCAS2 <gareth.hallberg1@digital.justice.gov.uk>
2025-04-14 10:07:29 +0100

    made persosNoms a nullable field in UnsubmittedApplicationsReportRow

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/model/cas2v2/UnsubmittedApplicationsReportRow.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/reporting/model/cas2v2/UnsubmittedApplicationsReportRow.kt
@@ -5,7 +5,7 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApplicationOri
 class UnsubmittedApplicationsReportRow(
   val applicationId: String,
   val personCrn: String,
-  val personNoms: String,
+  val personNoms: String?,
   val startedAt: String,
   val startedBy: String,
   val applicationOrigin: ApplicationOrigin,
