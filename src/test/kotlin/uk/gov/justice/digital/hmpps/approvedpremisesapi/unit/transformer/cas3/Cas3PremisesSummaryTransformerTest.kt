e3bd9a9bff68d4936e815500a7c7e984775de70c
Muhammad El Abdulla <>
2025-04-24 14:18:26 +0100

    Remove /premises/summary api endpoint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas3/Cas3PremisesSummaryTransformerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/unit/transformer/cas3/Cas3PremisesSummaryTransformerTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas3PremisesSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PropertyStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TemporaryAccommodationPremisesSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas3.Cas3PremisesSummaryTransformer
 import java.util.UUID
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TemporaryAccommodationPremisesSummary as DomainTemporaryAccommodationPremisesSummary
@@ -13,40 +12,7 @@ class Cas3PremisesSummaryTransformerTest {
   private val cas3PremisesSummaryTransformer = Cas3PremisesSummaryTransformer()
 
   @Test
-  fun `transformDomainToApi transforms the DomainTemporaryAccommodationPremisesSummary into a TemporaryAccommodationPremisesSummary`() {
-    val uuid = UUID.randomUUID()
-    val domainPremisesSummary = TemporaryAccommodationPremisesSummaryData(
-      id = uuid,
-      name = "bob",
-      addressLine1 = "address",
-      addressLine2 = "address line 2",
-      postcode = "123ABC",
-      pdu = "North east",
-      status = PropertyStatus.active,
-      bedCount = 1,
-      localAuthorityAreaName = "Rochford",
-    )
-
-    val result = cas3PremisesSummaryTransformer.transformDomainToApi(domainPremisesSummary)
-
-    assertThat(result).isEqualTo(
-      TemporaryAccommodationPremisesSummary(
-        id = uuid,
-        name = "bob",
-        addressLine1 = "address",
-        addressLine2 = "address line 2",
-        postcode = "123ABC",
-        pdu = "North east",
-        status = PropertyStatus.active,
-        bedCount = 1,
-        service = "CAS3",
-        localAuthorityAreaName = "Rochford",
-      ),
-    )
-  }
-
-  @Test
-  fun `transformDomainToApi transforms the DomainTemporaryAccommodationPremisesSummary into a Cas3PremisesSummary`() {
+  fun `transformDomainToCas3PremisesSummary transforms the DomainTemporaryAccommodationPremisesSummary into a Cas3PremisesSummary`() {
     val uuid = UUID.randomUUID()
     val domainPremisesSummary = TemporaryAccommodationPremisesSummaryData(
       id = uuid,
@@ -78,40 +44,7 @@ class Cas3PremisesSummaryTransformerTest {
   }
 
   @Test
-  fun `transformDomainToApi transforms the DomainTemporaryAccommodationPremisesSummary into a TemporaryAccommodationPremisesSummary without optional elements`() {
-    val uuid = UUID.randomUUID()
-    val domainPremisesSummary = TemporaryAccommodationPremisesSummaryData(
-      id = uuid,
-      name = "bob",
-      addressLine1 = "address",
-      addressLine2 = null,
-      postcode = "123ABC",
-      pdu = "North east",
-      status = PropertyStatus.active,
-      bedCount = 1,
-      localAuthorityAreaName = null,
-    )
-
-    val result = cas3PremisesSummaryTransformer.transformDomainToApi(domainPremisesSummary)
-
-    assertThat(result).isEqualTo(
-      TemporaryAccommodationPremisesSummary(
-        id = uuid,
-        name = "bob",
-        addressLine1 = "address",
-        addressLine2 = null,
-        postcode = "123ABC",
-        pdu = "North east",
-        status = PropertyStatus.active,
-        bedCount = 1,
-        service = "CAS3",
-        localAuthorityAreaName = null,
-      ),
-    )
-  }
-
-  @Test
-  fun `transformDomainToApi transforms the DomainTemporaryAccommodationPremisesSummary into a Cas3PremisesSummary without optional elements`() {
+  fun `transformDomainToCas3PremisesSummary transforms the DomainTemporaryAccommodationPremisesSummary into a Cas3PremisesSummary without optional elements`() {
     val uuid = UUID.randomUUID()
     val domainPremisesSummary = TemporaryAccommodationPremisesSummaryData(
       id = uuid,
