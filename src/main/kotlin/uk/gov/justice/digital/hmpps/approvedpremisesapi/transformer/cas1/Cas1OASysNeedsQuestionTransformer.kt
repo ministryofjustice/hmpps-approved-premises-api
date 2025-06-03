57beb3b3438b0f9f90b3d9599ba2c08ea2272fcf
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-06-03 08:49:02 +0100

    Refactor CAS1 OAsys Transfer for reuse
    Moving the ‘needs’ question logic into an internal model to which we can add additional fields for other transformations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/cas1/Cas1OASysNeedsQuestionTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/cas1/Cas1OASysNeedsQuestionTransformer.kt
@@ -8,52 +8,70 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.NeedsDetails
 @Service
 class Cas1OASysNeedsQuestionTransformer {
 
-  fun transformToApi(needsDetails: NeedsDetails) = listOf(
+  fun transformToApi(needsDetails: NeedsDetails) = toQuestionState(needsDetails).map {
     Cas1OASysNeedsQuestion(
+      section = it.section,
+      name = it.sectionLabel,
+      optional = it.optional,
+      linkedToHarm = it.linkedToHarm,
+      linkedToReOffending = it.linkedToReOffending,
+    )
+  }
+
+  private data class QuestionState(
+    val section: Int,
+    val sectionLabel: String,
+    val optional: Boolean,
+    val linkedToHarm: Boolean?,
+    val linkedToReOffending: Boolean?,
+  )
+
+  private fun toQuestionState(needsDetails: NeedsDetails) = listOf(
+    QuestionState(
       section = 3,
-      name = sectionToName[3]!!,
+      sectionLabel = sectionToName[3]!!,
       optional = isOptional(needsDetails.linksToHarm?.accommodationLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.accommodationLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.accommodationLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 6,
-      name = sectionToName[6]!!,
+      sectionLabel = sectionToName[6]!!,
       optional = isOptional(needsDetails.linksToHarm?.relationshipLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.relationshipLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.relationshipLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 7,
-      name = sectionToName[7]!!,
+      sectionLabel = sectionToName[7]!!,
       optional = isOptional(needsDetails.linksToHarm?.lifestyleLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.lifestyleLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.lifestyleLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 10,
-      name = sectionToName[10]!!,
+      sectionLabel = sectionToName[10]!!,
       optional = isOptional(needsDetails.linksToHarm?.emotionalLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.emotionalLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.emotionalLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 11,
-      name = sectionToName[11]!!,
+      sectionLabel = sectionToName[11]!!,
       optional = isOptional(needsDetails.linksToHarm?.thinkingBehaviouralLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.thinkingBehaviouralLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.thinkingBehaviouralLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 12,
-      name = sectionToName[12]!!,
+      sectionLabel = sectionToName[12]!!,
       optional = isOptional(needsDetails.linksToHarm?.attitudeLinkedToHarm),
       linkedToHarm = needsDetails.linksToHarm?.attitudeLinkedToHarm,
       linkedToReOffending = needsDetails.linksToReOffending?.attitudeLinkedToReOffending,
     ),
-    Cas1OASysNeedsQuestion(
+    QuestionState(
       section = 13,
-      name = sectionToName[13]!!,
+      sectionLabel = sectionToName[13]!!,
       optional = true,
       linkedToHarm = null,
       linkedToReOffending = null,
