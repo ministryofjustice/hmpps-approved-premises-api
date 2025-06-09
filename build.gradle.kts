bd4bb484d0a03f3b96ace3ae7eca17a611caec88
daveawc <168838504+daveawc@users.noreply.github.com>
2025-06-09 09:46:28 +0100

    update hmpps-kotlin-spring-boot-starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ configurations.matching { it.name == "detekt" }.all {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.2.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.5")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
