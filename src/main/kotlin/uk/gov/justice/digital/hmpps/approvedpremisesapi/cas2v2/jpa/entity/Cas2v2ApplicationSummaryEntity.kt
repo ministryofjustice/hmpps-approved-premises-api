77740257dfbd7583bdf45cbac737ae85fdebe538
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-06-26 16:11:15 +0100

    feat: ensures that only prion bail applicatioins are returned when requested from a prison source

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/jpa/entity/Cas2v2ApplicationSummaryEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/jpa/entity/Cas2v2ApplicationSummaryEntity.kt
@@ -7,13 +7,17 @@ import jakarta.persistence.Table
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 
 @Repository
-interface Cas2v2ApplicationSummaryRepository : JpaRepository<Cas2v2ApplicationSummaryEntity, String> {
+interface Cas2v2ApplicationSummaryRepository :
+  JpaRepository<Cas2v2ApplicationSummaryEntity, String>,
+  JpaSpecificationExecutor<Cas2v2ApplicationSummaryEntity> {
+
   fun findByUserId(userId: String, pageable: Pageable?): Page<Cas2v2ApplicationSummaryEntity>
 
   fun findByUserIdAndSubmittedAtIsNotNull(userId: String, pageable: Pageable?): Page<Cas2v2ApplicationSummaryEntity>
