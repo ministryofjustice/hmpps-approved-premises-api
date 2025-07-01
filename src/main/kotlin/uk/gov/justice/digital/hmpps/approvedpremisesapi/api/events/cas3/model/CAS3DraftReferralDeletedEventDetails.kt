37b842959acd4fbbe777fbf12b8bfe7fbb7da9d5
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-01 16:49:02 +0100

    commiting cas3 domain event models

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/events/cas3/model/CAS3DraftReferralDeletedEventDetails.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas3.model
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * 
+ * @param applicationId 
+ * @param crn 
+ * @param deletedBy 
+ */
+data class CAS3DraftReferralDeletedEventDetails(
+
+    @Schema(example = "null", required = true, description = "")
+    @get:JsonProperty("applicationId", required = true) val applicationId: java.util.UUID,
+
+    @Schema(example = "null", required = true, description = "")
+    @get:JsonProperty("crn", required = true) val crn: kotlin.String,
+
+    @Schema(example = "null", required = true, description = "")
+    @get:JsonProperty("deletedBy", required = true) val deletedBy: java.util.UUID
+    ) {
+
+}
+
