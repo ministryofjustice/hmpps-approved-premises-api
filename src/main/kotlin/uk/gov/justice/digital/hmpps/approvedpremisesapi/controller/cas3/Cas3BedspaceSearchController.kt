74182357b7ef81382ff74fd524c64bc9a5994869
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-02 12:57:31 +0100

    Cas3BedspaceSearchController Cas3ApplicationsController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas3/Cas3BedspaceSearchController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas3/Cas3BedspaceSearchController.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.controller.cas3
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas3.BedspacesCas3Delegate
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas3BedspaceSearchParameters
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas3BedspaceSearchResults
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserService
@@ -10,13 +13,22 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas3.Cas3Bedspac
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas3.Cas3BedspaceSearchResultsTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.extractEntityFromCasResult
 
-@Service
+@RestController
+@RequestMapping(
+  "\${api.base-path:}/cas3}",
+  produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE],
+)
 class Cas3BedspaceSearchController(
   private val userService: UserService,
   private val cas3BedspaceSearchService: Cas3BedspaceSearchService,
   private val cas3BedspaceSearchResultsTransformer: Cas3BedspaceSearchResultsTransformer,
-) : BedspacesCas3Delegate {
-  override fun postBedspaceSearch(cas3BedspaceSearchParameters: Cas3BedspaceSearchParameters): ResponseEntity<Cas3BedspaceSearchResults> {
+) {
+
+  @PostMapping(
+    "/bedspaces/search",
+    consumes = [MediaType.APPLICATION_JSON_VALUE],
+  )
+  fun postBedspaceSearch(@RequestBody cas3BedspaceSearchParameters: Cas3BedspaceSearchParameters): ResponseEntity<Cas3BedspaceSearchResults> {
     val user = userService.getUserForRequest()
 
     val searchResult = cas3BedspaceSearchService.findBedspaces(
