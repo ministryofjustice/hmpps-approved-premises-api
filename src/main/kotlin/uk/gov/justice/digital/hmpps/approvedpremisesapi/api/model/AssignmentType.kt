e4d7eb8e0d846167765825f5fd9f9ca2512ec59f
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-03 22:43:45 +0100

    moving models in to codebase

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/AssignmentType.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model
+
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.annotation.JsonCreator
+
+/**
+* 
+* Values: ALLOCATED,DEALLOCATED,IN_PROGRESS,PRISON,UNALLOCATED
+*/
+enum class AssignmentType(@get:JsonValue val value: String) {
+
+    ALLOCATED("ALLOCATED"),
+    DEALLOCATED("DEALLOCATED"),
+    IN_PROGRESS("IN_PROGRESS"),
+    PRISON("PRISON"),
+    UNALLOCATED("UNALLOCATED");
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun forValue(value: String): AssignmentType {
+                return values().first{it -> it.value == value}
+        }
+    }
+}
+
