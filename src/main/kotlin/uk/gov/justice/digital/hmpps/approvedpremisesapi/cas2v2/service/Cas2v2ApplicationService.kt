11c72a655a4c8ab273be65035f19864911d8b8c3
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-07-04 11:25:15 +0100

    feat: improved the seed data for bail

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/service/Cas2v2ApplicationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/service/Cas2v2ApplicationService.kt
@@ -40,8 +40,6 @@ import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.util.UUID
-import org.springframework.data.jpa.domain.Specification
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.jpa.entity.Cas2v2ApplicationSummarySpecifications
 
 @Service("Cas2v2ApplicationService")
 class Cas2v2ApplicationService(
