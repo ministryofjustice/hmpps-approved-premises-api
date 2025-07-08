677f9be485ee6900e04a2e49fba600a872a5a89a
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-08 10:12:18 +0100

    moving annotations and removing generated files for Cas2SubmissionsController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/controller/Cas2SubmissionsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/controller/Cas2SubmissionsController.kt
@@ -1,10 +1,16 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.controller
 
 import jakarta.transaction.Transactional
+import java.util.UUID
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2.SubmissionsCas2Delegate
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2SubmittedApplication
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2SubmittedApplicationSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.SortDirection
@@ -19,9 +25,12 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.transformer.Submiss
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.HttpAuthService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.PageCriteria
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.extractEntityFromCasResult
-import java.util.UUID
 
-@Service("Cas2SubmissionsController")
+@RestController
+@RequestMapping(
+  "\${openapi.communityAccommodationServicesTier2CAS2.base-path:/cas2}",
+  produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE],
+)
 class Cas2SubmissionsController(
   private val httpAuthService: HttpAuthService,
   private val applicationService: Cas2ApplicationService,
@@ -29,9 +38,13 @@ class Cas2SubmissionsController(
   private val offenderService: Cas2OffenderService,
   private val externalUserService: ExternalUserService,
   private val cas2UserService: Cas2UserService,
-) : SubmissionsCas2Delegate {
+) {
 
-  override fun submissionsGet(page: Int?): ResponseEntity<List<Cas2SubmittedApplicationSummary>> {
+  @RequestMapping(
+    method = [RequestMethod.GET],
+    value = ["/submissions"],
+  )
+  fun submissionsGet(@RequestParam page: Int?): ResponseEntity<List<Cas2SubmittedApplicationSummary>> {
     val principal = httpAuthService.getCas2AuthenticatedPrincipalOrThrow()
     if (principal.isExternalUser()) {
       ensureExternalUserPersisted()
@@ -49,7 +62,11 @@ class Cas2SubmissionsController(
     ).body(getPersonNamesAndTransformToSummaries(applications))
   }
 
-  override fun submissionsApplicationIdGet(applicationId: UUID): ResponseEntity<Cas2SubmittedApplication> {
+  @RequestMapping(
+    method = [RequestMethod.GET],
+    value = ["/submissions/{applicationId}"],
+  )
+  fun submissionsApplicationIdGet(@PathVariable applicationId: UUID): ResponseEntity<Cas2SubmittedApplication> {
     val principal = httpAuthService.getCas2AuthenticatedPrincipalOrThrow()
     if (principal.isExternalUser()) {
       ensureExternalUserPersisted()
@@ -61,10 +78,12 @@ class Cas2SubmissionsController(
     return ResponseEntity.ok(getPersonDetailAndTransform(application))
   }
 
+  @RequestMapping(
+    method = [RequestMethod.POST],
+    value = ["/submissions"],
+  )
   @Transactional
-  override fun submissionsPost(
-    submitCas2Application: SubmitCas2Application,
-  ): ResponseEntity<Unit> {
+  fun submissionsPost(@RequestBody submitCas2Application: SubmitCas2Application): ResponseEntity<Unit> {
     val user = cas2UserService.getUserForRequest()
     val submitResult = applicationService.submitApplication(submitCas2Application, user)
 
