30b950e5cc1b9ec72cb2a0d5fcaf8c0738955a2f
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-08 10:24:23 +0100

    moving annotations and removing generated files for Cas2ReferenceDataController

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/controller/Cas2ReferenceDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/controller/Cas2ReferenceDataController.kt
@@ -1,19 +1,29 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.controller
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2.ReferenceDataCas2Delegate
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas2ApplicationStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.reporting.model.reference.Cas2PersistedApplicationStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.reporting.model.reference.Cas2PersistedApplicationStatusFinder
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.transformer.ApplicationStatusTransformer
 
-@Service("Cas2ReferenceDataController")
+@RestController
+@RequestMapping(
+  "\${openapi.communityAccommodationServicesTier2CAS2.base-path:/cas2}",
+  produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE],
+)
 class Cas2ReferenceDataController(
   private val statusTransformer: ApplicationStatusTransformer,
   private val statusFinder: Cas2PersistedApplicationStatusFinder,
-) : ReferenceDataCas2Delegate {
-  override fun referenceDataApplicationStatusGet(): ResponseEntity<List<Cas2ApplicationStatus>> = ResponseEntity.ok(transformToApi(statusFinder.active()))
+) {
+  @RequestMapping(
+    method = [RequestMethod.GET],
+    value = ["/reference-data/application-status"],
+  )
+  fun referenceDataApplicationStatusGet(): ResponseEntity<List<Cas2ApplicationStatus>> = ResponseEntity.ok(transformToApi(statusFinder.active()))
 
   private fun transformToApi(statusList: List<Cas2PersistedApplicationStatus>): List<Cas2ApplicationStatus> = statusList.map { status -> statusTransformer.transformModelToApi(status) }
 }
