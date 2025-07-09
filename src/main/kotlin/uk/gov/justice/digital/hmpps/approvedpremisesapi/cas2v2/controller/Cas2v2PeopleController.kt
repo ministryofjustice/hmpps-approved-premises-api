22b628d03f8c42b83fbf757a18c80d264e4d0b57
Toby Batch <toby.batch@digital.justice.gov.uk>
2025-07-09 16:52:55 +0100

    fix: updates cas2v2 controllers to use config in code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/controller/Cas2v2PeopleController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/controller/Cas2v2PeopleController.kt
@@ -1,21 +1,31 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.controller
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas2v2.PeopleCas2v2Delegate
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Person
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.service.Cas2v2OffenderSearchResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.service.Cas2v2OffenderService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.BadRequestProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.ForbiddenProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.NotFoundProblem
-
-@Service("Cas2v2PeopleController")
+@RestController
+@RequestMapping(
+  "\${openapi.communityAccommodationServicesTier2CAS2Version2.base-path:/cas2v2}",
+  produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE],
+)
 class Cas2v2PeopleController(
   private val cas2v2OffenderService: Cas2v2OffenderService,
-) : PeopleCas2v2Delegate {
+) {
 
-  override fun searchByCrnGet(crn: String): ResponseEntity<Person> {
+  @Suppress("ThrowsCount")
+  @GetMapping("/people/search-by-crn/{crn}")
+  fun searchByCrnGet(
+    @PathVariable crn: String,
+  ): ResponseEntity<Person> {
     when (val cas2v2OffenderSearchResult = cas2v2OffenderService.getPersonByNomisIdOrCrn(crn)) {
       is Cas2v2OffenderSearchResult.NotFound -> throw NotFoundProblem(crn, "Offender", "a CRN")
       is Cas2v2OffenderSearchResult.Unknown -> throw cas2v2OffenderSearchResult.throwable ?: BadRequestProblem(errorDetail = "Could not retrieve person info for CRN: $crn")
@@ -24,7 +34,11 @@ class Cas2v2PeopleController(
     }
   }
 
-  override fun searchByNomisIdGet(nomsNumber: String): ResponseEntity<Person> {
+  @Suppress("ThrowsCount")
+  @GetMapping("/people/search-by-noms/{nomsNumber}")
+  fun searchByNomisIdGet(
+    @PathVariable nomsNumber: String,
+  ): ResponseEntity<Person> {
     when (val cas2v2OffenderSearchResult = cas2v2OffenderService.getPersonByNomisIdOrCrn(nomsNumber)) {
       is Cas2v2OffenderSearchResult.NotFound -> throw NotFoundProblem(nomsNumber, "Offender", "a nomsNumber (prison number)")
       is Cas2v2OffenderSearchResult.Forbidden -> throw ForbiddenProblem()
