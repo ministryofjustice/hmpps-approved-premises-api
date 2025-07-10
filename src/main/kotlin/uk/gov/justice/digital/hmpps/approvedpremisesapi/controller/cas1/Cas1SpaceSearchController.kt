38d0daca293981bd12c6b4fcc021478c9db17ae9
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-07-10 11:14:51 +0100

    Make gender optional for premises search
    This commit makes gender optional on the Cas1SpaceSearch, in preparation for reusing the search for national occupancy results, which will filter on cru management areas instead of gender.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas1/Cas1SpaceSearchController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/controller/cas1/Cas1SpaceSearchController.kt
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas1.SpaceSearchesCas1Delegate
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1SpaceSearchParameters
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Cas1SpaceSearchResults
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesGender
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserPermission
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.BadRequestProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.UserAccessService
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas1.Cas1ApplicationService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.cas1.Cas1PremisesSearchService
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.cas1.Cas1SpaceSearchResultsTransformer
 
@@ -15,13 +18,22 @@ class Cas1SpaceSearchController(
   private val spaceSearchService: Cas1PremisesSearchService,
   private val spaceSearchResultTransformer: Cas1SpaceSearchResultsTransformer,
   private val userAccessService: UserAccessService,
+  private val cas1ApplicationService: Cas1ApplicationService,
 ) : SpaceSearchesCas1Delegate {
   override fun spaceSearch(cas1SpaceSearchParameters: Cas1SpaceSearchParameters): ResponseEntity<Cas1SpaceSearchResults> {
     userAccessService.ensureCurrentUserHasPermission(UserPermission.CAS1_SPACE_BOOKING_CREATE)
 
+    val applicationId = cas1SpaceSearchParameters.applicationId
+    val application = cas1ApplicationService.getApplication(applicationId)
+      ?: throw BadRequestProblem(errorDetail = "Cannot find application with ID $applicationId")
+
     val results = spaceSearchService.findPremises(
       Cas1PremisesSearchService.Cas1PremisesSearchCriteria(
-        cas1SpaceSearchParameters.applicationId,
+        gender = if (application.isWomensApplication == true) {
+          ApprovedPremisesGender.WOMAN
+        } else {
+          ApprovedPremisesGender.MAN
+        },
         cas1SpaceSearchParameters.targetPostcodeDistrict,
         cas1SpaceSearchParameters.spaceCharacteristics ?: emptyList(),
       ),
