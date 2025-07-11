ec235ee6d932be7819e5c6a3abfa9532e72395da
daveawc <168838504+daveawc@users.noreply.github.com>
2025-07-11 11:41:53 +0100

    remove openapi annotations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/AssessmentsApiController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/AssessmentsApiController.kt
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import java.util.Optional
 
-@jakarta.annotation.Generated(
-  value = ["org.openapitools.codegen.languages.KotlinSpringServerCodegen"],
-  comments = "Generator version: 7.13.0",
-)
 @Controller
 @RequestMapping("\${openapi.approvedPremises.base-path:}")
 class AssessmentsApiController(
