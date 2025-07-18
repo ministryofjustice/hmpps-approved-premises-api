4cfc846e13be387d7792fe266e3cfc0fc5f2bf2c
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-07-18 09:54:39 +0100

    Remove `BedMovesEntity`
    This was only used for CAS1 bookings, which have been migrated to space bookings:
    
    ```
    select count(*) from bed_moves;
    -- success --
    count
    -----
        0
    ```
    
    A subsequent commit will remove the table

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/cas1/Cas1BookingToSpaceBookingSeedJob.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/seed/cas1/Cas1BookingToSpaceBookingSeedJob.kt
@@ -9,7 +9,6 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.events.cas1.model.Ca
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesApplicationEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ApprovedPremisesRepository
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BedMoveRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.Cas1SpaceBookingEntity
@@ -31,7 +30,6 @@ class Cas1BookingToSpaceBookingSeedJob(
   private val approvedPremisesRepository: ApprovedPremisesRepository,
   private val spaceBookingRepository: Cas1SpaceBookingRepository,
   private val bookingRepository: BookingRepository,
-  private val bedMoveRepository: BedMoveRepository,
   private val domainEventRepository: DomainEventRepository,
   private val domainEventService: Cas1DomainEventService,
   private val userRepository: UserRepository,
@@ -144,7 +142,6 @@ class Cas1BookingToSpaceBookingSeedJob(
       placementRequestRepository.save(it)
     }
 
-    bedMoveRepository.deleteByBooking(booking)
     bookingRepository.delete(booking)
 
     log.info("Have migrated booking $bookingId to space booking")
