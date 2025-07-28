b84e09398148be6b34894597d87576e6dda777ab
Daniel Humphreys <daniel.humphreys@digital.justice.gov.uk>
2025-07-28 10:43:36 +0100

    Remove CAS 1 allocate tests since these are covered off in CAS1TasksTest.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/TasksTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/TasksTest.kt
@@ -5,17 +5,12 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ApprovedPremisesUser
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NewReallocation
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Reallocation
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.TaskType
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.config.Cas1NotifyTemplates
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.CaseSummaryFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.factory.NameFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.InitialiseDatabasePerClassTestBase
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.IntegrationTestBase
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAPlacementApplication
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAUser
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAnAssessmentForApprovedPremises
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.integration.givens.givenAnAssessmentForTemporaryAccommodation
@@ -25,8 +20,6 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.UserRole
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonSummaryInfoResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.community.OffenderDetailSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.UserTransformer
-import java.time.LocalDate
-import java.time.OffsetDateTime
 import java.util.UUID
 
 class TasksTest {
@@ -53,175 +46,6 @@ class TasksTest {
         .exchange()
         .expectStatus()
         .isUnauthorized
-    }
-
-    @Test
-    fun `Reallocate application to different assessor without CAS1_CRU_MEMBER role returns 403`() {
-      givenAUser { _, jwt ->
-        webTestClient.post()
-          .uri("/tasks/assessment/9c7abdf6-fd39-4670-9704-98a5bbfec95e/allocations")
-          .header("Authorization", "Bearer $jwt")
-          .header("X-Service-Name", ServiceName.approvedPremises.value)
-          .bodyValue(
-            NewReallocation(
-              userId = UUID.randomUUID(),
-            ),
-          )
-          .exchange()
-          .expectStatus()
-          .isForbidden
-      }
-    }
-
-    @Test
-    fun `Reallocate assessment to different assessor returns 201, creates new assessment, deallocates old one, sends emails`() {
-      givenAUser(roles = listOf(UserRole.CAS1_CRU_MEMBER)) { _, jwt ->
-        givenAUser(roles = listOf(UserRole.CAS1_ASSESSOR)) { currentlyAllocatedUser, _ ->
-          givenAUser(
-            roles = listOf(UserRole.CAS1_ASSESSOR),
-          ) { assigneeUser, _ ->
-            givenAnOffender { offenderDetails, _ ->
-              givenAnAssessmentForApprovedPremises(
-                allocatedToUser = currentlyAllocatedUser,
-                createdByUser = currentlyAllocatedUser,
-                crn = offenderDetails.otherIds.crn,
-              ) { existingAssessment, application ->
-
-                webTestClient.post()
-                  .uri("/tasks/assessment/${existingAssessment.id}/allocations")
-                  .header("Authorization", "Bearer $jwt")
-                  .header("X-Service-Name", ServiceName.approvedPremises.value)
-                  .bodyValue(
-                    NewReallocation(
-                      userId = assigneeUser.id,
-                    ),
-                  )
-                  .exchange()
-                  .expectStatus()
-                  .isCreated
-                  .expectBody()
-                  .json(
-                    objectMapper.writeValueAsString(
-                      Reallocation(
-                        user = userTransformer.transformJpaToApi(
-                          assigneeUser,
-                          ServiceName.approvedPremises,
-                        ) as ApprovedPremisesUser,
-                        taskType = TaskType.assessment,
-                      ),
-                    ),
-                  )
-
-                val assessments = approvedPremisesAssessmentRepository.findAll()
-
-                assertThat(assessments.first { it.id == existingAssessment.id }.reallocatedAt).isNotNull
-                assertThat(assessments)
-                  .anyMatch { it.application.id == application.id && it.allocatedToUser!!.id == assigneeUser.id }
-
-                emailAsserter.assertEmailsRequestedCount(2)
-                emailAsserter.assertEmailRequested(currentlyAllocatedUser.email!!, Cas1NotifyTemplates.ASSESSMENT_DEALLOCATED)
-                emailAsserter.assertEmailRequested(assigneeUser.email!!, Cas1NotifyTemplates.ASSESSMENT_ALLOCATED)
-              }
-            }
-          }
-        }
-      }
-    }
-
-    @Test
-    fun `Reallocate assessment to different assessor returns an error if the assessment has already been allocated`() {
-      givenAUser(roles = listOf(UserRole.CAS1_CRU_MEMBER)) { _, jwt ->
-        givenAUser(roles = listOf(UserRole.CAS1_ASSESSOR)) { user, _ ->
-          givenAUser(
-            roles = listOf(UserRole.CAS1_ASSESSOR),
-          ) { assigneeUser, _ ->
-            givenAnOffender { offenderDetails, _ ->
-              givenAnAssessmentForApprovedPremises(
-                allocatedToUser = user,
-                createdByUser = user,
-                crn = offenderDetails.otherIds.crn,
-                reallocated = true,
-              ) { existingAssessment, application ->
-
-                webTestClient.post()
-                  .uri("/tasks/assessment/${existingAssessment.id}/allocations")
-                  .header("Authorization", "Bearer $jwt")
-                  .header("X-Service-Name", ServiceName.approvedPremises.value)
-                  .bodyValue(
-                    NewReallocation(
-                      userId = assigneeUser.id,
-                    ),
-                  )
-                  .exchange()
-                  .expectStatus()
-                  .is4xxClientError()
-                  .expectBody()
-                  .jsonPath("title").isEqualTo("Conflict")
-                  .jsonPath("status").isEqualTo(409)
-                  .jsonPath("detail")
-                  .isEqualTo("This assessment has already been reallocated: ${existingAssessment.id}")
-              }
-            }
-          }
-        }
-      }
-    }
-
-    @Test
-    fun `Reallocating a placement application to different assessor returns 201, creates new placement application, deallocates old one`() {
-      givenAUser(roles = listOf(UserRole.CAS1_CRU_MEMBER)) { _, jwt ->
-        givenAUser { user, _ ->
-          givenAUser(
-            roles = listOf(UserRole.CAS1_ASSESSOR),
-          ) { assigneeUser, _ ->
-            givenAnOffender { offenderDetails, _ ->
-              givenAPlacementApplication(
-                createdByUser = user,
-                allocatedToUser = user,
-                crn = offenderDetails.otherIds.crn,
-                submittedAt = OffsetDateTime.now(),
-                expectedArrival = LocalDate.of(2012, 1, 1),
-                duration = 15,
-              ) { placementApplication ->
-                webTestClient.post()
-                  .uri("/tasks/placement-application/${placementApplication.id}/allocations")
-                  .header("Authorization", "Bearer $jwt")
-                  .header("X-Service-Name", ServiceName.approvedPremises.value)
-                  .bodyValue(
-                    NewReallocation(
-                      userId = assigneeUser.id,
-                    ),
-                  )
-                  .exchange()
-                  .expectStatus()
-                  .isCreated
-                  .expectBody()
-                  .json(
-                    objectMapper.writeValueAsString(
-                      Reallocation(
-                        user = userTransformer.transformJpaToApi(
-                          assigneeUser,
-                          ServiceName.approvedPremises,
-                        ) as ApprovedPremisesUser,
-                        taskType = TaskType.placementApplication,
-                      ),
-                    ),
-                  )
-
-                val placementApplications = placementApplicationRepository.findAll()
-                val allocatedPlacementApplication =
-                  placementApplications.find { it.allocatedToUser!!.id == assigneeUser.id }
-
-                assertThat(placementApplications.first { it.id == placementApplication.id }.reallocatedAt).isNotNull
-                assertThat(allocatedPlacementApplication).isNotNull
-
-                assertThat(allocatedPlacementApplication!!.expectedArrival).isEqualTo(LocalDate.of(2012, 1, 1))
-                assertThat(allocatedPlacementApplication.duration).isEqualTo(15)
-              }
-            }
-          }
-        }
-      }
     }
 
     @Test
