00554e6122a7c27dc5fac02ea6199e84802d68af
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-07-29 12:57:49 +0100

    Remove probation offender search models
    Use of the probation offender search API client was removed via commit d2b4b7d53784841e28901f9e53d8d31a82c05ede. This commit removes the now unused models related to that API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/model/probationoffendersearchapi/ProbationOffenderSearchNomsRequest.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.approvedpremisesapi.model.probationoffendersearchapi
-
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude
-data class ProbationOffenderSearchNomsRequest(val nomsNumber: String)
