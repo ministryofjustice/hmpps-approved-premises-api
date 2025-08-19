827eb4af4be13ac5f3b93338fd127c4ecdd6c29c
venkatesh bala <venkatesh.bala1@digital.justice.gov.uk>
2025-08-19 10:52:16 +0100

    Add release/sentence type to application.
    - Added release type and sentence type to ApprovedPremisesApplication.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/ApprovedPremisesApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/ApprovedPremisesApplication.kt
@@ -1,56 +1,59 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class ApprovedPremisesApplication(
 
-  @get:JsonProperty("createdByUserId", required = true) val createdByUserId: java.util.UUID,
+  val createdByUserId: java.util.UUID,
 
-  @get:JsonProperty("status", required = true) val status: ApprovedPremisesApplicationStatus,
+  val status: ApprovedPremisesApplicationStatus,
 
-  @get:JsonProperty("type", required = true) override val type: kotlin.String,
+  val isWomensApplication: kotlin.Boolean? = null,
 
-  @get:JsonProperty("id", required = true) override val id: java.util.UUID,
+  val isEmergencyApplication: kotlin.Boolean? = null,
 
-  @get:JsonProperty("person", required = true) override val person: Person,
+  val apType: ApType? = null,
 
-  @get:JsonProperty("createdAt", required = true) override val createdAt: java.time.Instant,
+  val arrivalDate: java.time.Instant? = null,
 
-  @get:JsonProperty("isWomensApplication") val isWomensApplication: kotlin.Boolean? = null,
+  val risks: PersonRisks? = null,
 
-  @get:JsonProperty("isEmergencyApplication") val isEmergencyApplication: kotlin.Boolean? = null,
+  val `data`: kotlin.Any? = null,
 
-  @get:JsonProperty("apType") val apType: ApType? = null,
+  val document: kotlin.Any? = null,
 
-  @get:JsonProperty("arrivalDate") val arrivalDate: java.time.Instant? = null,
+  val assessmentId: java.util.UUID? = null,
 
-  @get:JsonProperty("risks") val risks: PersonRisks? = null,
+  val assessmentDecision: AssessmentDecision? = null,
 
-  @get:JsonProperty("data") val `data`: kotlin.Any? = null,
+  val assessmentDecisionDate: java.time.LocalDate? = null,
 
-  @get:JsonProperty("document") val document: kotlin.Any? = null,
+  val submittedAt: java.time.Instant? = null,
 
-  @get:JsonProperty("assessmentId") val assessmentId: java.util.UUID? = null,
+  val personStatusOnSubmission: PersonStatus? = null,
 
-  @get:JsonProperty("assessmentDecision") val assessmentDecision: AssessmentDecision? = null,
+  val apArea: ApArea? = null,
 
-  @get:JsonProperty("assessmentDecisionDate") val assessmentDecisionDate: java.time.LocalDate? = null,
+  val cruManagementArea: Cas1CruManagementArea? = null,
 
-  @get:JsonProperty("submittedAt") val submittedAt: java.time.Instant? = null,
-
-  @get:JsonProperty("personStatusOnSubmission") val personStatusOnSubmission: PersonStatus? = null,
-
-  @get:JsonProperty("apArea") val apArea: ApArea? = null,
-
-  @get:JsonProperty("cruManagementArea") val cruManagementArea: Cas1CruManagementArea? = null,
-
-  @get:JsonProperty("applicantUserDetails") val applicantUserDetails: Cas1ApplicationUserDetails? = null,
+  val applicantUserDetails: Cas1ApplicationUserDetails? = null,
 
   @Schema(description = "If true, caseManagerUserDetails will provide case manager details. Otherwise, applicantUserDetails can be used for case manager details")
-  @get:JsonProperty("caseManagerIsNotApplicant") val caseManagerIsNotApplicant: kotlin.Boolean? = null,
+  val caseManagerIsNotApplicant: kotlin.Boolean? = null,
 
-  @get:JsonProperty("caseManagerUserDetails") val caseManagerUserDetails: Cas1ApplicationUserDetails? = null,
+  val caseManagerUserDetails: Cas1ApplicationUserDetails? = null,
 
-  @get:JsonProperty("licenceExpiryDate") val licenceExpiryDate: java.time.LocalDate? = null,
+  val licenceExpiryDate: java.time.LocalDate? = null,
+
+  val releaseType: ReleaseTypeOption? = null,
+
+  val sentenceType: SentenceTypeOption? = null,
+
+  override val type: kotlin.String,
+
+  override val id: java.util.UUID,
+
+  override val person: Person,
+
+  override val createdAt: java.time.Instant,
 ) : Application
