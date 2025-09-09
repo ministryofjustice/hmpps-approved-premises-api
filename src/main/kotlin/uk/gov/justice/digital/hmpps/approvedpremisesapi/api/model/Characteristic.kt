963ded2a3409d3a48b150fb99fe527007d763a2d
daveawc <168838504+daveawc@users.noreply.github.com>
2025-09-09 21:20:22 +0100

    tidied up characteristic generated code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/Characteristic.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/Characteristic.kt
@@ -1,42 +1,18 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonValue
-import io.swagger.v3.oas.annotations.media.Schema
+import java.util.UUID
 
-/**
- *
- * @param id
- * @param name
- * @param serviceScope
- * @param modelScope
- * @param propertyName
- */
 data class Characteristic(
-
-  @Schema(example = "952790c0-21d7-4fd6-a7e1-9018f08d8bb0", required = true, description = "")
-  @get:JsonProperty("id", required = true) val id: java.util.UUID,
-
-  @Schema(example = "Is this premises catered (rather than self-catered)?", required = true, description = "")
-  @get:JsonProperty("name", required = true) val name: kotlin.String,
-
-  @Schema(example = "null", required = true, description = "")
-  @get:JsonProperty("serviceScope", required = true) val serviceScope: Characteristic.ServiceScope,
-
-  @Schema(example = "null", required = true, description = "")
-  @get:JsonProperty("modelScope", required = true) val modelScope: Characteristic.ModelScope,
-
-  @Schema(example = "isCatered", description = "")
-  @get:JsonProperty("propertyName") val propertyName: kotlin.String? = null,
+  val id: UUID,
+  val name: String,
+  val serviceScope: ServiceScope,
+  val modelScope: ModelScope,
+  val propertyName: String? = null,
 ) {
-
-  /**
-   *
-   * Values: approvedMinusPremises,temporaryMinusAccommodation,star
-   */
   @Suppress("ktlint:standard:enum-entry-name-case", "EnumNaming")
-  enum class ServiceScope(@get:JsonValue val value: kotlin.String) {
+  enum class ServiceScope(@get:JsonValue val value: String) {
 
     approvedMinusPremises("approved-premises"),
     temporaryMinusAccommodation("temporary-accommodation"),
@@ -46,14 +22,10 @@ data class Characteristic(
     companion object {
       @JvmStatic
       @JsonCreator
-      fun forValue(value: kotlin.String): ServiceScope = values().first { it -> it.value == value }
+      fun forValue(value: String): ServiceScope = entries.first { it.value == value }
     }
   }
 
-  /**
-   *
-   * Values: premises,room,star
-   */
   @Suppress("ktlint:standard:enum-entry-name-case", "EnumNaming")
   enum class ModelScope(@get:JsonValue val value: kotlin.String) {
 
@@ -65,7 +37,7 @@ data class Characteristic(
     companion object {
       @JvmStatic
       @JsonCreator
-      fun forValue(value: kotlin.String): ModelScope = values().first { it -> it.value == value }
+      fun forValue(value: String): ModelScope = entries.first { it.value == value }
     }
   }
 }
