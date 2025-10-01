5299fc882ff425a387b8a8775be212b69c7b5a01
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-10-01 13:33:33 +0100

    chore/CAS-2002-removing-use-of-ExternalUserEntity

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/factory/Cas2StatusUpdateEntityFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/factory/Cas2StatusUpdateEntityFactory.kt
@@ -7,6 +7,7 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.Cas2Appl
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.Cas2AssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.Cas2StatusUpdateDetailEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.Cas2StatusUpdateEntity
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.Cas2UserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.jpa.entity.ExternalUserEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.reporting.model.reference.Cas2ApplicationStatusSeeding
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.randomDateTimeBefore
@@ -15,7 +16,8 @@ import java.util.UUID
 
 class Cas2StatusUpdateEntityFactory : Factory<Cas2StatusUpdateEntity> {
   private var id: Yielded<UUID> = { UUID.randomUUID() }
-  private var assessor: Yielded<ExternalUserEntity> = { ExternalUserEntityFactory().produce() }
+  private var assessor: Yielded<Cas2UserEntity?> = { null }
+  private var externalAssessor: Yielded<ExternalUserEntity?> = { null }
   private var assessment: Yielded<Cas2AssessmentEntity?> = { null }
   private var application: Yielded<Cas2ApplicationEntity>? = null
   private var statusId: Yielded<UUID> = { Cas2ApplicationStatusSeeding.statusList(ServiceName.cas2).random().id }
@@ -28,8 +30,12 @@ class Cas2StatusUpdateEntityFactory : Factory<Cas2StatusUpdateEntity> {
     this.id = { id }
   }
 
-  fun withAssessor(assessor: ExternalUserEntity) = apply {
+  fun withAssessor(assessor: Cas2UserEntity) = apply {
     this.assessor = { assessor }
+  }
+
+  fun withExternalAssessor(externalAssessor: ExternalUserEntity) = apply {
+    this.externalAssessor = { externalAssessor }
   }
 
   fun withAssessment(assessment: Cas2AssessmentEntity) = apply {
@@ -63,6 +69,7 @@ class Cas2StatusUpdateEntityFactory : Factory<Cas2StatusUpdateEntity> {
   override fun produce(): Cas2StatusUpdateEntity = Cas2StatusUpdateEntity(
     id = this.id(),
     assessor = this.assessor(),
+    externalAssessor = this.externalAssessor(),
     application = this.application?.invoke() ?: error("Must provide a submitted application"),
     assessment = this.assessment(),
     statusId = this.statusId(),
