df27f9ab281071ba73c934a20fc0cfad80be83cc
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-10-03 13:40:17 +0100

    chore/CAS-2002-add-more-flyways-to-prepare-for-merge-work

---
+++ src/main/resources/db/migration/all/20251003114505__.sql
@@ -0,0 +1,5 @@
+-- All of these tables are currently empty
+ALTER TABLE cas_2_users ADD COLUMN service_origin TEXT NOT NULL DEFAULT 'HDC';
+ALTER TABLE cas_2_assessments ADD COLUMN service_origin TEXT NOT NULL DEFAULT 'HDC';
+ALTER TABLE cas_2_applications ADD COLUMN service_origin TEXT NOT NULL DEFAULT 'HDC';
+ALTER TABLE cas_2_assessments DROP COLUMN application_origin;
