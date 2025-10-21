5422d17ba698221fd8622eac8f6371852e0c61a2
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-10-21 11:51:05 +0100

    chore/CAS-2002-flyways-to-drop-unwanted-cas-2-columns

---
+++ src/main/resources/db/migration/all/20251021113645__drop_unwanted_cas_2_columns.sql
@@ -0,0 +1,6 @@
+ALTER TABLE cas_2_application_assignments DROP COLUMN allocated_pom_user_id CASCADE;
+ALTER TABLE cas_2_application_notes DROP COLUMN created_by_nomis_user_id;
+ALTER TABLE cas_2_application_notes DROP COLUMN created_by_external_user_id;
+ALTER TABLE cas_2_applications DROP COLUMN created_by_user_id;
+ALTER TABLE cas_2_assessments DROP COLUMN application_origin;
+ALTER TABLE cas_2_status_updates DROP COLUMN assessor_id;
