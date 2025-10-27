55dda41ca6a8ea5f4f0f69b308496d14903c6725
bethan.dobb <bethan.dobb@justice.gov.uk>
2025-10-27 11:13:30 +0000

    Revert "Move Cas3 Get assessment to CAS3 namespace"
    This reverts commit c4bbdc566145fe3d5c473471fcd0345c466b9f5c.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas3/transformer/Cas3AssessmentTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas3/transformer/Cas3AssessmentTransformer.kt
@@ -4,62 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.AssessmentStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model.Cas3Assessment
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model.Cas3AssessmentSummary
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model.TemporaryAccommodationApplication
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model.generated.TemporaryAccommodationAssessmentStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model.generated.TemporaryAccommodationUser
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentDecision
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainAssessmentSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.DomainAssessmentSummaryStatus
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TemporaryAccommodationApplicationEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.TemporaryAccommodationAssessmentEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonRisks
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.ApplicationsTransformer
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.AssessmentClarificationNoteTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.PersonTransformer
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.RisksTransformer
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer.UserTransformer
-import java.time.LocalDate
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.AssessmentDecision as ApiAssessmentDecision
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.AssessmentDecision as JpaAssessmentDecision
 
 @Component
 class Cas3AssessmentTransformer(
-  private val applicationsTransformer: ApplicationsTransformer,
   private val personTransformer: PersonTransformer,
   private val risksTransformer: RisksTransformer,
-  private val assessmentClarificationNoteTransformer: AssessmentClarificationNoteTransformer,
-  private val userTransformer: UserTransformer,
   private val objectMapper: ObjectMapper,
 ) {
-  fun transformJpaToApi(
-    jpa: TemporaryAccommodationAssessmentEntity,
-    personInfo: PersonInfoResult,
-  ) = Cas3Assessment(
-    id = jpa.id,
-    application = applicationsTransformer.transformJpaToApi(jpa.application, personInfo) as TemporaryAccommodationApplication,
-    createdAt = jpa.createdAt.toInstant(),
-    allocatedAt = jpa.allocatedAt?.toInstant(),
-    data = if (jpa.data != null) objectMapper.readTree(jpa.data) else null,
-    clarificationNotes = jpa.clarificationNotes.map(assessmentClarificationNoteTransformer::transformJpaToApi),
-    allocatedToStaffMember = jpa.allocatedToUser?.let {
-      userTransformer.transformJpaToApi(
-        it,
-        ServiceName.temporaryAccommodation,
-      ) as TemporaryAccommodationUser
-    },
-    submittedAt = jpa.submittedAt?.toInstant(),
-    decision = transformJpaDecisionToApi(jpa.decision),
-    rejectionRationale = jpa.rejectionRationale,
-    status = getAssessmentStatus(jpa),
-    summaryData = objectMapper.readTree(jpa.summaryData),
-    releaseDate = jpa.releaseDate ?: jpa.typedApplication<TemporaryAccommodationApplicationEntity>().personReleaseDate,
-    accommodationRequiredFromDate = jpa.accommodationRequiredFromDate ?: LocalDate.from(jpa.typedApplication<TemporaryAccommodationApplicationEntity>().arrivalDate),
-  )
-
   fun transformDomainToApiSummary(ase: DomainAssessmentSummary, personInfo: PersonInfoResult): Cas3AssessmentSummary = Cas3AssessmentSummary(
     id = ase.id,
     applicationId = ase.applicationId,
@@ -98,25 +58,9 @@ class Cas3AssessmentTransformer(
     else -> TemporaryAccommodationAssessmentStatus.unallocated
   }
 
-  private fun getAssessmentStatus(entity: TemporaryAccommodationAssessmentEntity) = when {
-    entity.decision == AssessmentDecision.REJECTED -> TemporaryAccommodationAssessmentStatus.rejected
-    entity.decision == AssessmentDecision.ACCEPTED && (entity as TemporaryAccommodationAssessmentEntity).completedAt != null ->
-      TemporaryAccommodationAssessmentStatus.closed
-
-    entity.decision == AssessmentDecision.ACCEPTED -> TemporaryAccommodationAssessmentStatus.readyToPlace
-    entity.allocatedToUser != null -> TemporaryAccommodationAssessmentStatus.inReview
-    else -> TemporaryAccommodationAssessmentStatus.unallocated
-  }
-
   private fun transformDomainSummaryDecisionToApi(decision: String?) = when (decision) {
     "ACCEPTED" -> ApiAssessmentDecision.accepted
     "REJECTED" -> ApiAssessmentDecision.rejected
     else -> null
-  }
-
-  fun transformJpaDecisionToApi(decision: JpaAssessmentDecision?) = when (decision) {
-    JpaAssessmentDecision.ACCEPTED -> ApiAssessmentDecision.accepted
-    JpaAssessmentDecision.REJECTED -> ApiAssessmentDecision.rejected
-    null -> null
   }
 }
