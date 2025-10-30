51e2c9fa57f3efbd1f74b7bb04a7d027c976dd0b
daveawc <168838504+daveawc@users.noreply.github.com>
2025-10-30 15:39:13 +0000

    removing cas3bookingseviceTest and remove unused parameters

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/service/PremisesService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.service
 
-import arrow.core.Either
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Characteristic
@@ -11,8 +10,6 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingReposi
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.LocalAuthorityAreaRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesEntity
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.PremisesRepository
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ProbationDeliveryUnitEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ProbationDeliveryUnitRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.ProbationRegionRepository
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.Availability
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.ValidationErrors
@@ -29,7 +26,6 @@ class PremisesService(
   private val bookingRepository: BookingRepository,
   private val localAuthorityAreaRepository: LocalAuthorityAreaRepository,
   private val probationRegionRepository: ProbationRegionRepository,
-  private val probationDeliveryUnitRepository: ProbationDeliveryUnitRepository,
   private val characteristicService: CharacteristicService,
 ) {
   fun getPremises(premisesId: UUID): PremisesEntity? = premisesRepository.findByIdOrNull(premisesId)
@@ -135,42 +131,6 @@ class PremisesService(
     return AuthorisableActionResult.Success(
       ValidatableActionResult.Success(savedPremises),
     )
-  }
-
-  private fun tryGetProbationDeliveryUnit(
-    probationDeliveryUnitIdentifier: Either<String, UUID>?,
-    probationRegionId: UUID,
-    onValidationError: (property: String, err: String) -> Unit,
-  ): ProbationDeliveryUnitEntity? {
-    val probationDeliveryUnit = when (probationDeliveryUnitIdentifier) {
-      null -> {
-        onValidationError("$.probationDeliveryUnitId", "empty")
-        null
-      }
-      else -> probationDeliveryUnitIdentifier.fold({ name ->
-        if (name.isBlank()) {
-          onValidationError("$.pdu", "empty")
-        }
-
-        val result = probationDeliveryUnitRepository.findByNameAndProbationRegionId(name, probationRegionId)
-
-        if (result == null) {
-          onValidationError("$.pdu", "doesNotExist")
-        }
-
-        result
-      }, { id ->
-        val result = probationDeliveryUnitRepository.findByIdAndProbationRegionId(id, probationRegionId)
-
-        if (result == null) {
-          onValidationError("$.probationDeliveryUnitId", "doesNotExist")
-        }
-
-        result
-      })
-    }
-
-    return probationDeliveryUnit
   }
 
   fun getBedCount(premises: PremisesEntity): Int = premisesRepository.getBedCount(premises)
