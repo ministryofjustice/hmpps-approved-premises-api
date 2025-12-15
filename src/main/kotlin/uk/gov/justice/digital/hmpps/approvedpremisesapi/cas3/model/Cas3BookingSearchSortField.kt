5c7abf5fde8bb4b16af8dc0a1a1d770a0127d312
Rashad Mughal <rashadmughal@users.noreply.github.com>
2025-12-15 17:20:16 +0000

    SAS-145: C3 BE Fix booking sort order enum

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas3/model/Cas3BookingSearchSortField.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas3/model/Cas3BookingSearchSortField.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 
 enum class Cas3BookingSearchSortField(val value: String) {
   PERSON_NAME("name"),
@@ -9,6 +10,9 @@ enum class Cas3BookingSearchSortField(val value: String) {
   BOOKING_END_DATE("endDate"),
   BOOKING_CREATED_AT("createdAt"),
   ;
+
+  @JsonValue
+  fun toValue(): String = value
 
   companion object {
     @JvmStatic
