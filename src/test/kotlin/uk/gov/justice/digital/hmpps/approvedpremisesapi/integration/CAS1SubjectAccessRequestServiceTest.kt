74e8643a8f4501c178964712bf33c3dc329824bf
Daniel Humphreys <daniel.humphreys@digital.justice.gov.uk>
2026-02-18 15:30:35 +0000

    PlacementRequirements - Placement Requirement ID: delete from query

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/CAS1SubjectAccessRequestServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/CAS1SubjectAccessRequestServiceTest.kt
@@ -520,7 +520,6 @@ class CAS1SubjectAccessRequestServiceTest : SubjectAccessRequestServiceTestBase(
       {
         "crn": "${placementRequirement.application.crn}",
         "noms_number": "${placementRequirement.application.nomsNumber}",
-        "placement_requirements_id": "${placementRequirement.id}",
         "ap_type": "${placementRequirement.apType.name}",
         "outcode": "${placementRequirement.postcodeDistrict.outcode}",
         "radius": ${placementRequirement.radius},
