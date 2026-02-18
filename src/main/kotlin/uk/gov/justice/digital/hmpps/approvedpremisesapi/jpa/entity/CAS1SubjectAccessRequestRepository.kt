4fa04bd5b9c0eb37c4db94ffe7ccd330c4056ce5
Daniel Humphreys <daniel.humphreys@digital.justice.gov.uk>
2026-02-18 12:37:21 +0000

    PlacementRequest - Assessment ID: delete from query

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/CAS1SubjectAccessRequestRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/CAS1SubjectAccessRequestRepository.kt
@@ -298,7 +298,6 @@ from
               pr.expected_arrival,
               pr.duration, 
               pr.created_at,
-              pr.assessment_id,
               pr.notes,
               pr.is_parole,
               pr.is_withdrawn,
