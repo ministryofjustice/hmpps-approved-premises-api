7f24fd077f3fc57882edd670df5f15d234ab6d74
Daniel Humphreys <daniel.humphreys@digital.justice.gov.uk>
2026-02-18 15:30:35 +0000

    PlacementRequirements - Placement Requirement ID: delete from query

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/CAS1SubjectAccessRequestRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/CAS1SubjectAccessRequestRepository.kt
@@ -328,7 +328,6 @@ from
         select 
              app.crn,
              app.noms_number,
-             pr.id as placement_requirements_id,
              case
                when pr.ap_type = '0' then 'NORMAL'
                when pr.ap_type = '1' then 'PIPE'
