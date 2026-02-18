0ddab73936cd367871763720e9699677152e3e5a
Daniel Humphreys <daniel.humphreys@digital.justice.gov.uk>
2026-02-18 16:04:27 +0000

    Appeals - Assessment ID: delete from query

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/CAS1SubjectAccessRequestServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/integration/CAS1SubjectAccessRequestServiceTest.kt
@@ -569,7 +569,6 @@ class CAS1SubjectAccessRequestServiceTest : SubjectAccessRequestServiceTestBase(
       {
           "crn": "${appeal.application.crn}" ,
           "noms_number": "${appeal.application.nomsNumber}", 
-          "assessment_id": "${appeal.assessment.id}",
           "appeal_date": "$APPEAL_DATE_ONLY",
           "appeal_detail": "${appeal.appealDetail}",
           "decision" : "${appeal.decision}",
