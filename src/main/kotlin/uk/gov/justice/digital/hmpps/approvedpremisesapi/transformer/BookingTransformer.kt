a5973934001a8b6ded1183ec382ea4841c5b8ddb
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-05-22 16:54:30 +0100

    Remove unused shared /premises endpoints
    Migration to cas1 and cas3 specific endpoints for premises operations is now complete, leaving the following shared endpoints unused:
    
    * GET /premises/{premisesId}/bookings/{bookingId} (this also uses the now defunct booking model
    * GET /premises/{premisesId}
    * PUT /premises/{premisesId}
    
    Removing these controllers allows us to remove misc. API types that are no longer in use

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/BookingTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/transformer/BookingTransformer.kt
@@ -1,66 +1,16 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.transformer
 
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.Booking
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingPremisesSummary
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceName
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.transformer.Cas3ArrivalTransformer
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.transformer.Cas3ConfirmationTransformer
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas3.transformer.Cas3TurnaroundTransformer
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.convert.EnumConverterFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.jpa.entity.BookingEntity
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.PersonInfoResult
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.problem.InternalServerErrorProblem
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.service.WorkingDayService
 import java.time.LocalDate
 
 @Component
 class BookingTransformer(
-  private val personTransformer: PersonTransformer,
-  private val cas3ArrivalTransformer: Cas3ArrivalTransformer,
-  private val departureTransformer: DepartureTransformer,
-  private val nonArrivalTransformer: NonArrivalTransformer,
-  private val cancellationTransformer: CancellationTransformer,
-  private val cas3ConfirmationTransformer: Cas3ConfirmationTransformer,
-  private val extensionTransformer: ExtensionTransformer,
-  private val bedTransformer: BedTransformer,
-  private val cas3TurnaroundTransformer: Cas3TurnaroundTransformer,
-  private val enumConverterFactory: EnumConverterFactory,
   private val workingDayService: WorkingDayService,
 ) {
-
-  fun transformJpaToApi(jpa: BookingEntity, personInfo: PersonInfoResult): Booking {
-    val hasNonZeroDayTurnaround = jpa.turnaround != null && jpa.turnaround!!.workingDayCount != 0
-
-    return Booking(
-      id = jpa.id,
-      person = personTransformer.transformModelToPersonApi(personInfo),
-      arrivalDate = jpa.arrivalDate,
-      departureDate = jpa.departureDate,
-      serviceName = enumConverterFactory.getConverter(ServiceName::class.java).convert(jpa.service) ?: throw InternalServerErrorProblem("Could not convert '${jpa.service}' to a ServiceName"),
-      status = determineStatus(jpa),
-      arrival = cas3ArrivalTransformer.transformJpaToArrival(jpa.arrival),
-      departure = departureTransformer.transformJpaToApi(jpa.departure),
-      departures = jpa.departures.map { departureTransformer.transformJpaToApi(it)!! },
-      nonArrival = nonArrivalTransformer.transformJpaToApi(jpa.nonArrival),
-      cancellation = cancellationTransformer.transformJpaToApi(jpa.cancellation),
-      cancellations = jpa.cancellations.map { cancellationTransformer.transformJpaToApi(it)!! },
-      confirmation = cas3ConfirmationTransformer.transformJpaToApi(jpa.confirmation),
-      extensions = jpa.extensions.map(extensionTransformer::transformJpaToApi),
-      bed = jpa.bed?.let { bedTransformer.transformJpaToApi(it) },
-      originalArrivalDate = jpa.originalArrivalDate,
-      originalDepartureDate = jpa.originalDepartureDate,
-      createdAt = jpa.createdAt.toInstant(),
-      turnaround = jpa.turnaround?.let(cas3TurnaroundTransformer::transformJpaToApi),
-      turnarounds = jpa.turnarounds.map(cas3TurnaroundTransformer::transformJpaToApi),
-      turnaroundStartDate = if (hasNonZeroDayTurnaround) workingDayService.addWorkingDays(jpa.departureDate, 1) else null,
-      effectiveEndDate = if (hasNonZeroDayTurnaround) workingDayService.addWorkingDays(jpa.departureDate, jpa.turnaround!!.workingDayCount) else jpa.departureDate,
-      applicationId = jpa.application?.id,
-      assessmentId = jpa.application?.getLatestAssessment()?.id,
-      premises = jpa.premises.let { BookingPremisesSummary(it.id, it.name) },
-    )
-  }
 
   fun determineStatus(jpa: BookingEntity) = when {
     jpa.service == ServiceName.approvedPremises.value -> determineApprovedPremisesStatus(jpa)
