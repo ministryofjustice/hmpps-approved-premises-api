fa372664ffbcb00c4d0ef4d7c4b336c58a15bc68
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-05-26 12:34:42 +0100

    Add CAS2V2 OASys Risk to Self Endpoint
    This information is required for new screens as part of the ISR changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/transformer/Cas2v2OASysTransformer.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2v2/transformer/Cas2v2OASysTransformer.kt
@@ -2,7 +2,9 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.transformer
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.model.Cas2v2OASysAssessmentMetadataDto
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2v2.model.Cas2v2OAsysRiskToSelfDto
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.apandoasys.OASysAssessmentSummary
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.client.apandoasys.RisksToTheIndividual
 
 @Service
 class Cas2v2OASysTransformer {
@@ -11,5 +13,15 @@ class Cas2v2OASysTransformer {
     hasApplicableAssessment = summary != null,
     dateStarted = summary?.initiationDate?.toInstant(),
     dateCompleted = summary?.completedDate?.toInstant(),
+  )
+
+  fun toOASysRiskToSelfDto(risksToTheIndividual: RisksToTheIndividual?) = Cas2v2OAsysRiskToSelfDto(
+    metadata = Cas2v2OASysAssessmentMetadataDto(
+      hasApplicableAssessment = risksToTheIndividual != null,
+      dateStarted = risksToTheIndividual?.initiationDate?.toInstant(),
+      dateCompleted = risksToTheIndividual?.dateCompleted?.toInstant(),
+    ),
+    analysisSuicideSelfharm = risksToTheIndividual?.riskToTheIndividual?.analysisSuicideSelfharm,
+    analysisVulnerabilities = risksToTheIndividual?.riskToTheIndividual?.analysisVulnerabilities,
   )
 }
