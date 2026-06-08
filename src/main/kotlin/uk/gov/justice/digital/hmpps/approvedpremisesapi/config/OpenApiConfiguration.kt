b94d9d6f883639e9b82232e6e9be3cbf36ff7cd1
Daniel Humphreys <daniel.humphreys@justice.gov.uk>
2026-06-08 13:21:14 +0100

    Add deprecated schema aliases for renamed Cas2Hdc DTOs in OpenAPI configuration.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/config/OpenApiConfiguration.kt
@@ -148,6 +148,33 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
         if (pathItem.patch != null) pathItem.patch.operationId = prefix + pathItem.patch.operationId
         if (pathItem.delete != null) pathItem.delete.operationId = prefix + pathItem.delete.operationId
       }
+
+    addDeprecatedSchemaAlias(openApi, "Cas2ApplicationStatusSeeding", "Cas2HdcApplicationStatusSeeding")
+    addDeprecatedSchemaAlias(openApi, "Cas2PersistedApplicationStatus", "Cas2HdcPersistedApplicationStatus")
+    addDeprecatedSchemaAlias(openApi, "Cas2PersistedApplicationStatusDetail", "Cas2HdcPersistedApplicationStatusDetail")
+    addDeprecatedSchemaAlias(openApi, "Cas2PersistedApplicationStatusFinder", "Cas2HdcPersistedApplicationStatusFinder")
+    addDeprecatedSchemaAlias(openApi, "Cas2Application", "Cas2HdcApplication")
+    addDeprecatedSchemaAlias(openApi, "Cas2ApplicationNote", "Cas2HdcApplicationNote")
+    addDeprecatedSchemaAlias(openApi, "Cas2ApplicationStatus", "Cas2HdcApplicationStatus")
+    addDeprecatedSchemaAlias(openApi, "Cas2ApplicationStatusDetail", "Cas2HdcApplicationStatusDetail")
+    addDeprecatedSchemaAlias(openApi, "ApplicationStatusUpdatesReportRow", "Cas2HdcApplicationStatusUpdatesReportRow")
+    addDeprecatedSchemaAlias(openApi, "Cas2ApplicationSummary", "Cas2HdcApplicationSummary")
+    addDeprecatedSchemaAlias(openApi, "Cas2Assessment", "Cas2HdcAssessment")
+    addDeprecatedSchemaAlias(openApi, "Cas2AssessmentStatusUpdate", "Cas2HdcAssessmentStatusUpdate")
+    addDeprecatedSchemaAlias(openApi, "LatestCas2StatusUpdate", "Cas2HdcLatestStatusUpdate")
+    addDeprecatedSchemaAlias(openApi, "NewCas2ApplicationNote", "Cas2HdcNewApplicationNote")
+    addDeprecatedSchemaAlias(openApi, "Cas2ReportName", "Cas2HdcReportName")
+    addDeprecatedSchemaAlias(openApi, "Cas2ServiceOrigin", "Cas2HdcServiceOrigin")
+    addDeprecatedSchemaAlias(openApi, "Cas2StatusUpdate", "Cas2HdcStatusUpdate")
+    addDeprecatedSchemaAlias(openApi, "Cas2StatusUpdateDetail", "Cas2HdcStatusUpdateDetail")
+    addDeprecatedSchemaAlias(openApi, "SubmitCas2Application", "Cas2HdcSubmitApplication")
+    addDeprecatedSchemaAlias(openApi, "Cas2SubmittedApplication", "Cas2HdcSubmittedApplication")
+    addDeprecatedSchemaAlias(openApi, "SubmittedApplicationReportRow", "Cas2HdcSubmittedApplicationReportRow")
+    addDeprecatedSchemaAlias(openApi, "Cas2SubmittedApplicationSummary", "Cas2HdcSubmittedApplicationSummary")
+    addDeprecatedSchemaAlias(openApi, "Cas2TimelineEvent", "Cas2HdcTimelineEvent")
+    addDeprecatedSchemaAlias(openApi, "UnsubmittedApplicationsReportRow", "Cas2HdcUnsubmittedApplicationsReportRow")
+    addDeprecatedSchemaAlias(openApi, "UpdateCas2Application", "Cas2HdcUpdateApplication")
+    addDeprecatedSchemaAlias(openApi, "UpdateCas2Assessment", "Cas2HdcUpdateAssessment")
   }
 
   private fun createProblemSchema(): Schema<*> = ModelConverters.getInstance()
@@ -169,5 +196,20 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
           ),
         )
     }
+  }
+
+  private fun addDeprecatedSchemaAlias(
+    openApi: OpenAPI,
+    alias: String,
+    target: String,
+  ) {
+    val schemas = openApi.components?.schemas ?: return
+    if (!schemas.containsKey(target) || schemas.containsKey(alias)) return
+
+    val aliasSchema = Schema<Any>().`$ref`("#/components/schemas/$target")
+    aliasSchema.deprecated = true
+    aliasSchema.description = "Deprecated alias of $target"
+
+    schemas[alias] = aliasSchema
   }
 }
