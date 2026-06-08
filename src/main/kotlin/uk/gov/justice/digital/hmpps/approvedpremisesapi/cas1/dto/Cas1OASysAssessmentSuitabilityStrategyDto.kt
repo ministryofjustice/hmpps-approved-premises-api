670acf79975230b89e6b19a5f5c949e151a9a09c
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-06-08 09:37:15 +0100

    Prefix OASys Suitability Strategy with ‘CAS1’

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/Cas1OASysAssessmentSuitabilityStrategyDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/Cas1OASysAssessmentSuitabilityStrategyDto.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas1.dto
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
 
-enum class OASysAssessmentSuitabilityStrategyDto(@get:JsonValue val value: String) {
+enum class Cas1OASysAssessmentSuitabilityStrategyDto(@get:JsonValue val value: String) {
   ALLOW_ALL("allow_all"),
   COMPLETED_IN_LAST_SIX_MONTHS("completed_in_last_six_months"),
   ;
