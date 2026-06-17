1b9646de6495b5ef4b887654e2eeedaf5c3ca5c4
Sajif Deva <sajif.devapalan@justice.gov.uk>
2026-06-17 09:17:04 +0100

    Remove the  endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/PlacementRequestsApiDelegate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/PlacementRequestsApiDelegate.kt
@@ -6,29 +6,12 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.context.request.NativeWebRequest
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.BookingNotMade
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NewBookingNotMade
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NewPlacementRequestBooking
-import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.NewPlacementRequestBookingConfirmation
 import java.util.Optional
 
 @jakarta.annotation.Generated(value = ["org.openapitools.codegen.languages.KotlinSpringServerCodegen"], comments = "Generator version: 7.13.0")
 interface PlacementRequestsApiDelegate {
 
   fun getRequest(): Optional<NativeWebRequest> = Optional.empty()
-
-  fun placementRequestsIdBookingPost(
-    id: java.util.UUID,
-    newPlacementRequestBooking: NewPlacementRequestBooking,
-  ): ResponseEntity<NewPlacementRequestBookingConfirmation> {
-    getRequest().ifPresent { request ->
-      for (mediaType in MediaType.parseMediaTypes(request.getHeader("Accept"))) {
-        if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
-          ApiUtil.setExampleResponse(request, "application/json", "{  \"premisesName\" : \"premisesName\",  \"departureDate\" : \"2022-09-30\",  \"arrivalDate\" : \"2022-07-28\"}")
-          break
-        }
-      }
-    }
-    return ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
-  }
 
   fun placementRequestsIdBookingNotMadePost(
     id: java.util.UUID,
