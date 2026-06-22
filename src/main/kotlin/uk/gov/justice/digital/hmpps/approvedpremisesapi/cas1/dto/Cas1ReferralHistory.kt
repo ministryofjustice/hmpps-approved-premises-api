60ff97dafa59f1be2ee76ecfa5745359ae6de99f
Rashad Mughal <rashadmughal@users.noreply.github.com>
2026-06-22 14:36:00 +0100

    SAS-467: Fix cas1 and cas3 status.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/Cas1ReferralHistory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/Cas1ReferralHistory.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas1.dto
 
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.RequestForPlacementStatus
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.ServiceType
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.model.ApprovedPremisesApplicationStatus
 import java.time.Instant
 import java.util.UUID
 
@@ -8,13 +10,14 @@ data class Cas1ReferralHistory(
   val type: ServiceType,
   val id: UUID,
   val applicationId: UUID,
-  val status: Cas1AssessmentStatus,
+  val applicationStatus: ApprovedPremisesApplicationStatus,
+  val requestForPlacementStatus: RequestForPlacementStatus?,
   val createdAt: Instant,
   val referralRejectionReason: String?,
   val localAuthorityArea: String?,
   val pdu: String?,
   val referredBy: Cas1StaffDto,
   val placementAddress: String?,
-  val placementStatus: String?,
+  val placementStatus: Cas1SpaceBookingStatus?,
   val uiUrl: String,
 )
