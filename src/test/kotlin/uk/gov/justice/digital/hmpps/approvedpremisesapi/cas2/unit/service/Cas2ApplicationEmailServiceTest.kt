59176d96c67e0fa5724cdfc6ed2b8a46b7f9f126
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-07-24 15:13:59 +0100

    Send cas2 application to assess email to assessors

--- src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/unit/service/Cas2ApplicationEmailServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas2/unit/service/Cas2ApplicationEmailServiceTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2.unit.service
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
@@ -9,16 +10,20 @@ import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2hdc.factory.Cas2Appl
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2hdc.factory.Cas2UserEntityFactory
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.cas2hdc.jpa.entity.Cas2Cohort
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.config.Cas2NotifyTemplates
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.config.EmailAddressConfig
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.config.NotifyConfig
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.util.UrlTemplate
 import java.time.OffsetDateTime
 import java.util.UUID
 
 class Cas2ApplicationEmailServiceTest {
   private val mockCas2EmailService = mockk<Cas2EmailService>(relaxed = true)
+  private val mockNotifyConfig = mockk<NotifyConfig>()
   private val submittedApplicationUrlTemplate = UrlTemplate("http://frontend/applications/#applicationId")
 
   private val service = Cas2ApplicationEmailService(
     cas2EmailService = mockCas2EmailService,
+    notifyConfig = mockNotifyConfig,
     submittedApplicationUrlTemplate = submittedApplicationUrlTemplate,
   )
 
@@ -28,6 +33,10 @@ class Cas2ApplicationEmailServiceTest {
 
   @Test
   fun `applicationSubmitted sends submitted and assessed emails`() {
+    val emailConfig = EmailAddressConfig()
+    emailConfig.cas2Assessors = "assessors@example.com"
+    every { mockNotifyConfig.emailAddresses } returns emailConfig
+
     val applicationId = UUID.randomUUID()
     val submittedAt = OffsetDateTime.parse("2024-06-24T16:07:00Z")
     val application = Cas2ApplicationEntityFactory()
@@ -58,7 +67,7 @@ class Cas2ApplicationEmailServiceTest {
 
     verify(exactly = 1) {
       mockCas2EmailService.sendEmail(
-        recipientEmailAddress = user.email!!,
+        recipientEmailAddress = "assessors@example.com",
         templateId = Cas2NotifyTemplates.CAS2_BAIL_APPLICATION_TO_ASSESS,
         personalisation = mapOf(
           "cohort" to "HDC",
