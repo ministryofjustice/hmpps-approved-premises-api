1cddd0531bb1af0786c5fabf5ff0fa21b0e639c5
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-07-29 14:36:53 +0100

    Improve tier documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/TierDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/TierDto.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas1.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 data class TierDto(
   val tierScore: String,
+  @Schema(description = "The date that the tier score last changed, or the most recent calculation date if this is the first time we've captured the tier for the given CRN")
   val calculationDate: LocalDateTime,
+  @Schema(description = "Provisional will only be provided when version is V3")
   val provisional: Boolean? = null,
   val version: TierVersionDto,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/common/entity/model/Tier.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/common/entity/model/Tier.kt
@@ -11,8 +11,16 @@ import java.util.UUID
 data class Tier(
   val tierScore: String,
   val calculationId: UUID,
+  /**
+   * The date that the tier score last changed, or the most recent
+   * calculation date if this is the first time we've captured the
+   * tier for the given CRN
+   */
   val calculationDate: LocalDateTime,
   val changeReason: String?,
+  /**
+   * Provisional will only be provided when version is V3
+   */
   val provisional: Boolean? = null,
   val version: TierVersion,
 )
