21c952d8a4c8e3823c1e6b2b21d24a2d62c9fb1b
muhammad-elabdulla <muhammad.elabdulla@justice.gov.uk>
2026-08-03 11:52:36 +0100

    FM-1005 Remove Common assessments rejection API endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/model/NewReferralHistoryUserNote.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class NewReferralHistoryUserNote(
-
-  @get:JsonProperty("message", required = true) val message: String,
-)
