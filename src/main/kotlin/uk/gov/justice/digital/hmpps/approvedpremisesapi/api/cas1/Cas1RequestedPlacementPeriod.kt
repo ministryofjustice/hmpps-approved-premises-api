52484f3a0f806c10adfca3582a172bed687a91d4
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-08-26 15:52:30 +0100

    Improve duration documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/cas1/Cas1RequestedPlacementPeriod.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/api/cas1/Cas1RequestedPlacementPeriod.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas1
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class Cas1RequestedPlacementPeriod(
   val arrival: java.time.LocalDate,
   val arrivalFlexible: Boolean?,
+  @Schema(description = "Duration requested by the applicant, which may be the default duration if not overidden")
   val duration: Int,
 )
