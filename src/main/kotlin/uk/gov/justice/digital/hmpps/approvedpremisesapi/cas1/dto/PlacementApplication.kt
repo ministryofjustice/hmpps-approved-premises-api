f390cd486cfb2146d735b653ce00687fbeb254bd
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2026-09-01 11:02:15 +0100

    Add PlacementApplication.requestedPlacementPeriod
    In preparation for adding a nullable duration when making a request for placement via placement_applications, this commit deprecates placement containers where duration will not be nullable and replaces them with `Cas1RequestedPlacementPeriod`, which will support a nullable placement duration in a future commit

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/PlacementApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/cas1/dto/PlacementApplication.kt
@@ -1,13 +1,14 @@
 package uk.gov.justice.digital.hmpps.approvedpremisesapi.cas1.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.cas1.Cas1RequestedPlacementPeriod
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.PlacementDates
 import uk.gov.justice.digital.hmpps.approvedpremisesapi.api.model.WithdrawPlacementRequestReason
 import java.time.Instant
 import java.util.UUID
 
+@Schema(description = "The API model used when constructing, submitting and approving a PlacementApplication. Once approved this is represented by a RequestForPlacement type")
 data class PlacementApplication(
-
   val applicationId: UUID,
   @Schema(description = "If type is 'Additional', provides the PlacementApplication ID. If type is 'Initial' this field provides a PlacementRequest ID.")
   val id: UUID,
@@ -20,11 +21,15 @@ data class PlacementApplication(
   val canBeWithdrawn: Boolean,
   val isWithdrawn: Boolean,
   val type: PlacementApplicationType,
-  @Schema(deprecated = true, description = "Deprecated, use dates. Only populated with values after the placement application has been submitted")
+  @Deprecated("use requestedPlacementPeriod")
+  @Schema(deprecated = true, description = "Use requestedPlacementPeriod")
   val placementDates: List<PlacementDates>,
   val submittedAt: Instant? = null,
   val data: Any? = null,
   val document: Any? = null,
   val withdrawalReason: WithdrawPlacementRequestReason? = null,
+  @Deprecated("use requestedPlacementPeriod")
+  @Schema(deprecated = true, description = "Use requestedPlacementPeriod")
   val dates: PlacementDates? = null,
+  val requestedPlacementPeriod: Cas1RequestedPlacementPeriod?,
 )
