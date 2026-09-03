856ae49e4a7cb569f4667e977f92b1c6c7ee1368
venkatesh bala <venkatesh.bala1@justice.gov.uk>
2026-09-03 09:48:43 +0100

    Remove unused functions.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/SubjectAccessRequestRepositoryBase.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/approvedpremisesapi/jpa/entity/SubjectAccessRequestRepositoryBase.kt
@@ -8,53 +8,6 @@ import java.time.LocalDateTime
 @Repository
 open class SubjectAccessRequestRepositoryBase(val jdbcTemplate: NamedParameterJdbcTemplate) {
 
-  fun domainEvents(
-    crn: String?,
-    nomsNumber: String?,
-    startDate: LocalDateTime?,
-    endDate: LocalDateTime?,
-    serviceName: String = "CAS1",
-  ): String? {
-    val result = jdbcTemplate.queryForMap(
-      """
-           select json_agg(domain_events) as json from ( 
-               select 
-                 de."type",
-                 de.occurred_at,
-                 de.created_at,
-                 de."data",
-                 CASE
-                   WHEN cas1_3_app.id IS NOT NULL THEN cas1_3_app.submitted_at
-                   WHEN cas2_app.id IS NOT NULL THEN cas2_app.submitted_at
-                   ELSE NULL
-                 END as application_submitted_at,
-                 u.delius_username as triggered_by_username,
-                 cas3_booking.arrival_date as cas3_booking_arrival_date,
-                 cas3_premises.name as cas3_booking_premises_name
-               from
-                     domain_events de 
-               left join users u on 
-                     u.id = de.triggered_by_user_id
-               left join applications cas1_3_app on de.application_id = cas1_3_app.id     
-               left join cas_2_applications cas2_app on de.application_id = cas2_app.id
-               left join cas3_bookings cas3_booking on de.booking_id = cas3_booking.id
-               left join cas3_premises cas3_premises on cas3_booking.premises_id = cas3_premises.id
-               where
-                  de.service = :service_name and
-                  (de.crn = :crn
-                        or de.noms_number = :noms_number )
-               and (:start_date::date is null or de.created_at >= :start_date)
-               and (:end_date::date is null or de.created_at <= :end_date) 
-               order by de.created_at
-           ) domain_events
-      """.trimIndent(),
-      MapSqlParameterSource()
-        .addSarParameters(crn, nomsNumber, startDate, endDate)
-        .addValue("service_name", serviceName),
-    )
-    return toJsonString(result)
-  }
-
   protected fun toJsonString(result: Map<String, Any?>): String? = result["json"]?.toString()
   protected fun MapSqlParameterSource.addSarParameters(
     crn: String?,
